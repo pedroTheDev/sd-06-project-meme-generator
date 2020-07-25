65321461d47d40eb8c28862d9321f727bb833815
Havyner Caetano <hrcgeo@gmail.com>
2020-07-25 12:34:12 -0300

    Add onChange function to show typed input text in designated paragraph

---
+++ script.js
@@ -0,0 +1,6 @@
+let textInput = document.getElementById('text-input');
+let memeText = document.getElementById('meme-text');
+
+textInput.addEventListener('change', function (event) {
+    memeText.innerText = event.target.value
+})
