e09615ea51e278a46f9e049450739780163867f5
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-25 18:38:17 -0300

    passo 6

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ function showText() {
   paragrafo.innerText = input.value;
 }
 input.addEventListener('keyup', showText);
+
 function showImage() {
   const upload = document.getElementById('meme-image');
   const inserido = document.getElementById('meme-insert').value;
   upload.src = inserido;
 }
+
 paragrafo.style.textShadow = '5px 5px 5px black';
 paragrafo.style.fontSize = '30px';
 paragrafo.style.color = 'white';
 
+const container = document.querySelector('.container');
+function fire() {
+  container.style.border = '3px dashed red';
+}
+function water() {
+  container.style.border = '5px double blue';
+}
+function earth() {
+  container.style.border = '6px groove green';
+}
