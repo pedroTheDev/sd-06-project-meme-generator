1c651b2583799b02cf865acbdcdca567e9a98f3c
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-25 17:58:51 -0300

    passo 4 e 5

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function showImage() {
   const inserido = document.getElementById('meme-insert').value;
   upload.src = inserido;
 }
-document.getElementById('meme-insert').addEventListener('change', showImage);
 paragrafo.style.textShadow = '5px 5px 5px black';
 paragrafo.style.fontSize = '30px';
 paragrafo.style.color = 'white';
