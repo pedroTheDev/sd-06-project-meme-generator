8139b0ecb705bc473580a8b5d235c26fc6f45cde
Marxneves <marxkhristie@gmail.com>
2020-07-24 23:41:18 -0300

    Imagem colocada atraves do input e alinhada dentro da caixa, texto formatado com tamanho e sombreamento e acima da imagem

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ window.onload = function () {
   const text = document.getElementById('text-input');
   const divText = document.getElementById('meme-text');
   text.addEventListener('input', function () {
-    divText.innerText = text.value; 
+    divText.innerText = text.value;
+  });
+
+  const inputImg = document.getElementById('meme-insert');
+  const img = document.getElementById('meme-image');
+  inputImg.addEventListener('change', function (event) {
+    img.src = URL.createObjectURL(event.target.files[0]);
   });
 };
