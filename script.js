36b8f5245b6054dd9b7b22c816cdafc0e08e2c8b
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-25 18:34:34 -0300

    Adding new requirement check

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-window.onload = function() {
-  let memeText = document.querySelector("#meme-text")
-  let textInput = document.querySelector("#text-input")
+window.onload = function () {
+  let memeText = document.querySelector('#meme-text');
+  let textInput = document.querySelector('#text-input');
 
-  textInput.addEventListener("input", function(event) {
+  textInput.addEventListener('input', function(event) {
 
-    memeText.innerText = textInput.value
-  })
-}
+    memeText.innerText = textInput.value;
+  });
+
+  let fireButton = document.querySelector('#fire');
+  let imgContainer = document.querySelector("#meme-image-container");
+
+  fireButton.addEventListener('click', function (event) {
+    imgContainer.style.border = "3px solid red"
+  });
+
+  let waterButton = document.querySelector('#water');
+
+  waterButton.addEventListener('click', function (event) {
+    imgContainer.style.border = "3px solid blue"
+  });
+
+  let earthButton = document.querySelector('#earth');
+
+  earthButton.addEventListener('click', function (event) {
+    imgContainer.style.border = "3px solid green"
+  });
+};
