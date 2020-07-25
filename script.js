2e37a337335d9064f97a6ff78eded5a2e18d9863
Havyner Caetano <hrcgeo@gmail.com>
2020-07-25 14:51:08 -0300

    Add handler for the file input

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let textInput = document.getElementById('text-input');
 let memeText = document.getElementById('meme-text');
+let fileInput = document.getElementById('meme-insert');
+let image = document.getElementById('meme-image');
 
 textInput.addEventListener('change', function (event) {
   memeText.innerText = event.target.value;
 })
+
+fileInput.addEventListener('mouseout', function (event) {
+  image.src = URL.createObjectURL(event.target.files[0]);
+})
+
