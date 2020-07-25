fa9b71a9ce12101fdc2f9272d4cc526a7cada6d8
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-25 08:24:26 -0300

    Corrigindo primeiras divergências em relação aos requisitos e alguns fix issues do codeclimate, anterior 25 issues

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-  const textBox = document.querySelector("#meme-insert");
-  const memeContainer = document.querySelector("#meme-text");
-  const fileChooser = document.getElementById("img-chooser"); // Imagem escolhida
-  const imagePreview = document.getElementById("meme-image"); // preview da imagem
+  const textBox = document.querySelector('#meme-insert');
+  const memeContainer = document.querySelector('#meme-text');
+  const fileChooser = document.getElementById('img-chooser'); // Imagem escolhida
+  const imagePreview = document.getElementById('meme-image'); // preview da imagem
 
   function trocaTexto() {
     memeContainer.innerHTML = textBox.value;
   }
-  textBox.addEventListener("keyup", trocaTexto);
+  textBox.addEventListener('keyup', trocaTexto);
   
   function readImage() {
     imagePreview.src = window.URL.createObjectURL(fileChooser.files[0]);
