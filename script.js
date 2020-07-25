be005c4f0a6c2abfdbcef6ab340986350acf04a7
Daniel Moura <darthur.moura@gmail.com>
2020-07-25 18:39:58 -0300

    Primeiro commit com algumas tentativas

---
+++ script.js
@@ -0,0 +1,17 @@
+// Text handlers
+let memeText = document.getElementById("meme-text");
+let textInput = document.getElementById("text-input");
+let textBtn = document.getElementById("meme-button");
+
+textInput.addEventListener("keyup", function(){
+  memeText.innerText = textInput.value;
+})
+textBtn.addEventListener("keyup", function(){
+  memeText.innerText = textInput.value;
+})
+
+// Image handlers
+let memeImg = document.getElementById("meme-image");
+let imgInput = function(event){
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
+}
