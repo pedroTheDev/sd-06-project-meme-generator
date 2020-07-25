9ecce85df4854d787f2b71b2918163c406753dc2
Bruna Franciele <brunafranciele1902@gmail.com>
2020-07-25 17:53:52 -0300

    Envio do requisito 2

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let caixaTexto = document.querySelector("#text-input")
 caixaTexto.addEventListener("keyup", function(){
     document.querySelector("#meme-text").innerHTML = caixaTexto.value
 })
+let caixaImagem = document.querySelector("#meme-insert")
+caixaImagem.addEventListener("input", function(){
+document.getElementById("meme-image").src = caixaImagem.files[0].name
+console.log(document.getElementById("meme-image"))
 
 
+})
 
 
 
