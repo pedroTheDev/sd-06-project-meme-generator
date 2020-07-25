965ef1ed80e8c97a4a7a91acfe668f0611f62dcd
higoramorim <1810042@aluno.univesp.br>
2020-07-25 15:23:41 -0300

    adequação html e css ao projeto

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-window.onload = function(){
-    console.log("página carregada");
-}
 
-var carregaTexto = document.getElementById("texto");
+//variaveis
+let carregaTexto = document.getElementById("texto");
+const carregaImgbambam = document.getElementById("bambam");
+const carregaImgnaruto = document.getElementById("naruto");
+const carregaImgquedemais = document.getElementById("quedemais");
+const carregaImgvegeta = document.getElementById("vegeta");
+const capturaSection = document.getElementById("container");
 
-var carregaImagem = document.getElementById("fotos");
+//evento clique na imagem carrega na section container
+/*
+carregaTexto.style.fontFamily = ("monospace");
+carregaTexto.style.color = ("red");
+carregaTexto.style.fontSize = ("3em");
+*/
