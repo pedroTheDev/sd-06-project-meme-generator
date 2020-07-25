5f7c5a5379fc02512c3b1be61313289ad74799e0
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-25 19:27:16 -0300

    passo 6

--- script.js
+++ script.js
@@ -4,17 +4,9 @@ function showText() {
   paragrafo.innerText = input.value;
 }
 input.addEventListener('keyup', showText);
-
-function showImage() {
-  const upload = document.getElementById('meme-image');
-  const inserido = document.getElementById('meme-insert').value;
-  upload.src = inserido;
-}
-
 paragrafo.style.textShadow = '5px 5px 5px black';
 paragrafo.style.fontSize = '30px';
 paragrafo.style.color = 'white';
-
 const container = document.querySelector('#meme-image-container');
 const btn1 = document.querySelector('.fire');
 const btn2 = document.querySelector('.water')
