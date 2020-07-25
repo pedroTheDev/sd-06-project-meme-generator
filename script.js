6ba6c1ae4862a2d591524b91d33e9f53b59428bc
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-25 16:25:00 -0300

    passo 2

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function showText() {
   paragrafo.innerText = input.value;
 }
 input.addEventListener('keyup', showText);
+function showImage() {
+    let upload = document.getElementById('meme-image');
+    let inserido = document.getElementById('meme-insert').value;
+     upload.src = inserido;
+}
