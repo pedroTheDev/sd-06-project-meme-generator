dc2b8c876ec7cc7a282eac508fdee0d878b07a83
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-25 15:52:14 -0300

    Testing first requirement

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Scripts
+window.onload = function() {
+  let memeText = document.querySelector("#meme-text")
+  let textInput = document.querySelector("#text-input")
+
+  textInput.addEventListener("input", function(event) {
+
+    memeText.innerText = textInput.value
+  })
+}
