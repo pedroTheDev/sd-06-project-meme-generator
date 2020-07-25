664977680c4dcc50a5d87efe431d581ed7d402c1
Bruna Franciele <brunafranciele1902@gmail.com>
2020-07-25 19:50:59 -0300

    envio do requisito 3

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ caixaTexto.addEventListener("keyup", function(){
 let caixaImagem = document.querySelector("#meme-insert")
 caixaImagem.addEventListener("change", function(event) {
 document.getElementById("meme-image").src =  URL.createObjectURL(event.target.files[0]);
+// document.getElementById("meme-image").style.height = "400px"
+// document.getElementById("meme-image").style.widht = "400px"
 })
 
 
