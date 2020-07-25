6bbcb369a03a5bc411935492090ac60a8a7ba81d
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-25 20:04:52 -0300

    Checking if requirements are 100% done

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ window.onload = function () {
   let memeText = document.querySelector('#meme-text');
   let textInput = document.querySelector('#text-input');
 
-  textInput.addEventListener('input', function() {
-
+  textInput.addEventListener('input', function () {
     memeText.innerText = textInput.value;
   });
 
@@ -11,18 +10,41 @@ window.onload = function () {
   const imgContainer = document.querySelector('#meme-image-container');
 
   fireButton.addEventListener('click', function () {
-    imgContainer.style.border = "3px solid red";
+    imgContainer.style.border = '3px dashed red';
   });
 
   const waterButton = document.querySelector('#water');
 
   waterButton.addEventListener('click', function () {
-    imgContainer.style.border = "3px solid blue";
+    imgContainer.style.border = '5px blue double';
   });
 
   const earthButton = document.querySelector('#earth');
 
   earthButton.addEventListener('click', function () {
-    imgContainer.style.border = "3px solid green";
+    imgContainer.style.border = '6px green groove';
+  });
+
+  const picMeme1 = document.querySelector('#meme-1');
+  const picMeme2 = document.querySelector('#meme-2');
+  const picMeme3 = document.querySelector('#meme-3')
+  const picMeme4 = document.querySelector('#meme-4')
+
+  let memeImage = document.querySelector('#meme-image');
+
+  picMeme1.addEventListener('click', function () {
+    memeImage.style.backgroundImage = "url('./imgs/meme1.png')"
+  });
+  
+  picMeme2.addEventListener('click', function () {
+    memeImage.style.backgroundImage = "url('./imgs/meme2.png')"
+  });
+
+  picMeme3.addEventListener('click', function () {
+    memeImage.style.backgroundImage = "url('./imgs/meme3.png')"
+  });
+
+  picMeme4.addEventListener('click', function () {
+    memeImage.style.backgroundImage = "url('./imgs/meme4.png')"
   });
 };
