9598def6bab9bdf030be9d681c2add8b59456e73
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-25 19:35:48 -0300

    passo 6

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ paragrafo.style.fontSize = '30px';
 paragrafo.style.color = 'white';
 const container = document.querySelector('#meme-image-container');
 const btn1 = document.querySelector('.fire');
-const btn2 = document.querySelector('.water')
-const btn3 = document.querySelector('.earth')
+const btn2 = document.querySelector('.water');
+const btn3 = document.querySelector('.earth');
 function fire() {
   container.style.border = '3px dashed red';
   btn1.style.border = '3px dashed red';
 }
+btn1.addEventListener('click', fire);
 function water() {
   container.style.border = '5px double blue';
   btn2.style.border = '5px double blue';
 }
+btn2.addEventListener('click', water);
 function earth() {
   container.style.border = '6px groove green';
   btn3.style.border = '6px groove green';
 }
+btn3.addEventListener('click', earth);
