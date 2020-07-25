70d2e2261cd20afd25f8fb58433bd5131a0fe28e
Victor Bergamini <vbergadev@gmail.com>
2020-07-25 11:57:21 -0300

    criando id's obrigatorias

---
+++ script.js
@@ -0,0 +1,9 @@
+function gerarmeme() {
+    alert('Gerado')
+    let textoMeme = document.getElementById("text-input");
+    let imagemMeme = document.getElementById("meme-image-container");
+    
+}
+function uploadImagem(){
+    alert('imagem')
+}
