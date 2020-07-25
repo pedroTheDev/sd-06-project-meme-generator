0df9c4262c23d68cab4838ee0f90eb2cc221631d
fefarina <wfelipefarina@gmail.com>
2020-07-25 19:27:14 -0300

    Update all

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-teste
+
+function capturar (){
+    let inserText = "";
+
+    insertText = document.getElementById('text-input').value;
+    document.getElementById('meme-text').innerHTML = inserText;
+}
+
+capturar();
