5ec5eda5706ef9adf311cf179e4873c60cef6f31
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-25 13:50:49 -0300

    passo 1

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("text-input");
+const paragrafo = document.getElementById("meme-text");
+function showText() {
+    paragrafo.innerText = input.value;
+}
+input.addEventListener('keyup', showText);
