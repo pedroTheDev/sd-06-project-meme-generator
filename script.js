0662e386ace3d4e92e12d87c88a438dd203861de
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-26 19:47:47 -0300

    alteraçao

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ window.onload = function(){
     const imageInput = document.querySelector("#meme-insert");
     imageInput.addEventListener("change", function(){
         const image = document.querySelector("#meme-image");
-        image.src = imageInput.value;
+        image.src = imageInput.files[0].name;
     })    
 }
