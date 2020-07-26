db0fc425e5386d697af2c3b262dd0e6cee8b2bc8
Havyner Caetano <hrcgeo@gmail.com>
2020-07-26 11:56:32 -0300

    Add new values for a new attempt at having the code validated by evaluator

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let memeText = document.getElementById('meme-text');
 let fileInput = document.getElementById('meme-insert');
 let image = document.getElementById('meme-image');
 
-textInput.addEventListener('change', function (event) {
+textInput.addEventListener('input', function (event) {
   memeText.innerText = event.target.value;
 })
 
