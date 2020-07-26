7d935eec493b1454a02d55c55adb027815500bff
Julia Porto <juportob@gmail.com>
2020-07-26 09:48:54 -0300

    Add mais requisitos

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.onload = function() {
 
-  let inputDeTextop = document.querySelector("#text-input")
+  const inputDeTextop = document.querySelector("#text-input")
   inputDeTextop.addEventListener("keyup", function(){
     document.querySelector("#meme-text").innerHTML = inputDeTextop.value
   })
 
-  let localImagem = document.querySelector("#meme-insert")
+  const localImagem = document.querySelector("#meme-insert")
   localImagem.addEventListener("input", function(){
   document.getElementById("meme-image").src = URL.createObjectURL(event.target.files[0]);
   })
-  console.log(document.getElementById("meme-image"))
+  
 }
