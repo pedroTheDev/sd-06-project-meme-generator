50d1c5d33f173ec2614f1400000efa6335aa53c8
rafMewtwo <ponciano.raf@gmail.com>
2020-07-26 01:56:38 -0300

    testando os requisitos

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 window.onload = function() {
 
     let caixaTexto = document.querySelector("#text-input")
-    let imgTexto = document.querySelector("#meme-text")
+    
 
     caixaTexto.addEventListener("keyup", function(){
         document.querySelector("#meme-text").innerHTML = caixaTexto.value
     })
+
+    // let buttonImg = document.querySelector("#meme-insert")
+    // let caixaImg = document.querySelector("#meme-image")
+    // buttonImg.addEventListener("input", function(){
+    //     caixaImg.style.backgroundImage = url(buttonImg.value)e
+    //     //document.getElementById("meme-image").src = caixaImg.files[0].name
+    //     console.log(buttonImg.value)
+    // })
+
+
+
+
 
 }
