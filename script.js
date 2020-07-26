e06000057ff61059456390cd7445ac40063d14cc
Ana Flávia <anaflvia@gmail.com>
2020-07-26 11:35:02 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let textoDoMeme = document.querySelector('#text-input');
-textoDoMeme.addEventListener('input', function() {
+textoDoMeme.addEventListener('input', function () {
   let texto = document.querySelector('#meme-text');
   texto.innerHTML = textoDoMeme.value;
-})
+});
