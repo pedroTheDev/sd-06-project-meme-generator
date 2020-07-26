015ab6ebf3258d8a4c475b573974595574415426
Renan Cunha <renan2610@gmail.com>
2020-07-26 14:29:55 -0300

    adicionando container onde o texto será mostrado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload = function() {
-  let textInput = document.getElementById('text-input');
-  textInput.addEventListener('keyup', function() {
+window.onload = function () {
+  const textInput = document.getElementById('text-input');
+  textInput.addEventListener('keyup', function () {
     document.getElementById('meme-text').value = textInput.value;
-  })
-}
+  });
+};
