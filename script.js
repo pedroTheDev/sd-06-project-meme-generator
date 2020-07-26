3b4188150f647c420723ddd162d407e70abbee38
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-26 19:08:14 -0300

    alteracao javascript

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 window.onload = function(){
-    const textInput = document.querySelector("#text-input");
-    textInput.addEventListener("keyup", function(){
-        const text = document.querySelector("#meme-text");
-        text.innerHTML = textInput.value;
-    })
-    const imageInput = document.querySelector("#meme-insert");
-    imageInput.addEventListener("change", function(){
-        const image = document.querySelector("#meme-image");
-        image.src = imageInput.value;
-    })
+    let textInput = document.getElementById('text-input');
+    let inserText = document.getElementById('meme-text');
     
+    let returnText = textInput.addEventListener('input', function (e) {
+        inserText.innerText = e.target.value
+    });
+    
+    //
+    
+    let imageInput = document.querySelector('#meme-insert');
+    let image = document.querySelector('#meme-image');
+    
+    imageInput.addEventListener('change', function (e) {
+        image.src = URL.createObjectURL(e.target.files[0]);
+    });
 }
