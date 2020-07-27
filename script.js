fdb435449d3d62f624c62bc86bb0bf0f00796438
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-27 19:37:14 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const inputTexto = document.querySelector('#text-input');
 const outputTexto = document.querySelector('#meme-text');
+const inputImg = document.querySelector('input[id="meme-insert"]')
 
 inputTexto.addEventListener('input', function() {
 outputTexto.innerHTML = inputTexto.value;
 });
 
-
+inputImg.addEventListener('change', function (e) {
+const reader = new FileReader()
+reader.onload = function () {
+const img = new Image()
+img.src = reader.result
+document.body.appendChild(img)
+}
+reader.readAsDataURL(inputImg.files[0])
+}, false) 
