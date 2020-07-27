56d7e97e448bed4e4d249e0c73a694764f2dd54f
isabellavjs <isabellavjs@gmail.com>
2020-07-27 19:33:56 -0300

    Requirements - add image and text

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-//Variables
+//Receive user text, and write it on the image;
 let userText = document.getElementById("text-input");
 let containerText = document.getElementById("meme-text");
-let insertMeme = document.getElementById("meme-insert");
-//let buttonInsertMeme = document.getElementsByTagName("btn-meme-text");
-//let uploadedMeme = document.getElementsByTagName("meme-image");"
-
-
-//Receive user text, and write it on the image;
 userText.addEventListener("keyup", function() {
     containerText.innerHTML = userText.value;
 });
-//buttonInsertMeme.addEventListener("click", function() {
-    //containerText.innerHTML = userText.value;
-//})
 
 //Upload user image
-insertMeme.addEventListener("change", function() {
-    img.id = "meme-image";
-    img.src = URL.createObjectURL(event.target.files[0]);
-})
+let insertMeme = document.getElementById("meme-image");
+//let uploadedMeme = document.getElementsByTagName("meme-image")[0];
+let uploadedMeme = function(event) {
+    insertMeme.src = URL.createObjectURL(event.target.files[0]);
+}
+
 
 
