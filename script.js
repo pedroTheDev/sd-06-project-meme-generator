ac1184ad98b19cd76ab18a81796a2fd28eaa4541
kissyla <kissylamottapereira@gmail.com>
2020-07-27 19:26:00 -0300

    tentativa req2

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function imageUpload(){
-    let image = document.getElementById("meme-insert");
-
-}
-function textUpload(){
+window.onload = function(){
+function textUpload(event){
     let text = document.getElementById("text-input").value;
     document.getElementById("meme-text").innerText = text;
     
 }
+function imageUpload(event) {
+    var output = document.getElementById('meme-image');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) 
+    }
 document.getElementById("text-input").addEventListener("input",textUpload);
+
+document.getElementById("meme-insert").addEventListener("change",imageUpload);
+}
