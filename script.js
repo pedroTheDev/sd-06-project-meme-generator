845991c02b497e9949008ae0729f21a4d748a8d9
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-27 18:12:59 -0300

    Desafio 1.1

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let memeText = document.getElementById('meme-text');
 let textImput = document.getElementById('text-input');
+let memeImageContainer = document.getElementById('meme-image-container');
 
 textImput.addEventListener('keyup', function () {
-  let criarTexto = document.createElement("p");
-  criarTexto.innerHTML = textImput.value;
-  memeText.appendChild(criarTexto);
+  memeText.innerHTML = textImput.value;
 });
 
 let loadFile = function (event) {
   let output = document.getElementById('output');
   output.src = URL.createObjectURL(event.target.files[0]);
+  output.style.maxHeight = '200px';
+  output.style.maxWidth = '200px';
   output.onload = function () {
-    URL.revokeObjectURL(output.src)
+    URL.revokeObjectURL(output.src);
   }
 };
+
+// memeImageContainer.src = output;
