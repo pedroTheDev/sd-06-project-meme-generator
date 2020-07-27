5f5c8a15c9a382dbe7a4455255c6371a8464b4e4
Renan Cunha <renan2610@gmail.com>
2020-07-27 00:17:21 -0300

    adicionando upload da imagem, moldura e edições de texto

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.onload = function () {
   const textInput = document.getElementById('text-input');
+  const memeInsert = document.getElementById('meme-insert');
+  let memeImage = document.getElementById('meme-image');
+
   textInput.addEventListener('keyup', function () {
     document.getElementById('meme-text').innerText = textInput.value;
   });
+
+  memeInsert.addEventListener('change', function (event) {
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    console.log(memeImage.src);
+    memeImage.onload = function () {
+      URL.revokeObjectURL(memeImage.src);
+    }
+    console.log(memeImage.src);
+  })
 };
