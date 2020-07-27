5a2edbff2c00d1c09285fdec1e5ac416fd318340
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-27 16:54:02 -0300

    Task 7: adding pre-set images

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let inputImageElement =document.querySelector('#meme-insert');
 let memeImage = document.querySelector('#meme-image');
 let buttons = document.querySelectorAll('button');
 let container = document.querySelector('#meme-image-container');
+let preSetImages = document.querySelectorAll('#thumbnails img');
 
 let availableStyles = {
     fire: 'dashed rgb(255 , 0 , 0) 3px',
@@ -23,6 +24,12 @@ let functionalities = {
         water: (event) => container.style.border = availableStyles['water'],
         earth: (event) => container.style.border = availableStyles['earth'],
     },
+    uploadPreSetImage: {
+        meme1: (event) => memeImage.src = 'imgs/meme1.png',
+        meme2: (event) => memeImage.src = 'imgs/meme2.png',
+        meme3: (event) => memeImage.src = 'imgs/meme3.png',
+        meme4: (event) => memeImage.src = 'imgs/meme4.png',
+    },
 }
 
 
@@ -33,5 +40,9 @@ inputImageElement.addEventListener('change', functionalities['uploadImage']);
 for (let i = 0; i < buttons.length; i += 1) {
     let key = buttons[i].id;
     buttons[i].addEventListener('click', functionalities.changeStyle[key]);
+}
+for (let i=0; i < preSetImages.length; i += 1) {
+    let key = 'meme' + (i+1);
+    preSetImages[i].addEventListener('click', functionalities.uploadPreSetImage[key]);
 }
 }
