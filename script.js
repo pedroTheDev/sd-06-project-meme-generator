b14dd101fd4e1d4ba087b385d5978e3f5d98a694
Keila Sales <k.f.saless@gmail.com>
2020-07-27 18:43:45 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let memeText = document.getElementById("meme-text");
-memeText.addEventListener(function () {
-    
+//Elementos 
+let memeText = document.getElementById("meme-text");  
+let textInput = document.getElementById("text-input");
 
+//Eventos
+textInput.addEventListener('keyup', escreverTexto);
 
-
-})
-    
+function escreverTexto() {
+    memeText.innerHTML = textInput.value;
+}
 
  
 
