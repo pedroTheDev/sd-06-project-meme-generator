0398ba0ec94ec26261e4108e86489a681ba88b18
Ana Flávia <anaflvia@gmail.com>
2020-07-27 17:27:20 -0300

    upload de imagem

--- script.js
+++ script.js
@@ -4,17 +4,11 @@ textoDoMeme.addEventListener('input', function () {
   let texto = document.querySelector('#meme-text');
   texto.innerHTML = textoDoMeme.value;
 });
-function visualizarImg() {
-  let image = document.querySelector('input[name=imagem]').files[0];
-  let preview = document.querySelector('img')
-  let reader = new FileReader();
-  reader.onloaded = function() {
-    preview.src = reader.result;
-  }
-  if(image) {
-    reader.readAsDataURL(image);
-  } else {
-    preview.src="";
+function visualizarImg(event) {
+  let preview = document.querySelector('img');
+  preview.src = URL.createObjectURL(event.target.files[0]);
+  preview.onload = function() {
+    URL.revokeObjectURL(preview.src);
   }
 }
 inputImg.addEventListener('change', visualizarImg)
