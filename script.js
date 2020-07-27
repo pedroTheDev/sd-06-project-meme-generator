a500ee706fc6b0de6fd5d0f983024a2aaefda282
FPhilipi <fabiphilipi@gmail.com>
2020-07-27 14:19:50 -0300

    Requisitos

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 window.onload = function() {
+
+let textInPut = document.querySelector("#text-input");
+textInPut.addEventListener("keyup", function(){
+    let textOutPut = document.querySelector("#meme-text");
+    textOutPut.innerHTML = textInPut.value;
+})
+    
 let imageInPut = document.querySelector("#meme-insert");
 
 imageInPut.addEventListener("change", function(event) {
