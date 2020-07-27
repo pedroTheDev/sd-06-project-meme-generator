63155023e7be03a5d4029b63b761713c04ced786
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-27 19:03:21 -0300

    Algoritmo texto

---
+++ script.js
@@ -0,0 +1,13 @@
+//Interação com o input de TEXTO
+//1. Elementos para acessar
+let inputText = document.getElementById("text-input");
+let memeText = document.getElementById("meme-text");
+//2. Pega o input e mostra na tela
+let userTextMeme = "";
+function getTextInput() {
+    userTextMeme = inputText.value;
+    memeText.innerText = userTextMeme;
+}
+inputText.addEventListener("keyup", getTextInput);
+
+//Interação com o input da IMAGEM
