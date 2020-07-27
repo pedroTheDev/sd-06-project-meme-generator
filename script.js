9c3c31c3c65b2490d02ce4241acb72fdf46b8da0
Pablo Santos <pablo.trybe@gmail.com>
2020-07-27 18:45:31 -0300

    Funçao de carregar a foto implementada

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let inputText = document.getElementById('text-input');
+let memeText = document.getElementById('meme-text');
+let textInMeme = '';
+function printTextMeme() {
+  textInMeme = inputText.value;
+  memeText.innerHTML = textInMeme;
+}
+inputText.addEventListener('keyup', printTextMeme);
 const loadFile = function (event) {
   const output = document.getElementById('output');
   output.src = URL.createObjectURL(event.target.files[0]);
