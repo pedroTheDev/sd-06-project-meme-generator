eb04f930aec22eaf20d63255f3ab8cca526657cd
Renan Cunha <renan2610@gmail.com>
2020-07-27 01:03:09 -0300

    adicionando limite de caracteres no input

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const textInput = document.getElementById('text-input');
   const memeInsert = document.getElementById('meme-insert');
-  let memeImage = document.getElementById('meme-image');
+  const memeImage = document.getElementById('meme-image');
 
   textInput.addEventListener('keyup', function () {
     document.getElementById('meme-text').innerText = textInput.value;
@@ -9,10 +9,8 @@ window.onload = function () {
 
   memeInsert.addEventListener('change', function (event) {
     memeImage.src = URL.createObjectURL(event.target.files[0]);
-    console.log(memeImage.src);
     memeImage.onload = function () {
       URL.revokeObjectURL(memeImage.src);
     }
-    console.log(memeImage.src);
-  })
+  });
 };
