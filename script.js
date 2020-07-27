06ddea80084d44018c78e94b75f3254b75a6d2ad
DehSartoreto <fixsartoreto@gmail.com>
2020-07-27 17:04:59 -0300

    add butoes + tentando agradar o evaluator

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ window.onload = function() {
     let upImage = document.getElementById("meme-insert");
     upImage.addEventListener("change", upload);
     
+    let textoMeme = document.querySelector("#text-input");
+    textoMeme.addEventListener("keyup", showText);
+
     let textShow = document.querySelector("#meme-text");
     function showText() {
         textShow.innerHTML = textoMeme.value; 
     }
-    let textoMeme = document.querySelector("#text-input");
-    textoMeme.addEventListener("keyup", showText)
+    
+    let containerImagem = document.querySelector("#meme-image-container");
+    let btn1 = document.querySelector("#fire");
+    let btn2 = document.querySelector("#water");
+    let btn3 = document.querySelector("#earth");
+
+    btn1.addEventListener("click", function() {
+        btn1.style.backgroundColor = "red";
+        containerImagem.style.border = "3px dashed red";
+    })
+
+    btn2.addEventListener("click", function() {
+        btn2.style.backgroundColor = "blue";
+        containerImagem.style.border = "5px double blue";
+    })
+
+    btn3.addEventListener("click", function() {
+        btn3.style.backgroundColor = "green";
+        containerImagem.style.border = "6px groove green";
+    })
 }
