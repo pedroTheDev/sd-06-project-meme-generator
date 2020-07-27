3d3de599b6419f3b7615e09adb90c6d8c6ad69d8
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-27 18:22:15 -0300

    se melhoro sintaxys

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-window.onload = function() {
-  //Tomando imagen e colocando no container
-  let meme_Image = document.createElement('img')
-  loadFile = function(event) {
-    let reader = new FileReader();
-    reader.onload = function(){
-      let image = document.getElementById('meme-image').appendChild(meme_Image);
+window.onload = function () {
+  //  Tomando imagen e colocando no container
+  const meme_Image = document.createElement('img');
+  loadFile = function (event) {
+    const reader = new FileReader();
+    reader.onload = function () {
+      const image = document.getElementById('meme-image').appendChild(meme_Image);
       image.src = reader.result;
     };
     reader.readAsDataURL(event.target.files[0]);
   };
-
-
-  //imagem.addEventListener('input', function() {
-  //  imagemInserida.src = "./img/" + imagem.files[0].name;
-  //  document.querySelector('#meme-image-container').//appendChild(imagemInserida)
-  //})
- //
-  //Colocando o texto inserido em no container
-  
-  
-  let textoInput = document.querySelector('#text-input');
-  textoInput.addEventListener('keyup', function() {
-    let texto = document.querySelector('#meme-text');
-    texto.innerHTML = textoInput.value; 
-  })
-}
+  //  Colocando o texto inserido em no container
+  const textoInput = document.querySelector('#text-input');
+  textoInput.addEventListener('keyup', function () {
+    const texto = document.querySelector('#meme-text');
+    texto.innerHTML = textoInput.value;
+  });
+};
