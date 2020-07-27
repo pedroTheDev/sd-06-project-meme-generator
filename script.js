c5d589bfe4a59889ed31efc323c5a6f8ba1d83cd
Alana Monteiro <lanaah.15@gmail.com>
2020-07-27 17:37:07 -0300

    Base do projeto

---
+++ script.js
@@ -0,0 +1,8 @@
+function textAdd(){
+    let memeText = textoMeme.inputText.value;
+    if (memeText.length > 50) {
+        alert("O texto deve ter menos de 50 caracteres!");
+    } else {
+
+    }
+}
