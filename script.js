27a1960d65f98c7bab48d28adc2c23ea5b9b2ce6
Anderson <and.stuber@gmail.com>
2020-07-27 19:37:09 -0300

    Try Evaluator

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getText() {
   const textInput = document.getElementById('text-input');
   const memeText = document.getElementById('meme-text');
   textInput.addEventListener('input', () => {
-    memeText.innerText = textInput.value;
+    memeText.innerHTML = textInput.value;
   });
 }
 
