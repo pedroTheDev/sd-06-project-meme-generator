545dc5e59465bff403a86566e5a91a6c8b539151
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-27 18:17:07 -0300

    Ajuste codeclimate 1

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let memeText = document.getElementById('meme-text');
-let textImput = document.getElementById('text-input');
-let memeImageContainer = document.getElementById('meme-image-container');
+const memeText = document.getElementById('meme-text');
+const textImput = document.getElementById('text-input');
+const memeImageContainer = document.getElementById('meme-image-container');
 
 textImput.addEventListener('keyup', function () {
   memeText.innerHTML = textImput.value;
 });
 
-let loadFile = function (event) {
-  let output = document.getElementById('output');
+const loadFile = function (event) {
+  const output = document.getElementById('output');
   output.src = URL.createObjectURL(event.target.files[0]);
   output.style.maxHeight = '200px';
   output.style.maxWidth = '200px';
   output.onload = function () {
     URL.revokeObjectURL(output.src);
-  }
+  };
 };
-
-// memeImageContainer.src = output;
