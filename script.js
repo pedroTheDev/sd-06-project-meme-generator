ce53b391a56d20d33a87edb7ec7b69dd1fcc4f1f
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-27 19:04:08 -0300

    alterações de codeclimate

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-//Script do projeto meme generator
-let loadFile = function(event) {
-    let output = document.getElementById('meme-image');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      URL.revokeObjectURL(output.src) 
-    }
+const loadFile = function (event) {
+  const output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src);
+  }
 };
 
 addEventListener('input', function insertText() {
-  let text = document.getElementById('text-input')
+  const text = document.getElementById('text-input');
   document.getElementById('meme-text').innerHTML=text.value;
 })
