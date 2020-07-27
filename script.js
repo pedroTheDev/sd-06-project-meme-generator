3a61c36c249d14a0ef917f5b4663628c96a41bcb
Danilo <dansantosalfa@gmail.com>
2020-07-27 19:26:53 -0300

    Adicionando codigo basico .js

---
+++ script.js
@@ -0,0 +1,6 @@
+let text_Input = document.getElementById("text-input");
+let text_meme = document.getElementById("meme-text");
+text_meme.style.textShadow = '5px 5px 5px black';
+text_meme.style.fontSize = "30px";
+text_meme.style.color = "White";
+text_meme.innerHTML = text_Input;
