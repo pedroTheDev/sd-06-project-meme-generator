8d9f45b1cbca1fb0334fbe4644a3091e04ab2f5e
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-27 15:10:20 -0300

    se mudo codigo para upload da foto

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 window.onload = function() {
   //Tomando imagen e colocando no container
-  let imagem = document.querySelector('#meme-insert');
-  let imagemInserida = document.createElement('img');
-  imagem.addEventListener('input', function() {
-    imagemInserida.src = "./img/" + imagem.files[0].name;
-    
-    document.querySelector('#meme-image-container').appendChild(imagemInserida)
-  })
+  let output = document.getElementById('output');
+  loadFile = function(event) {
+    let reader = new FileReader();
+    reader.onload = function(){
+      let output = document.getElementById('output');
+      output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
+
+
+  //imagem.addEventListener('input', function() {
+  //  imagemInserida.src = "./img/" + imagem.files[0].name;
+  //  document.querySelector('#meme-image-container').//appendChild(imagemInserida)
+  //})
+//
+  //Colocando o texto inserido em no container
+  let textoInput = document.querySelector('#text-input');
+
 }
