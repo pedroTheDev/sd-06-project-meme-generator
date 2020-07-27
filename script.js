dea95757bbaa53a9c2f82247a2297d4eeb3bb8b6
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-27 16:25:20 -0300

    Resolução do requisito 01

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-window.onload = function() {
-    // alert("pagina carregada"); 
+window.onload = function () {
+	let myTextImput = document.getElementById("text-input");
+	myTextImput.addEventListener("keyup", function(){
+		console.log(myTextImput.value);
+		let myTextMeme = document.getElementById("meme-text");
+		if (myTextImput.value == "") {
+			myTextMeme.innerText = "Texto do Meme"	
+		} else {
+			myTextMeme.innerText = myTextImput.value;
+		}
+	});
 };
