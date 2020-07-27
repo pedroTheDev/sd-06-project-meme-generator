db85ae0edf524e756d8397f85db9043a486321fd
Isadora Koga <isadorakoga710@gmail.com>
2020-07-27 20:46:29 -0300

    Requisito 7 done

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function textInput() {
   divMemeText.innerHTML = inputText;
 }
 
+const outputImage = document.getElementById('meme-image');
+
 function imageInput() {
   // Função que vai fazer a imagem aparecer no meme
-  const outputImage = document.getElementById('meme-image');
   outputImage.src = URL.createObjectURL(event.target.files[0]);
   outputImage.onload = function () {
     URL.revokeObjectURL(outputImage.src);
@@ -27,4 +28,35 @@ function waterCustom() {
 
 function earthCustom() {
   divContainer.style.border = '6px groove green';
+}
+
+// Cria a variavel que pega o src da imagem
+// Insere a url da imagem na div meme-container
+
+function firstMeme() {
+    const memeImage1 = document.getElementById('meme1').src;
+    outputImage.src = memeImage1;
+    outputImage.style.width = "500px";
+    outputImage.style.height = "400px";
+}
+
+function secondMeme() {
+    const memeImage2 = document.getElementById('meme2').src;
+    outputImage.src = memeImage2;
+    outputImage.style.width = "500px";
+    outputImage.style.height = "400px";
+}
+
+function thirdMeme() {
+    const memeImage3 = document.getElementById('meme3').src;
+    outputImage.src = memeImage3;
+    outputImage.style.width = "500px";
+    outputImage.style.height = "400px";
+}
+
+function fourthMeme() {
+    const memeImage4 = document.getElementById('meme4').src;
+    outputImage.src = memeImage4;
+    outputImage.style.width = "500px";
+    outputImage.style.height = "400px";
 }
