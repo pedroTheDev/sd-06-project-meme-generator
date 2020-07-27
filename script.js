c690ca776b84c901ba671dccf523da4ebadf95e2
Marxneves <marxkhristie@gmail.com>
2020-07-27 19:22:19 -0300

    botoes de personalizacao de borda

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-window.onload = function () {
-  const text = document.getElementById('text-input');
-  const divText = document.getElementById('meme-text');
-  text.addEventListener('input', function () {
-    divText.innerText = text.value;
-  });
+const text = document.getElementById('text-input');
+const divText = document.getElementById('meme-text');
+text.addEventListener('input', function () {
+  divText.innerText = text.value;
+});
 
-  const inputImg = document.getElementById('meme-insert');
-  const img = document.getElementById('meme-image');
-  inputImg.addEventListener('change', function (event) {
-    img.src = URL.createObjectURL(event.target.files[0]);
-  });
-};
+const inputImg = document.getElementById('meme-insert');
+const img = document.getElementById('meme-image');
+inputImg.addEventListener('change', function (event) {
+  img.src = URL.createObjectURL(event.target.files[0]);
+});
+
+const container = document.getElementById('meme-image-container');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+
+btnFire.addEventListener('click', () => {
+  container.className = 'container fire';
+});
+
+btnWater.addEventListener('click', () => {
+  container.className = 'container water';
+});
+
+btnEarth.addEventListener('click', () => {
+  container.className = 'container earth';
+});
