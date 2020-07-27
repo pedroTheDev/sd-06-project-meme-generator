c27cd060669061799862d5124447a0ee9fb01697
Ana Flávia <anaflvia@gmail.com>
2020-07-27 13:29:53 -0300

    modificações

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ textoDoMeme.addEventListener('input', function () {
   let texto = document.querySelector('#meme-text');
   texto.innerHTML = textoDoMeme.value;
 });
+let loadImage = function(event) {
+  let output = document.querySelector('#output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src);
+  }
+}
