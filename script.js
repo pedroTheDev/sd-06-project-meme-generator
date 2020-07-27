49fc7cd310ad4d388251943f862b98fd46c50bd5
Daniel Jesus <dev.danieljs@gmail.com>
2020-07-27 18:42:58 -0300

    Adding function input text and input img

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 window.onload = function() {
-  
+
+  // Input Text
+    let text = document.getElementById('text-input');
+    text.addEventListener('input', function() {
+      let print = document.getElementById('meme-text');
+      print.innerHTML = text.value;
+    })
+
+   // Input img
+  let setImgInput = document.querySelector('#meme-insert');
+  setImgInput.addEventListener('change', function(event) {
+    document.querySelector('#meme-image').src = URL.createObjectURL(event.target.files[0]);
+  })
 }
