edb10b0e7f328ae592e9e6299e5296e69d48f2fd
cyranowebdev <cyrano@cyranowebdev.com>
2020-07-27 18:13:12 -0300

    add ajustes para primeiro bônus - change borders.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const image = document.querySelector('#img-receiver');
 const imageSelector = document.querySelector('#meme-insert');
+const memeContainer = document.querySelector('#meme-image-container');
 
 const textInput = document.querySelector('#text-input');
 const textPrint = document.querySelector('#meme-text');
-
-textInput.addEventListener('keyup', printText);
 
 function loadImg(event) {
   image.src = URL.createObjectURL(imageSelector.files[0]);
@@ -14,6 +13,22 @@ function loadImg(event) {
 imageSelector.addEventListener('change', loadImg);
 
 function printText(event) {
-  let textValue = document.querySelector('#text-input').value;
+  const textValue = document.querySelector('#text-input').value;
   textPrint.innerHTML = textValue;
 }
+
+textInput.addEventListener('keyup', printText);
+
+const fireBtn = document.querySelector('#fire');
+const waterBtn = document.querySelector('#water');
+const earthBtn = document.querySelector('#earth');
+const resetBtn = document.querySelector('#reset');
+
+function changeBorder(element) {
+  memeContainer.style.border = element;
+}
+
+fireBtn.addEventListener('click', () => { changeBorder('3px dashed red') });
+waterBtn.addEventListener('click', () => { changeBorder('5px double blue') });
+earthBtn.addEventListener('click', () => { changeBorder('6px groove green') });
+resetBtn.addEventListener('click', () => { changeBorder('1px solid black') });
