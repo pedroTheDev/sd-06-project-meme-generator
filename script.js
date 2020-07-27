8130d7d2b964310cb6deff31f9ee474a14f40fab
William <williamjog@hotmail.com>
2020-07-27 20:16:22 -0300

    1st bonus requirement - border buttons

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const memeImg = document.querySelector('#meme-image');
 const memeInsertImg = document.querySelector('#meme-insert');
+const containerMemeImage = document.querySelector('#meme-image-container');
+const fireBorderButton = document.querySelector('#fire');
+const waterBorderButton = document.querySelector('#water');
+const earthBorderButton = document.querySelector('#earth');
 
 textInput.addEventListener('input', function () {
   memeText.innerText = textInput.value;
@@ -11,3 +15,24 @@ textInput.addEventListener('input', function () {
 memeInsertImg.addEventListener('input', function () {
   memeImg.src = URL.createObjectURL(event.target.files[0]);
 });
+
+function changeBorders() {
+  if (event.target === fireBorderButton) {
+    containerMemeImage.classList.remove('waterBorder');
+    containerMemeImage.classList.remove('earthBorder');
+    containerMemeImage.classList.add('fireBorder');
+  }
+  if (event.target === waterBorderButton) {
+    containerMemeImage.classList.remove('fireBorder');
+    containerMemeImage.classList.remove('earthBorder');
+    containerMemeImage.classList.add('waterBorder');
+  }
+  if (event.target === earthBorderButton) {
+    containerMemeImage.classList.remove('waterBorder');
+    containerMemeImage.classList.remove('fireBorder');
+    containerMemeImage.classList.add('earthBorder');
+  }
+}
+fireBorderButton.addEventListener('click', changeBorders);
+waterBorderButton.addEventListener('click', changeBorders);
+earthBorderButton.addEventListener('click', changeBorders);
