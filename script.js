be394e87218726d2ed400a9f1573b291cd5f575a
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-27 02:36:50 -0300

    Task 6: changing container style

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-//Showing meme text
+//Referencing elements
 let memeTextInput = document.querySelector('#text-input');
 let memeTextBox = document.querySelector('#meme-text');
 let realImageUploadButton = document.querySelector('#meme-insert');
 let imagegUplodButton =document.querySelector('#fake-upload-button');
 let memeImage = document.querySelector('#meme-image');
+let fireStyleButton = document.querySelector('#fire');
+let waterStyleButton = document.querySelector('#water');
+let earthStyleButton = document.querySelector('#earth');
+let imageContainer = document.querySelector('#meme-image-container');
+
+let availableStyles = {
+    fire: 'dashed red 3px',
+    water: 'double blue 5px',
+    earth: 'groove green 6px',
+    }
 
 //All functionalities used
 let functionalities = {
@@ -23,9 +33,18 @@ let functionalities = {
     addMemeText: function addMemeText() {
         memeTextBox.innerHTML = memeTextInput.value;
     },
+    changeStyle: {
+        fire: (event) => imageContainer.style.border = availableStyles['fire'],
+        water: (event) => imageContainer.style.border = availableStyles['water'],
+        earth: (event) => imageContainer.style.border = availableStyles['earth'],
+    },
 }
 
 //Commands
 imagegUplodButton.addEventListener('click', functionalities['selectImageFile']);
 memeTextInput.addEventListener('change', functionalities['addMemeText']);
-realImageUploadButton.addEventListener('change',functionalities['uploadImage']);
+realImageUploadButton.addEventListener('change',(event) => functionalities['uploadImage']);
+fireStyleButton.addEventListener('click', functionalities.changeStyle['fire']);
+waterStyleButton.addEventListener('click', functionalities.changeStyle['water']);
+earthStyleButton.addEventListener('click', functionalities.changeStyle['earth']);
+    
