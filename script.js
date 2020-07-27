b2a23ee9c4732868bf9be1c7e284e28453c4e873
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-07-27 17:48:32 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let memeFace = document.getElementById('titleImg');
+let textInput = document.getElementById('text-input');
+let memeText = document.getElementById('meme-text');
 
 memeFace.onmouseover = function () {
   document.getElementById('audio').play();
+}
+
+textInput.onkeyup = function () {
+  memeText.innerHTML = textInput.value;
 }
