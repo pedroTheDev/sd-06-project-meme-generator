10e389422538918e0d21b31bacc04c5bfbbc7dd1
cyranowebdev <cyrano@cyranowebdev.com>
2020-07-27 19:18:38 -0300

    ajustes e commit do último bônus.

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const textInput = document.querySelector('#text-input');
 const textPrint = document.querySelector('#meme-text');
 
 function loadImg(event) {
-  image.src = URL.createObjectURL(imageSelector.files[0]);
-  imageSelector.value = null;
+  if (event.target.nodeName === 'IMG') {
+    image.src = event.target.src;
+  } else {
+    image.src = URL.createObjectURL(event.target.files[0]);
+    imageSelector.value = null;
+  }
 }
 
 imageSelector.addEventListener('change', loadImg);
@@ -32,3 +36,13 @@ fireBtn.addEventListener('click', () => { changeBorder('3px dashed red') });
 waterBtn.addEventListener('click', () => { changeBorder('5px double blue') });
 earthBtn.addEventListener('click', () => { changeBorder('6px groove green') });
 resetBtn.addEventListener('click', () => { changeBorder('1px solid black') });
+
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
+
+meme1.addEventListener('click', loadImg);
+meme2.addEventListener('click', loadImg);
+meme3.addEventListener('click', loadImg);
+meme4.addEventListener('click', loadImg);
