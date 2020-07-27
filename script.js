957b5d3d2ebdd1f295c6267c555212a74c2d3396
Kesley Lahoz <namielahoz@gmail.com>
2020-07-27 19:32:14 -0300

    adding a css style button and meme-text

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 
 
 function showText() {
-  let show = document.querySelector('#text-input').value;
+  const show = document.querySelector('#text-input').value;
   document.querySelector('#meme-text').innerHTML = show;
 }
 
-var loadFile = function(event) {
-    var pic = document.querySelector('#meme-insert');
-    pic.src = url.createObjectURL(event.target.files[0]);
-    pic.onload = function() {
-        URL.revokeObjectURL(pic.src)
-    }
+let loadFile = function(event) {
+  let pic = document.querySelector('#meme-insert');
+  pic.src = url.createObjectURL(event.target.files[0]);
+  pic.onload = function() {
+    URL.revokeObjectURL(pic.src);
+  };
 };
 
-//options
+// options
 
-let options = document.querySelector('.options')
-var water = document.querySelector('#water')
-var fire = document.querySelector('#fire')
-var earth = document.querySelector('#earth')
-let memeBox = document.querySelector('#meme-image-container')
+const options = document.querySelector('.options');
+const water = document.querySelector('#water');
+const fire = document.querySelector('#fire');
+const earth = document.querySelector('#earth');
+const memeBox = document.querySelector('#meme-image-container');
 
 options.addEventListener('click', function(event) {
-    if (event.target === water) {
-        memeBox.style.borderColor = "blue"
-        memeBox.style.borderStyle = "double"
-    }else if (event.target === fire) {
-        memeBox.style.borderColor = "red"
-        memeBox.style.borderStyle = "double"
-    }else if (event.target === earth) {
-        memeBox.style.borderColor = "green"
-        memeBox.style.borderStyle = "double"
-    }
-}); 
+  if (event.target === water) {
+    memeBox.style.borderColor = 'blue';
+    memeBox.style.borderStyle = 'double';
+  } else if (event.target === fire) {
+    memeBox.style.borderColor = 'red';
+    memeBox.style.borderStyle = 'double';
+  } else if (event.target === earth) {
+    memeBox.style.borderColor = 'green';
+    memeBox.style.borderStyle = 'double';
+  }
+});
