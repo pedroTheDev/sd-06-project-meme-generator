2cb00fade1bb7dce2d70a4173806310fc5ce4c93
Pedro <pedroedu77@gmail.com>
2020-07-27 18:45:46 -0300

    Segundos requisitos do projeto concluidos

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// Seção criada para os Containers e para as Variáveis
 const frase = document.getElementById('text-input');
 const fraseNaImagem = document.getElementById('meme-text');
+const image = document.getElementById('meme-image');
+const imageInsert = document.getElementById('meme-insert');
+// Seção para as Funções e Eventos
 function getPhrase() {
   fraseNaImagem.innerHTML = frase.value;
 }
 frase.addEventListener('keyup', getPhrase);
+function getImage() {
+  image.src = URL.createObjectURL(imageInsert.files[0]); 
+}
+imageInsert.addEventListener("change", getImage);
