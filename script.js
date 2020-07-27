b6044cf87923fb530808fd8252bac89481eaa3b4
Anderson <and.stuber@gmail.com>
2020-07-27 13:26:00 -0300

    CodeClimate fixes

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-window.onload = () => {
-  console.log('carregou')
-  insertMemeText();
-  imageUploadURL();
-}
-
 function insertMemeText() {
   const insertTextField = document.getElementById('text-input');
   const memeText = document.getElementById('meme-text');
@@ -12,20 +6,21 @@ function insertMemeText() {
       memeText.innerText = insertTextField.value;
       insertTextField.value = '';
     }
-  })
+  });
 }
 
 function imageUploadURL() {
-  let reader = new FileReader();
   const memeImageUpload = document.getElementById('meme-insert');
   memeImageUpload.addEventListener('change', () => {
-    //console.log(memeImageUpload.files[0])
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.onload = (e) => {
       document.getElementById('meme-image').src = e.target.result;
     };
-    reader.readAsDataURL(memeImageUpload.files[0])
-
-  })
+    reader.readAsDataURL(memeImageUpload.files[0]);
+  });
 }
 
+window.onload = () => {
+  insertMemeText();
+  imageUploadURL();
+}
