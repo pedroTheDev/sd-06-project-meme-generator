1395aad5e0be284a03865277e388500103dff238
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-27 17:43:27 -0300

    Bônus 6. Add bordas

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const btnFire = document.querySelector("#fire");
+const btnWater = document.querySelector("#water");
+const btnEarth = document.querySelector("#earth");
+const container = document.querySelector("#meme-image-container");
 
 function textWrite() {
   const texto = document.querySelector('#text-input').value;
@@ -12,3 +16,22 @@ function selectedImage(event) {
     URL.revokeObjectURL(memeImage.src);
   };
 }
+
+function changeFire () {  
+  container.style.border="3px dashed red";
+  console.log(container);
+}
+
+function changeWater () {  
+  container.style.border="5px double blue";
+  console.log(container);
+}
+
+function changeEarth () {  
+  container.style.border="6px groove green";
+  console.log(container);
+}
+
+btnFire.addEventListener('click', changeFire);
+btnWater.addEventListener('click', changeWater);
+btnEarth.addEventListener('click', changeEarth);
