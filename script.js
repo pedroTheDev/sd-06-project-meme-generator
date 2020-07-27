93b30d42e529e863c5135a7f65260ef6f37516e0
Isadora Koga <isadorakoga710@gmail.com>
2020-07-27 17:43:36 -0300

    Arrumar input texto

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function textInput() {
-    // Função que vai fazer o texto aparecer no meme
-    let inputText = document.getElementById("text-input").value;
-    let divMemeText = document.getElementById("meme-text");
-    divMemeText.innerHTML = inputText;
+  // Função que vai fazer o texto aparecer no meme
+  let inputText = document.getElementById('text-input').value;
+  let divMemeText = document.getElementById('meme-text');
+  return divMemeText.innerHTML = inputText;
 }
 
 function imageInput() {
-    // Função que vai fazer a imagem aparecer no meme
-    let outputImage = document.getElementById("meme-image");
-    outputImage.src = URL.createObjectURL(event.target.files[0]);
-    outputImage.onload = function() {
-        URL.revokeObjectURL(outputImage.src);
-    }
+  // Função que vai fazer a imagem aparecer no meme
+  const outputImage = document.getElementById('meme-image');
+  outputImage.src = URL.createObjectURL(event.target.files[0]);
+  outputImage.onload = function() {
+    URL.revokeObjectURL(outputImage.src);
+  };
 }
