2cadae03b9daba91ca00cc967081017e885abc2a
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-27 18:24:27 -0300

    Adicionados 3 botões para estilizar a borda do meme gerado.

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const memeText = document.querySelector('#meme-text');
 
 textInput.addEventListener('input', function() {
   memeText.innerText = textInput.value;
-  console.log(memeInput.value);
 });
 
-const memeInput = document.querySelector('#meme-input');
+const memeInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
 
-memeInput.addEventListener('change', function(path) {
+memeInsert.addEventListener('change', function(path) {
   memeImage.src = URL.createObjectURL(path.target.files[0]);
+});
+
+const memeBorder = document.querySelector('#meme-image-container');
+const fireButton = document.querySelector('#fire');
+const waterButton = document.querySelector('#water');
+const earthButton = document.querySelector('#earth');
+
+fireButton.addEventListener('click', function() {
+  memeBorder.style.border = '3px dashed red';
+});
+waterButton.addEventListener('click', function() {
+  memeBorder.style.border = '5px double blue';
+});
+earthButton.addEventListener('click', function() {
+  memeBorder.style.border = '6px groove green';
 });
