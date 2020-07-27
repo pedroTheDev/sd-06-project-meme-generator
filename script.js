e4d57f3caf99a1cd00a99c7ae035d50ebcaf14b3
FPhilipi <fabiphilipi@gmail.com>
2020-07-27 12:34:14 -0300

    Requisitos

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let oi = 0;
+window.onload = function() {
+let imageInPut = document.querySelector("#meme-insert");
+
+imageInPut.addEventListener("change", function(event) {
+    document.getElementById("meme-image").src = 
+    URL.createObjectURL(event.target.files[0]);
+    // imageOutPut.src = imageInPut.files[0].name;
+
+})
+
+}
