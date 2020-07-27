5c1b37a52edca342bc810af15573c96e0fbba704
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-07-27 19:27:08 -0300

    teste commit

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-// //function previewImagem() {
-//     let imagem = document.querySelector('input[name=imagem]').files[0];
+// function previewImagem() {
+//     let imagem = document.querySelector('#imagem').files[0];
 //     let preview = document.querySelector('img');
+//     let text = document.querySelector('#text-input')
+//     let memeText = document.querySelector("")
 
-//     let reader = new FileReader();
+//     function Prhase(){
 
-//     reader.onloadend = function() {
-//         preview.src = reader.result;
+
 //     }
 
-//     if (imagem) {
-//         reader.readAsDataURL(imagem);
-//     } else {
-//         preview.src = "";
+
+//     function handleFiles(files) {
+//         for (let i = 0; i < files.length; i++) {
+//             const file = files[i];
+
+//             if (!file.type.startsWith('image/')) { continue }
+
+//             const img = document.createElement("img");
+//             img.classList.add("obj");
+//             img.file = file;
+//             preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
+
+//             const reader = new FileReader();
+//             reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+//             reader.readAsDataURL(file);
+//         }
 //     }
-// }
