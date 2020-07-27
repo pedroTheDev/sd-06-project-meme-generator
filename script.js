d4fc9f5883af38cbbc5f6c78a3d3387cf72cbd3f
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-07-27 10:38:06 -0300

    Bônus 6

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const DQSEL = function (params) { return document.querySelector(params); };
-const INPUTXT = DQSEL('.input-text');
+const INPUTXT = DQSEL('#text-input');
 const INPUTIMG = DQSEL('#meme-insert');
 const MEMEIMG = DQSEL('.meme-img');
 const MEMETXT = DQSEL('#meme-text');
-
-function getInputText() {
-  return document.querySelector('#text-input').value;
-}
+const DIVIMG = DQSEL('.image-container');
+const BORDERBTN = document.querySelectorAll('.border-btn');
+const BORDERS = ['3px dashed red', '5px double blue', '6px groove green'];
 
 INPUTXT.oninput = () => {
-  MEMETXT.textContent = getInputText();
+  MEMETXT.textContent = INPUTXT.value;
 };
 
 INPUTIMG.oninput = () => {
   MEMEIMG.src = URL.createObjectURL(INPUTIMG.files[0]);
+};
+
+BORDERBTN[0].onclick = () => {
+  DIVIMG.style.border = BORDERS[0];
+};
+
+BORDERBTN[1].onclick = () => {
+  DIVIMG.style.border = BORDERS[1];
+};
+
+BORDERBTN[2].onclick = () => {
+  DIVIMG.style.border = BORDERS[2];
 };
