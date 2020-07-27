61fc96f6afd7ea5d5e107dc63671485453645f80
Anderson <and.stuber@gmail.com>
2020-07-27 10:48:46 -0300

    meme insert text function done

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 window.onload = () => {
     console.log('carregou')
+    insertMemeText();
+}
+
+
+function insertMemeText() {
+    const insertTextField = document.getElementById('text-input');
+    const memeText = document.getElementById('meme-text');
+    console.log(insertTextField.innerText)
+    insertTextField.addEventListener('keypress', (key) => {
+        if (key.key === 'Enter') {
+            memeText.innerText = insertTextField.value;
+            insertTextField.value = '';
+        }
+    })
 }
