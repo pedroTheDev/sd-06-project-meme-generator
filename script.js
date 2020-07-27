55b73c2b844ea0ff9fafb674980eb44b2a883a06
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-27 19:03:25 -0300

    Tentando matar umas issues

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
 const texto = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const memeImage = document.getElementById('meme-image');
-const memeInput = document.querySelector("#meme-insert");
-const btnFire = document.querySelector("#fire");
-const btnWater = document.querySelector("#water");
-const btnEarth = document.querySelector("#earth");
+const memeInput = document.querySelector('#meme-insert');
+const btnFire = document.querySelector('#fire');
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
 const container = document.querySelector("#meme-image-container");
 
-const memeUm = document.getElementById("meme-1");
-const memeDois = document.getElementById("meme-2");
-const memeTres = document.getElementById("meme-3");
-const memeQuatro = document.getElementById("meme-4");
+const memeUm = document.getElementById('meme-1');
+const memeDois = document.getElementById('meme-2');
+const memeTres = document.getElementById('meme-3');
+const memeQuatro = document.getElementById('meme-4');
 
-function textWrite() {  
+function textWrite() {
   memeText.innerHTML = texto.value;
 }
 
-function selectedImage(event) {  
+function selectedImage(event) {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
   memeImage.onload = function () {
     URL.revokeObjectURL(memeImage.src);
   };
 }
 
-function changeFire () {  
-  container.style.border="3px dashed red";
-  console.log(container);
+function changeFire() {
+  container.style.border = '3px dashed red';
 }
 
-function changeWater () {  
-  container.style.border="5px double blue";
+function changeWater() {
+  container.style.border = '5px double blue';
 }
 
-function changeEarth () {  
-  container.style.border="6px groove green";
-  console.log(container);
+function changeEarth() {
+  container.style.border = '6px groove green';
 }
 
-function trocaMemeUm () {
+function trocaMemeUm() {
   memeImage.src = memeUm.src;
 }
 
-function trocaMemeDois () {
+function trocaMemeDois() {
   memeImage.src = memeDois.src;
 }
 
-function trocaMemeTres () {
+function trocaMemeTres() {
   memeImage.src = memeTres.src;
 }
 
-function trocaMemeQuatro () {
+function trocaMemeQuatro() {
   memeImage.src = memeQuatro.src;
 }
 
