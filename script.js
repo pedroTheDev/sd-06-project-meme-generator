4c42917bcd8efe21366418cb7e9f77b1fe25f2d5
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-27 16:52:24 -0300

    exercicio 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputTexto = document.querySelector('#text-input');
 const outputTexto = document.querySelector('#meme-text');
 
-inputTexto.addEventListener('input', (e) => {
-  outputTexto.innerHTML = e.target.value;
+inputTexto.addEventListener('input', function() {
+outputTexto.innerHTML = inputTexto.value;
 });
