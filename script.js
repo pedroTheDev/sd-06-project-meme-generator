7909c42f04bceb2e50fe15e4f402238d04dfb314
RenatoCostaDev <renatotrybe@gmail.com>
2020-07-27 18:35:13 -0300

    03- montando estrutura js fotos

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function digitadoTxt() {
   txtImage.innerHTML = txtDigitado.value;
 }
 txtDigitado.addEventListener('keyup', digitadoTxt);
+
+const inputImagem = document.querySelector('#meme-insert');
+function visualizarImg(event) {
+  const buscaImg = document.querySelector('img');
+  buscaImg.src = URL.createObjectURL(event.target.files[0]);
+  buscaImg.onload = function () {
+    URL.revokeObjectURL(buscaImg.src);
+  }
+}
+inputImagem.addEventListener('change', visualizarImg);
