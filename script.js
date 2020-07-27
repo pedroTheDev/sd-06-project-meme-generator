7e8c39ec43ee469dbad3ad8776a953c46f7b1b4e
Anderson R <misterregisslv@gmail.com>
2020-07-27 16:24:57 -0300

    atualizando html css e javascript tarefa 1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//subindo
+window.onload = function() {
+
+     let campotexto = document.querySelector('#text-input');
+     console.log(campotexto);
+   
+    campotexto.addEventListener('change', function () {
+        let conteudotexto = document.querySelector('#text-input').value;
+        let tagP = document.createElement('p');
+        tagP.innerHTML = conteudotexto
+        document.querySelector('#meme-text').appendChild(tagP);
+    });
+
+
+     // let tagP = document.createElement('p');
+    // tagP.innerHTML = conteudotexto
+}
