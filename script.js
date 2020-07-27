dc16108f3d15e2502762f6951b6202b311bb5375
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-27 17:45:44 -0300

    img container border and bgcolor

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-const imgButton = document.querySelector('#meme-insert');
+// const imgButton = document.querySelector('#meme-insert');
 const image = document.querySelector('#meme-image');
 
 textInput.addEventListener('keyup', function () {
   memeText.innerText = textInput.value.toUpperCase();
 });
 
-const loadFile = function(event) {
+loadFile = function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
   image.onload = function() {
-    URL.revokeObjectURL(image.src)
-  }
-}
+    URL.revokeObjectURL(image.src);
+  };
+};
