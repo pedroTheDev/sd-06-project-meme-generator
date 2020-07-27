649bd874daf70dbff6204986b28f7933028aa6fa
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-27 18:26:45 -0300

    Upload image function done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // document.getElementById('meme-text').innerHTML = document.getElementsByTagName('input')[0].value;
+
+let uploadImg = document.getElementById('meme-insert');
+uploadImg.addEventListener('change', function() {
+  // console.log(event);
+  let img = document.createElement('img');
+  img.src = URL.createObjectURL(event.target.files[0]); // URL.createObjectURL(this.files[0]) tbm funciona
+  img.width = '300';
+  img.height = '300';
+  let insertImg = document.getElementById('meme-image');
+  insertImg.appendChild(img);
+});
