9b77eb3e03ced81dbddcfae6f92edf8bf5e0bb44
Alessandra <itsalematos@hotmail.com>
2020-07-27 15:41:58 -0400

    Corrigindo requisitos 1 e 2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//commitando arquivo
+var capturando = "";
+
+function capturar() {
+    capturando = document.getElementById('text-input').value;
+    document.getElementById('meme-text').innerHTML = capturando;
+}
+
+// function capturar() {
+//     const textInput = document.getElementById('text-input');
+//     const memeText = document.getElementById('meme-text');
+//     textInput.addEventListener('input', () => {
+//       memeText.innerText = textInput.value;
+//     });
+//   }
