9850d512f9eb135ea7a9a95b201d4b16b2378cd2
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-27 17:45:41 -0300

    CC

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 window.onload = function () {
-  let textInput = document.querySelector("#text-input");
-  let textContainer = document.querySelector("#meme-text");
-  function addText () {
+  const textInput = document.querySelector('#text-input');
+  const textContainer = document.querySelector('#meme-text');
+  function addText() {
     if (textInput.value.length <= 60) {
       textContainer.innerText = textInput.value;
     } else {
-      alert("You exceeded the maximum of 60 characters ")
+      alert('You exceeded the maximum of 60 characters ');
     }
   }
-  textInput.addEventListener("input", addText);
+  textInput.addEventListener('input', addText);
 };
-
-console.log(textInput);
