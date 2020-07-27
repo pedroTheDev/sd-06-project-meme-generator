065357fe6440a35dfe3dca38ee0febbf2d2f471b
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-07-27 19:04:47 -0300

    requisito 2, mas o 1 desfez kkkk

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let memeFace = document.getElementById('titleImg');
 let textInput = document.getElementById('text-input');
 let memeText = document.getElementById('meme-text');
-
-memeFace.onmouseover = function () {
-  document.getElementById('audio').play();
-}
+let memeImage = document.getElementById('meme-insert');
 
 textInput.onkeyup = function () {
   memeText.innerHTML = textInput.value;
+}
+
+let loadFile = function(event) {
+  let output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src)
+  }
 }
