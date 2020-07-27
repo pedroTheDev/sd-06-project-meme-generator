51db290f44067f831b536e81d108d224fc04374b
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-27 17:27:37 -0300

    Verificando o bonus 6

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ window.onload = function() {
 
   let text = document.querySelector("#text-input");
   let memeText = document.querySelector("#meme-text");
+  let memeCont = document.querySelector("#meme-image-container");
+  let fire = document.querySelector("#fire");
+  let water = document.querySelector("#water");
+  let earth = document.querySelector("#earth");
 
   text.addEventListener("keyup", function() {
     memeText.innerText = text.value;
   });
 
+  fire.addEventListener("click", function() {
+    memeCont.style.border = "3px dashed red";
+  });
   
+  water.addEventListener("click", function() {
+    memeCont.style.border = "5px double blue";
+  });
+
+  earth.addEventListener("click", function() {
+    memeCont.style.border = "6px groove green";
+  });
 
 }
 
