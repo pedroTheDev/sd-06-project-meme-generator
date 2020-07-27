81d54cbd6b6142d3b40df6a4906c340ab2ef4ebc
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-27 17:24:58 -0300

    Adicionada função que usa o valor do texto inserido e coloca sobre a imagem exibida.

---
+++ script.js
@@ -0,0 +1,6 @@
+const textInput = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+
+textInput.addEventListener('input', function() {
+  memeText.innerText = textInput.value;
+});
