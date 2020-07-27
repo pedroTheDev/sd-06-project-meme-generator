2be39c827d6867dd6cc54866f4112992c9270d7f
Kesley Lahoz <namielahoz@gmail.com>
2020-07-27 18:47:41 -0300

    Incluido button

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 
 
-
 function showText() {
-    let show = document.querySelector("#text-input").value;
-    document.querySelector("#meme-text").innerHTML = show
+  let show = document.querySelector('#text-input').value;
+  document.querySelector('#meme-text').innerHTML = show;
 }
+
+var loadFile = function(event) {
+    var pic = document.querySelector('#meme-insert');
+    pic.src = url.createObjectURL(event.target.files[0]);
+    pic.onload = function() {
+        URL.revokeObjectURL(pic.src)
+    }
+};
+
+//options
+
+let options = document.querySelector('.options')
+var water = document.querySelector('#water')
+var fire = document.querySelector('#fire')
+var earth = document.querySelector('#earth')
+let memeBox = document.querySelector('#meme-image-container')
+
+options.addEventListener('click', function(event) {
+    if (event.target === water) {
+        memeBox.style.borderColor = "blue"
+        memeBox.style.borderStyle = "double"
+    }else if (event.target === fire) {
+        memeBox.style.borderColor = "red"
+        memeBox.style.borderStyle = "double"
+    }else if (event.target === earth) {
+        memeBox.style.borderColor = "green"
+        memeBox.style.borderStyle = "double"
+    }
+}); 
