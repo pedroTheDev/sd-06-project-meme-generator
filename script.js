e9783fe103c0ba56c0a8a4589310c870e31bc938
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-27 15:11:38 -0300

    Task 2: upload image functionality

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ window.onload = function () {
 //Referencing elements
 let inputTextElement = document.querySelector('#text-input');
 let memeTextBox = document.querySelector('#meme-text');
+let inputImageElement =document.querySelector('#meme-insert');
+let memeImage = document.querySelector('#meme-image');
 
 //Functions
 let functionalities = {
     addText: (event) => memeTextBox.innerHTML = inputTextElement.value,
+    uploadImage: (event) => memeImage.src = window.URL.createObjectURL(inputImageElement.files[0]),
     }
 
 
 //Events
 inputTextElement.addEventListener('keyup', functionalities['addText']);
+inputImageElement.addEventListener('change', functionalities['uploadImage']);
+console.log(inputImageElement.value)
 }
