b97fd9d1fab155c023ca074575b0f0256603e922
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-27 19:35:50 -0300

    Added style for the text and image preview

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
   const textContainer = document.querySelector('#meme-text');
   const imgInput = document.querySelector('#meme-insert');
   const imgContainer = document.querySelector('#meme-image');
-  
+
   function addText() {
     if (textInput.value.length <= 60) {
       textContainer.innerText = textInput.value;
@@ -11,5 +11,10 @@ window.onload = function () {
       alert('You exceeded the maximum of 60 characters ');
     }
   }
+
+  function addImage(event) {
+    imgContainer.src = URL.createObjectURL(event.target.files[0]);
+  }
   textInput.addEventListener('input', addText);
+  imgInput.addEventListener('change', addImage);
 };
