14a65a659f46c239836c8e6c386f9b536ca86dd9
Isadora Koga <isadorakoga710@gmail.com>
2020-07-27 16:55:06 -0300

    Arrumando CSS

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let divMemeGenerator = document.getElementById("meme-image-container");
-
 function textInput() {
     // Função que vai fazer o texto aparecer no meme
     let inputText = document.getElementById("text-input").value;
-    console.log(inputText);
-    divMemeGenerator.innerHTML = inputText;
+    let divMemeText = document.getElementById("meme-text");
+    divMemeText.innerHTML = inputText;
 }
 
 function imageInput() {
     // Função que vai fazer a imagem aparecer no meme
     let inputImage = document.getElementById("meme-insert").value;
+    let divMemeGenerator = document.getElementById("meme-image-container");
     divMemeGenerator.innerHTML = `<img src="${inputImage}" alt="meme">`;
     console.log(inputImage);
 }
