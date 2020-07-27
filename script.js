4aab60966be878821ff2cf165ec079f249c7d607
VinicinBraga <vinicius@bragamatta.com.br>
2020-07-27 17:09:35 -0300

    primeiro teste para ver se esta correto - (sem imagem e sem style)

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-!
+window.onload = function(){
+    let textInput = document.querySelector("#text-input");
+    textInput.addEventListener("keyup", function(){
+        let text = document.querySelector("#meme-text");
+        text.innerHTML = textInput.value;
+    })
+  
