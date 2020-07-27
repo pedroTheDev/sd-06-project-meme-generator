d22f8cf7f6ba24a39d256399dacf7897a7e2baa3
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-27 18:27:26 -0300

    Desafio 2

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ textImput.addEventListener('keyup', function () {
 });
 
 const loadFile = function (event) {
-  const output = document.getElementById('output');
+  const output = document.getElementById('meme-insert');
   output.src = URL.createObjectURL(event.target.files[0]);
   output.style.maxHeight = '200px';
   output.style.maxWidth = '200px';
