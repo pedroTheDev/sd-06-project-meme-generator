d5aec5f2fb088869ab4ae7c60fd0f1076bc8af41
FlavioSugano <flavio.sugano@gmail.com>
2020-07-27 19:20:43 -0300

    ordenação dos comandos no CSS, inclusão de comando no script p/ aparecer o texto na imagem

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.onload = function() {
 
-  
+  // insere o texto no container conforme é digitado
+  let textInput = document.querySelector('#text-input');
+  textInput.addEventListener('change', function () {
+    document.querySelector('#meme-text').innerHTML = textInput.value;
+  })
 
 
 }
