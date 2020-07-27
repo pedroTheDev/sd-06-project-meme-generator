2371284529372824ce83027b980d4202e2a71ae2
MCoutinho <matheuscavalcante88@hotmail.com>
2020-07-27 16:08:28 -0300

    image container, text CSS added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const textInput = document.querySelector('#text-input');
 const imgInput = document.querySelector('#meme-insert');
+const memeImg = document.querySelector('#meme-image');
 const memeText = document.querySelector('#meme-text');
 
 textInput.onchange = () => {
-  memeText.innerHTML = textInput.value;
+  memeText.textContent = textInput.value;
+};
+
+imgInput.onchange = (event) => {
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
 };
