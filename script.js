21a6588e5c20a67c3fd3c9884b1d80769a8e187c
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-27 19:16:07 -0300

    Added image input and constants

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onload = function () {
   const textInput = document.querySelector('#text-input');
   const textContainer = document.querySelector('#meme-text');
+  const imgInput = document.querySelector('#meme-insert');
+  const imgContainer = document.querySelector('#meme-image');
+  
   function addText() {
     if (textInput.value.length <= 60) {
       textContainer.innerText = textInput.value;
