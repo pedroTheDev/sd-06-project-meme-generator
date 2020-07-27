c457a28202ed645771c4b5ece7ea0587a8b8061a
MCoutinho <matheuscavalcante88@hotmail.com>
2020-07-27 17:07:45 -0300

    add border buttons

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const textInput = document.querySelector('#text-input');
 const imgInput = document.querySelector('#meme-insert');
 const memeImg = document.querySelector('#meme-image');
 const memeText = document.querySelector('#meme-text');
+const imgContainer = document.querySelector('.img-container');
+const borderButtons = document.querySelectorAll('.border-button');
+
+const borders = {
+  fire: '3px dashed red',
+  water: '5px double blue',
+  earth: '6px groove green',
+};
 
 textInput.oninput = () => {
   memeText.textContent = textInput.value;
@@ -9,4 +17,16 @@ textInput.oninput = () => {
 
 imgInput.oninput = (event) => {
   memeImg.src = URL.createObjectURL(event.target.files[0]);
+};
+
+borderButtons[0].onclick = () => {
+  imgContainer.style.border = borders.fire;
+};
+
+borderButtons[1].onclick = () => {
+  imgContainer.style.border = borders.water;
+};
+
+borderButtons[2].onclick = () => {
+  imgContainer.style.border = borders.earth;
 };
