cecdeb5bdc459cd80d8a06011aa9e41043013867
jederson <jmacedo.ti@gmail.com>
2020-07-27 14:20:50 -0300

    03 incompleto

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ window.onload = function() {
   });
   
   let memeIsert = document.querySelector('#meme-insert');
-  let memeImg = document.querySelector('meme-image');
+  let memeImg = document.querySelector('#meme-image');
+  
 
   memeIsert.addEventListener('change', function(event){
     memeImg.src = URL.createObjectURL(event.target.files[0]);
