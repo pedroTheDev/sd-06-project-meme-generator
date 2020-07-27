35cb3ecfa172165206c02d8340adbba723c0a5df
Keila Sales <k.f.saless@gmail.com>
2020-07-27 20:52:28 -0300

    Desafio 3

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ let memeText = document.getElementById("meme-text");
 let textInput = document.getElementById("text-input");
 let memeImage = document.getElementById("meme-image");
 let memeInsert = document.getElementById("meme-insert");
+
 //Eventos
 textInput.addEventListener('keyup', escreverTexto);
-
 function escreverTexto() {
     memeText.innerHTML = textInput.value;
 }
 
 
 memeInsert.addEventListener('change', adicionandoFoto);
-
 function adicionandoFoto() {
     memeImage.src = window.URL.createObjectURL(memeInsert.files[0]);
    
