63895c1b5dee4e7dc59db5c4425cdc9ef5ebd92c
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-27 17:54:31 -0300

    Req. 6: iniciando JS para os botoes

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ let memeTextInput = document.querySelector("#meme-text");
 userTextInput.addEventListener("input", function(){
     memeTextInput.innerText = userTextInput.value;
 })
+
+// Req 6
+let personalizeSection = document.querySelector(".personalize-section");
+let fireButton = document.querySelector("#fire");
+let waterButton = document.querySelector("#water");
+let earthButton = document.querySelector("#earth");
+
+personalizeSection.addEventListener("click", function(event){
+    if (event.target == fireButton){
+        console.log("fire");
+    } else if (event.target == waterButton){
+        console.log("water");
+    } else if (event.target == earthButton){
+        console.log("earth");
+    }
+})
