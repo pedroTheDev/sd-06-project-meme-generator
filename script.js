2e41a0fcffbb5f4da6e351ee4043252da71143d5
Isadora Koga <isadorakoga710@gmail.com>
2020-07-27 16:42:00 -0300

    Adicionando CSS e funções

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-textInput() {
-    //Função que vai fazer o texto aparecer no meme
+let divMemeGenerator = document.getElementById("meme-image-container");
+
+function textInput() {
+    // Função que vai fazer o texto aparecer no meme
+    let inputText = document.getElementById("text-input").value;
+    console.log(inputText);
+    divMemeGenerator.innerHTML = inputText;
 }
 
-imageInput() {
-    //Função que vai fazer a imagem aparecer no meme
+function imageInput() {
+    // Função que vai fazer a imagem aparecer no meme
+    let inputImage = document.getElementById("meme-insert").value;
+    divMemeGenerator.innerHTML = `<img src="${inputImage}" alt="meme">`;
+    console.log(inputImage);
 }
