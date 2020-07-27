6bcc970227c246d3ffae53e740f22e8bafa53213
Pedro Risso <pmerchiorattor@gmail.com>
2020-07-27 13:06:26 -0300

    dealing with text input

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const selectedFile = document.getElementById('image-input');
-const btnGenerateMeme = document.getElementById('btn-generate-meme');
-const memeText = document.getElementById('text-input');
-let memeImage =  document.createElement('img');
-let memeSection = document.querySelector('#meme-text');
+const SELECTEDFILE = document.getElementById('image-input');
+const TEXTINPUT = document.getElementById('text-input');
+const MEMEIMAGE =  document.createElement('img');
+const MEMETEXTSECTION = document.querySelector('#meme-text');
 
-selectedFile.addEventListener('change', handleFiles, false);
+SELECTEDFILE.addEventListener('change', handleFiles, false);
 function handleFiles() {
   const fileList = this.files;
   if (fileList.length > 1) {
     alert('Only one file at a time')
   } else {
-    memeImage.src = window.URL.createObjectURL(fileList[0]);
-    document.getElementById('meme-image-container').appendChild(memeImage)
+    MEMEIMAGE.src = window.URL.createObjectURL(fileList[0]);
+    MEMEIMAGE.id = 'meme-image';
+    MEMEIMAGE.className = 'meme-image';
+    document.getElementById('meme-image-container').appendChild(MEMEIMAGE)
   }
+}
+
+TEXTINPUT.addEventListener('change', updateText);
+function updateText(e) {
+  MEMETEXTSECTION.textContent = e.target.value;
 }
