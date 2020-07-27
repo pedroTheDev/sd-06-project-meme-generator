762269c769d89ca2bc62c13a4f7b284273208314
Anderson R <misterregisslv@gmail.com>
2020-07-27 17:58:37 -0300

    atualizando html css e javascript tarefa 1 quarta vez

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ window.onload = function() {
     // função para inserir texto no meme
      let campotexto = document.querySelector('#text-input');
      console.log(campotexto);
-   
-    campotexto.addEventListener('change', function () {
+     let tagP = document.createElement('p');
+    campotexto.addEventListener('keyup', function () {
         let conteudotexto = document.querySelector('#text-input').value;
-        let tagP = document.createElement('p');
         tagP.innerHTML = conteudotexto
         document.querySelector('#meme-text').appendChild(tagP);
     });
