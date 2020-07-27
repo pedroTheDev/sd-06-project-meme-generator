066716465182c64abbc1ae2590749d188fd629ae
Pedro Risso <pmerchiorattor@gmail.com>
2020-07-27 13:26:27 -0300

    changed sequence to meet code climate requirements

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const SELECTEDFILE = document.getElementById('image-input');
+const SELECTEDFILE = document.getElementById('meme-insert');
 const TEXTINPUT = document.getElementById('text-input');
 const MEMEIMAGE =  document.createElement('img');
 const MEMETEXTSECTION = document.querySelector('#meme-text');
 
-SELECTEDFILE.addEventListener('change', handleFiles, false);
 function handleFiles() {
   const fileList = this.files;
   if (fileList.length > 1) {
-    alert('Only one file at a time')
+    alert('Only one file at a time');
   } else {
     MEMEIMAGE.src = window.URL.createObjectURL(fileList[0]);
     MEMEIMAGE.id = 'meme-image';
     MEMEIMAGE.className = 'meme-image';
-    document.getElementById('meme-image-container').appendChild(MEMEIMAGE)
+    document.getElementById('meme-image-container').appendChild(MEMEIMAGE);
   }
 }
 
-TEXTINPUT.addEventListener('change', updateText);
 function updateText(e) {
   MEMETEXTSECTION.textContent = e.target.value;
 }
+
+SELECTEDFILE.addEventListener('change', handleFiles, false);
+
+TEXTINPUT.addEventListener('change', updateText);
