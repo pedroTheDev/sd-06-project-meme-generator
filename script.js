b624ad80c3daad0c0e385f9d641bce276e524330
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-27 20:32:52 -0300

    mandatory requirements done

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// document.getElementById('meme-text').innerHTML = document.getElementsByTagName('input')[0].value;
-
+// 
 let uploadImg = document.getElementById('meme-insert');
 uploadImg.addEventListener('change', function() {
-  // console.log(event);
+  console.log(event);
   let img = document.createElement('img');
   img.src = URL.createObjectURL(event.target.files[0]); // URL.createObjectURL(this.files[0]) tbm funciona
   img.width = '300';
   img.height = '300';
   let insertImg = document.getElementById('meme-image');
   insertImg.appendChild(img);
+});
+
+let text = document.querySelector('#text-input');
+text.addEventListener('change', function() {
+  document.getElementById('meme-text').innerHTML = document.querySelector('#text-input').value;
 });
