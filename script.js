862d5288a5cafce38e1dbf572f329ca527ff6d38
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-27 17:56:44 -0300

    Criada função que utiliza a imagem inserida para colocar no campo de exibição do meme.

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ const memeText = document.querySelector('#meme-text');
 
 textInput.addEventListener('input', function() {
   memeText.innerText = textInput.value;
+  console.log(memeInput.value);
+});
+
+const memeInput = document.querySelector('#meme-input');
+const memeImage = document.querySelector('#meme-image');
+
+memeInput.addEventListener('change', function(path) {
+  memeImage.src = URL.createObjectURL(path.target.files[0]);
 });
