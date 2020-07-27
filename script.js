211c95571030203b2d87352176064840669268e8
Pedro <pedroedu77@gmail.com>
2020-07-27 17:27:35 -0300

    Primeiros requisitos corrigidos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let frase = document.getElementById("text-input"); 
-let fraseNaImagem = document.getElementById("meme-text");
-function getPhrase(){
+const frase = document.getElementById('text-input');
+const fraseNaImagem = document.getElementById('meme-text');
+function getPhrase() {
   fraseNaImagem.innerHTML = frase.value;
 }
-frase.addEventListener("keyup",getPhrase);
+frase.addEventListener('keyup', getPhrase);
