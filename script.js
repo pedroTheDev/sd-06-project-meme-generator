c8429fd5d3eca2828c3157f4eac557ff396b33f4
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-27 20:03:36 -0300

    alterações de codeclimate

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const loadFile = function (event) {
   output.src = URL.createObjectURL(event.target.files[0]);
   output.onload = function () {
     URL.revokeObjectURL(output.src);
-  }
+  };
 };
 
 addEventListener('input', function insertText() {
   const text = document.getElementById('text-input');
   document.getElementById('meme-text').innerHTML=text.value;
-})
+});
