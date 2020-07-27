c05088d7d24126894ca6a533939793ed82c801ee
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-07-27 19:56:22 -0300

    Edição do código dos arquivos html, css e js para atender as expecificações solicitadas

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const memeTxt = document.createElement('p');
-memeTxt.textContent = 'Teste';
-document.querySelector('#meme-image').appendChild(memeTxt);
+const textInput = document.querySelector('#text-input');
+const memeText = document.getElementById('meme-text');
+function inputText () {
+    memeText.innerText = textInput.value;
+}
+textInput.addEventListener('keyup', inputText);
+const imageChoose = document.querySelector('#meme-insert');
+const imageInput = document.querySelector('#meme-image');
+function image() {
+    imageInput.src = window.URL.createObjectURL(imageChoose.files[0]);
+}
+imageChoose.addEventListener('change', image);
