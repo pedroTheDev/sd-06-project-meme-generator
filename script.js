3127fbcf5187383a800884d2661141d1a0ad4933
Daniele Perse <danieleperse3@gmail.com>
2020-07-26 21:27:06 -0300

    funçoes javascript criadas

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+window.onload = function() {    
+    let textoInput = document.getElementById('text-input');
+    textoInput.addEventListener('keyup', teclado); 
+
+    let memeInsert = document.getElementById('meme-insert');
+    memeInsert.addEventListener('change', addImagem);
+}
+
 function teclado() {
     let texto = document.getElementById('meme-text');
     let textoInput = document.getElementById('text-input');
     texto.innerHTML = textoInput.value;
+}
+
+function addImagem() {
+    let memeInsert = document.getElementById('meme-insert');
+    let memeImage = document.getElementById('meme-image');
+    memeImage.src = URL.createObjectURL(memeInsert.files[0]);
 }
