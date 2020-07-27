521e022cf9562c999b23115e5fffcecde25283f4
lucasvr39 <lucasveras1994@gmail.com>
2020-07-27 18:15:02 -0300

    add ids and basic page's structure

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//const textInput = "";
+// Req 1
+const userTextInput = document.querySelector("#text-input");
+const memeTextInput = document.querySelector("#meme-text");
+
+userTextInput.addEventListener("input", function () {
+  memeTextInput.innerText = userTextInput.value;
+})
+
+function uploadMemeImg() {
+  document.getElementById('meme-image').src = window.URL.createObjectURL(this.file[0]);
+}
+
+let uploadImg = document.getElementById('meme-insert');
+uploadImg.addEventListener('change', uploadMemeImg);
