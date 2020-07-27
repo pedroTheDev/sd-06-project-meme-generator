7c80f3848609c7cbe2f76ffe45f3cffbfbd872e9
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-27 20:01:26 -0300

    colocando letra em cima da imagem

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ window.onload = function() {
 
     let escreverTexto = document.getElementById("text-input");
     let textoMostrado = document.getElementById("meme-text");
+    let imagemSelecionada = document.getElementById("meme-insert");
+    let imagemMostrada = document.getElementById("meme-image");
     escreverTexto.addEventListener("change", function() {
-        document.getElementById("meme-text").innerHTML = document.getElementById("text-input").value;
+        textoMostrado.innerHTML = escreverTexto.value;
+    });
+    imagemSelecionada.addEventListener("change", function() {
+        console.log(imagemSelecionada.file);
     });
     
 
