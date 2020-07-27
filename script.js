aa63b4153271b850472be4c0a371b37b77223649
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-27 19:47:34 -0300

    Interaçao do input da imagem

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Interação com o input de TEXTO
 // 1. Elementos para acessar
-let inputText = document.getElementById("text-input");
-let memeText = document.getElementById("meme-text");
-// 2. Pega o input e mostra na tela
+let textInput = document.getElementById("text-input");
+let textMeme = document.getElementById("meme-text");
+// 2. Pega o texto e mostra na tela
 let userTextMeme = "";
-function getTextInput() {
-    userTextMeme = inputText.value;
-    memeText.innerHTML = userTextMeme;
+function getTextInput () {
+    userTextMeme = textInput.value;
+    textMeme.innerHTML = userTextMeme;
 }
-inputText.addEventListener("keyup", getTextInput);
+textInput.addEventListener("keyup", getTextInput);
 
 // Interação com o input da IMAGEM
+// 1. Elementos para acessar
+let inputImage = document.getElementById("meme-insert");
+let memeImage = document.getElementById("meme-image");
+// 2. Pegar a imagem e mostrar na tela
+function displayImage(event) {
+    let file = event.target.files[0];
+    memeImage.src = URL.createObjectURL(file);
+}
+inputImage.addEventListener("change", displayImage);
