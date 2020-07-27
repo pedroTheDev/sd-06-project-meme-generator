1d8e39ecf7ada116ad3c024dd3efac8732ba7bf3
Pablo Santos <pablo.trybe@gmail.com>
2020-07-27 20:21:28 -0300

    Funçao de adicionar imagem corrigida

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ function printTextMeme() {
   textInMeme = inputText.value;
   memeText.innerHTML = textInMeme;
 }
-inputText.addEventListener('keyup', printTextMeme);
-const loadFile = function (event) {
-  const output = document.getElementById('output');
-  output.src = URL.createObjectURL(event.target.files[0]);
-  output.onload = function () {
-    URL.revokeObjectURL(output.src);
-  }
-};
+
+// const loadFile = function (event) {
+//   const output = document.getElementById('output');
+//   output.src = URL.createObjectURL(event.target.files[0]);
+//   output.onload = function () {
+//     URL.revokeObjectURL(output.src);
+//   }
+// };
+
+inputText.addEventListener("keyup", printTextMeme);
+
+let output = document.getElementById('output');
+let memeImg = document.getElementById('meme-image');
+function showImage(e) {
+  let getFile = e.target.files[0];
+  memeImg.src = URL.createObjectURL(getFile);
+}
+output.addEventListener('change', showImage);
