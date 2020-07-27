c47cdca685f832119568730b5b9a29d0be138955
Anderson <and.stuber@gmail.com>
2020-07-27 14:21:55 -0300

    Fixes for Evaluator Job

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function insertMemeText() {
   const insertTextField = document.getElementById('text-input');
   const memeText = document.getElementById('meme-text');
-  insertTextField.addEventListener('keypress', (key) => {
-    if (key.key === 'Enter') {
-      memeText.innerText = insertTextField.value;
-      insertTextField.value = '';
-    }
-  });
+  insertTextField.addEventListener('change', () => {
+    memeText.innerText = insertTextField.value;
+  })
+  
+  // insertTextField.addEventListener('keypress', (key) => {
+  //   if (key.keyCode === 32) {
+  //     memeText += " ";
+  //   } else {
+  //     memeText.innerText += key.key
+  //   }
+  //   // memeText.innerText = insertTextField.value;
+  //   // insertTextField.value = '';
+    
+  // });
 }
 
 function imageUploadURL() {
