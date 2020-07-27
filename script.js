f93a7083f5a40ab4d75f6d0b6c4ab1382c9a39b2
thalesgr <thalesgr@gmail.com>
2020-07-27 18:01:22 -0300

    CC

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-let inputTexto = document.querySelector('#text-input');
-let outputTexto = document.querySelector('#meme-text');
+const inputTexto = document.querySelector('#text-input');
+const outputTexto = document.querySelector('#meme-text');
 
 inputTexto.addEventListener('input', (e) => {
-    outputTexto.innerHTML = e.target.value;
+  outputTexto.innerHTML = e.target.value;
 });
 
-let inputImagem = document.querySelector('#meme-insert');
-let outputImagem = document.querySelector('#meme-image');
+const inputImagem = document.querySelector('#meme-insert');
+const outputImagem = document.querySelector('#meme-image');
 
 inputImagem.addEventListener('change', (e) => {
-    outputImagem.src = URL.createObjectURL(e.target.files[0]);
+  outputImagem.src = URL.createObjectURL(e.target.files[0]);
 });
-
-
