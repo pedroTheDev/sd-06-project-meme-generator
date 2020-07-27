43992289a706c3341bc3dc9309f406f0d74f3500
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-27 19:10:32 -0300

    testing cc

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const memeInput = document.querySelector('#meme-insert');
 const image = document.querySelector('#meme-image');
 
 textInput.addEventListener('keyup', function () {
   memeText.innerText = textInput.value;
 });
 
-const loadFile = function (event) {
+// const loadFile = function (event) {
+//   image.src = URL.createObjectURL(event.target.files[0]);
+//   image.onload = function () {
+//     URL.revokeObjectURL(image.src);
+//   };
+// };
+
+memeInput.addEventListener('change', function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
-  image.onload = function () {
-    URL.revokeObjectURL(image.src);
-  };
-};
+});
