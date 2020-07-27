6dfdfe6ce6e402d42ff60eb6510767912acfb16e
Anderson <and.stuber@gmail.com>
2020-07-27 16:22:47 -0300

    Adjusts dor evaluator

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function getText() {
   const textInput = document.getElementById('text-input');
   const memeText = document.getElementById('meme-text');
-  textInput.addEventListener('change', () => {
+  textInput.addEventListener('input', () => {
     memeText.innerText = textInput.value;
   });
 }
