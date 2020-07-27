31f5ab91e921d49950444cd47d835cefad66f004
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-27 17:42:22 -0300

    Resolução do requisito 02

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
+	// Altera o texto do Meme
 	let myTextImput = document.getElementById("text-input");
 	myTextImput.addEventListener("keyup", function(){
-		console.log(myTextImput.value);
 		let myTextMeme = document.getElementById("meme-text");
 		if (myTextImput.value == "") {
 			myTextMeme.innerText = "Texto do Meme"	
@@ -9,4 +9,16 @@ window.onload = function () {
 			myTextMeme.innerText = myTextImput.value;
 		}
 	});
+
+	// Altera a imagem do Meme	
+	let myImageInput = document.getElementById("meme-insert");
+	myImageInput.addEventListener("change", function(event){
+		let myImageMeme = document.getElementById("meme-image");
+		myImageMeme.src = URL.createObjectURL(event.target.files[0]);
+		console.log(myImageInput.scr);
+		myImageMeme.onload = function() {
+			URL.revokeObjectURL(myImageMeme.src);
+		}
+	});
+
 };
