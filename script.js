9ed4bec464431f4577afd1daf99ad83165c65bbc
Isadora Koga <isadorakoga710@gmail.com>
2020-07-27 19:02:14 -0300

    requisito 6 done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function textInput() {
   // Função que vai fazer o texto aparecer no meme
   let inputText = document.getElementById('text-input').value;
   let divMemeText = document.getElementById('meme-text');
-  divMemeText.style.maxWidth = "500px"
+  divMemeText.style.maxWidth = '500px';
   divMemeText.innerHTML = inputText;
 }
 
@@ -10,21 +10,21 @@ function imageInput() {
   // Função que vai fazer a imagem aparecer no meme
   const outputImage = document.getElementById('meme-image');
   outputImage.src = URL.createObjectURL(event.target.files[0]);
-  outputImage.onload = function() {
+  outputImage.onload = function () {
     URL.revokeObjectURL(outputImage.src);
   };
 }
 
-let divContainer = document.getElementById('meme-image-container');
+const divContainer = document.getElementById('meme-image-container');
 
 function fireCustom() {
-    divContainer.style.border = "3px dashed red";
+  divContainer.style.border = '3px dashed red';
 }
 
 function waterCustom() {
-    divContainer.style.border = "5px double blue";
+  divContainer.style.border = '5px double blue';
 }
 
 function earthCustom() {
-    divContainer.style.border = "6px groove green";
+  divContainer.style.border = '6px groove green';
 }
