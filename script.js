e2e8ee6fe1b50acff7b3732c8bbb48e277aac55b
Arthur de Sa <arthurdesa94@gmail.com>
2020-07-27 17:03:01 -0300

    alterando o 1, 2 e 3

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// comecando o projeto
+window.onload = function () {
+    let memeText = document.querySelector('#meme-text');
+    let textInput = document.querySelector('#text-input');
+        textInput.addEventListener('input', function () {
+        memeText.innerHTML = textInput.value;
+    }
+
+    const imgContainer = document.querySelector('#meme-image-container');
+    let memeImage = document.querySelector('#meme-image');
