25257cfd3601a33b8007d4be2e698e5a6728241e
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-27 14:23:45 -0300

    Fixing bugs to Task 1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-    
+
+window.onload = function () {
+//Referencing elements
+let inputTextElement = document.querySelector('#text-input');
+let memeTextBox = document.querySelector('#meme-text');
+
+//Functions
+let functionalities = {
+    addText: (event) => memeTextBox.innerHTML = inputTextElement.value,
+    }
+
+
+//Events
+inputTextElement.addEventListener('keypress', functionalities['addText']);
+}
