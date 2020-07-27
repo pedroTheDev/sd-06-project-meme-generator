b8f45d1ef60950b361106f2ce4d9b5723838d2c1
FPhilipi <fabiphilipi@gmail.com>
2020-07-27 16:28:59 -0300

    Requisitos

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ window.onload = function() {
 let textInPut = document.querySelector("#text-input");
 textInPut.addEventListener("keyup", function(){
     let textOutPut = document.querySelector("#meme-text");
-    textOutPut.innerHTML = textInPut.value;
+    textOutPut.innerHTML = textInPut.value;    
 })
     
 let imageInPut = document.querySelector("#meme-insert");
 
 imageInPut.addEventListener("change", function(event) {
-    document.getElementById("meme-image").src = 
-    URL.createObjectURL(event.target.files[0]);
-    // imageOutPut.src = imageInPut.files[0].name;
-
+    
+    document.getElementById("meme-image").src = URL.createObjectURL(event.target.files[0]);
+    // let imageOutPut = document.getElementById("meme-image");
+    // imageOutPut.src = URL.createObjectURL(event.target.files[0]);
+    
+         
 })
 
 }
