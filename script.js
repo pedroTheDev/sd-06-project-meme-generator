d1c5980542c64e5019d0a2a232602baa6427119a
lucasvr39 <lucasveras1994@gmail.com>
2020-07-27 20:07:09 -0300

    fix some code climate issues and img container background color

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const userTextInput = document.querySelector("#text-input");
-const memeTextInput = document.querySelector("#meme-text");
+const userTextInput = document.querySelector('#text-input');
+const memeTextInput = document.querySelector('#meme-text');
 
-userTextInput.addEventListener("input", function () {
+userTextInput.addEventListener('input', function () {
   memeTextInput.innerText = userTextInput.value;
-})
+});
 
-var loadFile = function(event) {
-  var uploadImg = document.getElementById('memeimg');
+let loadFile = function(event) {
+  let uploadImg = document.getElementById('memeimg');
   uploadImg.src = URL.createObjectURL(event.target.files[0]);
   uploadImg.onload = function() {
-    URL.revokeObjectURL(uploadImg.src)
+    URL.revokeObjectURL(uploadImg.src);
   }
 };
