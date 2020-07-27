c90662f77f308fc6258b7b87e61befa7f99ac1c3
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-27 14:26:54 -0300

    add memeInsert func

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ window.onload = function() {
   function createMemeText(event) {
     document.getElementById('meme-text').innerText = event.target.value;
   }
+  function memeInsert(event) {
+    var output = document.getElementById('meme-image');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  }
   
-  document.getElementById('text-input').addEventListener('change', createMemeText);  
-}
+  document.getElementById('text-input').addEventListener('change', createMemeText);
+  document.getElementById('meme-insert').addEventListener('change', memeInsert);
+};
