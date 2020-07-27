814daaee219dd0f2bd12455edc1cad61ff27c3f4
Marcelo Pessini <marceloppessini@gmail.com>
2020-07-27 17:50:10 -0300

    Imagem aparecendo na página

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ document.addEventListener("keyup", function(){
   memeText.innerHTML = textImputed;
 })
 
+function onFileSelected(event) {
+  var selectedFile = event.target.files[0];
+  var reader = new FileReader();
+
+  var imgtag = document.getElementById("myimage");
+  imgtag.title = selectedFile.name;
+
+  reader.onload = function(event) {
+    imgtag.src = event.target.result;
+  };
+
+  reader.readAsDataURL(selectedFile);
+}
+
+// document.getElementById('meme-insert').addEventListener('change', function(){
+//   let uploadedImage = document.getElementById('meme-insert').value;
+//   let reader = new FileReader();
+//   reader.onload = 
+// })
