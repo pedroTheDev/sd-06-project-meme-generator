c7f4b07bedfac02dbfb0ed1e5d17e277bb2f0aac
paulo-lins <paulolins100@gmail.com>
2020-07-27 17:56:59 -0300

    Meme generetor. Requisito 1

---
+++ script.js
@@ -0,0 +1,9 @@
+function getInputValue() {
+    let text = document.getElementById("text-input").value;
+    // let texto = document.getElementById("text-meme");
+    // texto.appendChild(text);
+    document.getElementById('meme-text').innerText = text
+}
+
+let area =  document.getElementById("text-input");
+area.addEventListener('keyup', getInputValue)
