51f689a77770e7ec677dc2edab289ac21ea7c76e
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-07-27 19:41:14 -0300

    Correções do CodeClimate

--- script.js
+++ script.js
@@ -1,53 +1,4 @@
 // ver se dá para refatorar usando event.target nas situações
-
-// Insert text
-const elementTextInput = document.getElementById('text-input');
-elementTextInput.addEventListener('input', function() {
-  const textInput = elementTextInput.value;
-  const memeText = getElementMemeText();
-  memeText.innerText = textInput;
-});
-
-// Add image
-const elementImageInput = document.getElementById('meme-insert');
-elementImageInput.addEventListener('input', function(event) {
-  const image = getElementImage();
-  image.src = URL.createObjectURL(event.target.files[0]);
-  image.onload = function() {
-    URL.revokeObjectURL(image.src);
-  }
-});
-
-// Choose preset meme image
-const elementPresetsContainer = document.getElementById('presets-images');
-elementPresetsContainer.addEventListener('click', function(event) {
-  const presetImage = event.target;
-  console.log(presetImage);
-  const image = getElementImage();
-  image.src = presetImage.src;
-});
-
-// Fire border style
-const elementRedButton = document.getElementById('fire');
-elementRedButton.addEventListener('click', function() {
-  const memeBorder = getElementMemeContainer();
-  memeBorder.style.border = '3px dashed red';
-});
-
-// Water border style
-const elementBlueButton = document.getElementById('water');
-elementBlueButton.addEventListener('click', function() {
-  const memeBorder = getElementMemeContainer();
-  memeBorder.style.border = '5px double blue';
-});
-
-// Earth border style
-const elementGreenButton = document.getElementById('earth');
-elementGreenButton.addEventListener('click', function() {
-  const memeBorder = getElementMemeContainer();
-  memeBorder.style.border = '6px groove green';
-});
-
 function getElementMemeText() {
   const memeText = document.getElementById('meme-text');
   return memeText;
@@ -58,7 +9,53 @@ function getElementImage() {
   return elementImage;
 }
 
-function getElementMemeContainer () {
+function getElementMemeContainer() {
   return document.getElementById('meme-image-container');
 }
 
+// Insert text
+const elementTextInput = document.getElementById('text-input');
+elementTextInput.addEventListener('input', function () {
+  const textInput = elementTextInput.value;
+  const memeText = getElementMemeText();
+  memeText.innerText = textInput;
+});
+
+// Add image
+const elementImageInput = document.getElementById('meme-insert');
+elementImageInput.addEventListener('input', function (event) {
+  const image = getElementImage();
+  image.src = URL.createObjectURL(event.target.files[0]);
+  image.onload = function () {
+    URL.revokeObjectURL(image.src);
+  }
+});
+
+// Choose preset meme image
+const elementPresetsContainer = document.getElementById('presets-images');
+elementPresetsContainer.addEventListener('click', function (event) {
+  const presetImage = event.target;
+  const image = getElementImage();
+  image.src = presetImage.src;
+});
+
+// Fire border style
+const elementRedButton = document.getElementById('fire');
+elementRedButton.addEventListener('click', function () {
+  const memeBorder = getElementMemeContainer();
+  memeBorder.style.border = '3px dashed red';
+});
+
+// Water border style
+const elementBlueButton = document.getElementById('water');
+elementBlueButton.addEventListener('click', function () {
+  const memeBorder = getElementMemeContainer();
+  memeBorder.style.border = '5px double blue';
+});
+
+// Earth border style
+const elementGreenButton = document.getElementById('earth');
+elementGreenButton.addEventListener('click', function () {
+  const memeBorder = getElementMemeContainer();
+  memeBorder.style.border = '6px groove green';
+});
