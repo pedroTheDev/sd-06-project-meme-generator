881227703afa08bf38074958885dc3a280e2d222
isabellavjs <isabellavjs@gmail.com>
2020-07-27 16:50:14 -0300

    Requirements 1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Variables
 let userText = document.getElementById('text-input');
-let containerMemes = document.getElementById('meme-image-container');
+let containerText = document.getElementById('meme-text');
 let insertMeme = document.getElementById('meme-insert');
 //let divMemeText = document.getElementById('meme-text')
 
 
 //Receive user text, and attribute it to the container
 function WriteText() {
-    containerMemes.innerHTML = userText.value;
+    containerText.innerHTML = userText.value;
 }
 
 //Upload user image
