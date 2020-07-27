22be6e1ea51c47b24e8abbd18a3b099addbeb8ac
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-27 19:40:03 -0300

    testing border elements

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const memeInput = document.querySelector('#meme-insert');
 const image = document.querySelector('#meme-image');
+const container = document.querySelector('#meme-image-container');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
 
 textInput.addEventListener('keyup', function () {
   memeText.innerText = textInput.value;
@@ -16,4 +20,16 @@ textInput.addEventListener('keyup', function () {
 
 memeInput.addEventListener('change', function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
+});
+
+fireButton.addEventListener('click', function () {
+  container.style.border = '3px dashed red';
+});
+
+waterButton.addEventListener('click', function () {
+  container.style.border = 'blue 5px double';
+});
+
+earthButton.addEventListener('click', function () {
+  container.style.border = 'groove green 6px';
 });
