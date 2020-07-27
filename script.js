28dbbb05a91ee31e16497f693426dd198d904ea8
Marcelo Pessini <marceloppessini@gmail.com>
2020-07-27 16:39:28 -0300

    Adicionando elementos de configuração de cor/fontsize/shadow do texto

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener("keyup", function(){
   let textImputed = document.getElementById("text-input").value;
   let memeText = document.getElementById("meme-text");
+  memeText.style.textShadow = '5px 5px 5px black';
+  memeText.style.fontSize = "30px";
+  memeText.style.color = "white";
   memeText.innerHTML = textImputed;
 })
+
