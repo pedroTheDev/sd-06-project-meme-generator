0e1ef810b1313f582d50c9db59beda88324ded11
Victor Bergamini <vbergadev@gmail.com>
2020-07-27 12:49:46 -0300

    choose file

--- script.js
+++ script.js
@@ -2,8 +2,5 @@ function gerarmeme() {
     alert('Gerado')
     let textoMeme = document.getElementById("text-input");
     let imagemMeme = document.getElementById("meme-image-container");
-    
-}
-function uploadImagem(){
-    alert('imagem')
+    alert(textoMeme)
 }
