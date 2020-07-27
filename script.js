fc1bd0e92e70006935dabbb3c51de4d4fe2d0e57
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-27 15:22:59 -0300

    inserido o HTML básico e o loader de imagem

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//Script do projeto meme generator 
+//Script do projeto meme generator
+let loadFile = function(event) {
+    let output = document.getElementById('meme-image');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) 
+    }
+};
+
