0f6769c39605551f33401625c38fed50f48b6b62
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-27 14:01:31 -0300

    2 e 3: Foto + texto sobre foto

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function textWrite () {
-    let texto = document.querySelector("#text-input").value;
-    let memeText = document.querySelector("#meme-text");
-    memeText.innerHTML = texto;
+function textWrite() {
+  const texto = document.querySelector('#text-input').value;
+  const memeText = document.querySelector('#meme-text');
+  memeText.innerHTML = texto;
+}
+
+function selectedImage(event) {
+  let memeImage = document.getElementById("meme-image");
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function () {
+    URL.revokeObjectURL(memeImage.src);
+  }
 }
