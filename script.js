5713f2924b5e188790bd91160cebf4ec6961895e
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-07-27 19:00:34 -0300

    Implementado requisito 7 (bônus2)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// ver se dá para refatorar usando event.target nas situações
 
+// Insert text
 const elementTextInput = document.getElementById('text-input');
 elementTextInput.addEventListener('input', function() {
   const textInput = elementTextInput.value;
@@ -6,6 +8,7 @@ elementTextInput.addEventListener('input', function() {
   memeText.innerText = textInput;
 });
 
+// Add image
 const elementImageInput = document.getElementById('meme-insert');
 elementImageInput.addEventListener('input', function(event) {
   const image = getElementImage();
@@ -15,18 +18,30 @@ elementImageInput.addEventListener('input', function(event) {
   }
 });
 
+// Choose preset meme image
+const elementPresetsContainer = document.getElementById('presets-images');
+elementPresetsContainer.addEventListener('click', function(event) {
+  const presetImage = event.target;
+  console.log(presetImage);
+  const image = getElementImage();
+  image.src = presetImage.src;
+});
+
+// Fire border style
 const elementRedButton = document.getElementById('fire');
 elementRedButton.addEventListener('click', function() {
   const memeBorder = getElementMemeContainer();
   memeBorder.style.border = '3px dashed red';
 });
 
+// Water border style
 const elementBlueButton = document.getElementById('water');
 elementBlueButton.addEventListener('click', function() {
   const memeBorder = getElementMemeContainer();
   memeBorder.style.border = '5px double blue';
 });
 
+// Earth border style
 const elementGreenButton = document.getElementById('earth');
 elementGreenButton.addEventListener('click', function() {
   const memeBorder = getElementMemeContainer();
