692ae23bb82320ce920144d1599855b24a48030d
Anderson <and.stuber@gmail.com>
2020-07-27 13:17:22 -0300

    image upload done

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 window.onload = () => {
-    console.log('carregou')
-    insertMemeText();
+  console.log('carregou')
+  insertMemeText();
+  imageUploadURL();
 }
-
 
 function insertMemeText() {
-    const insertTextField = document.getElementById('text-input');
-    const memeText = document.getElementById('meme-text');
-    console.log(insertTextField.innerText)
-    insertTextField.addEventListener('keypress', (key) => {
-        if (key.key === 'Enter') {
-            memeText.innerText = insertTextField.value;
-            insertTextField.value = '';
-        }
-    })
+  const insertTextField = document.getElementById('text-input');
+  const memeText = document.getElementById('meme-text');
+  insertTextField.addEventListener('keypress', (key) => {
+    if (key.key === 'Enter') {
+      memeText.innerText = insertTextField.value;
+      insertTextField.value = '';
+    }
+  })
 }
+
+function imageUploadURL() {
+  let reader = new FileReader();
+  const memeImageUpload = document.getElementById('meme-insert');
+  memeImageUpload.addEventListener('change', () => {
+    //console.log(memeImageUpload.files[0])
+    var reader = new FileReader();
+    reader.onload = (e) => {
+      document.getElementById('meme-image').src = e.target.result;
+    };
+    reader.readAsDataURL(memeImageUpload.files[0])
+
+  })
+}
+
