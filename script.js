a6e9e4b7f1a3c517a31a53ad159534729402c623
LughWalle <civalfran1@gmail.com>
2020-07-27 16:45:56 -0500

    imagem adicionada com input e exibicao

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// conteiner text
+// conteiner
+// variaveis
 const phrase = document.querySelector('#text-input');
 const inImage = document.querySelector('#meme-text');
+const image = document.querySelector('#meme-image');
+const imgInsert = document.querySelector('#meme-insert');
+
+// function and events
+// inserir imagem
 function getPhrase() {
   inImage.innerHTML = phrase.value;
 }
 phrase.addEventListener('keyup', getPhrase);
+
+// inserir imagem
+function getImage() {
+  image.src = URL.createObjectURL(imgInsert.files[0]);
+}
+imgInsert.addEventListener('change', getImage); 
