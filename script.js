2cd71356e3c2a1aab74ff7aa90baa3248a6a164f
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-27 17:22:02 -0300

    updating image

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const imgButton = document.querySelector('#meme-insert');
+const image = document.querySelector('#meme-image');
 
 textInput.addEventListener('keyup', function () {
-  memeText.innerHTML = textInput.value;
+  memeText.innerText = textInput.value.toUpperCase();
 });
+
+const loadFile = function(event) {
+  image.src = URL.createObjectURL(event.target.files[0]);
+  image.onload = function() {
+    URL.revokeObjectURL(image.src)
+  }
+}
