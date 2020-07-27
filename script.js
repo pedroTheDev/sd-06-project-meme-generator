b38f77c7028fa2bc78fbb85e68e340f9843556ac
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-27 17:14:28 -0300

    meme generator 01

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //inciando
+
+//meme-image
+
+function readMemeImage() {
+
+    var preview = document.querySelector('img');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+      
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+      
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
+
+
+
+//meme-text
+
+function onChangeText () {
+
+    let textInput = document.querySelector("#text-input");
+    
+    document.querySelector("#meme-text").innerText = textInput.value;
+}
+let textInput = document.querySelector("#text-input");
+textInput.addEventListener("input", onChangeText);
+
+
