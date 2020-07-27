52f2183b50e3ecb366f650a67f7c676f740a0f99
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-07-27 18:50:32 -0300

    First commit MEME GENERATOR

---
+++ script.js
@@ -0,0 +1,15 @@
+let loadImage = function (event) {
+  let output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src) // free memory
+  }
+};
+
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+textInput.addEventListener('input', memeTextInsert);
+
+function memeTextInsert() {
+  memeText.innerHTML = textInput.value;
+}
