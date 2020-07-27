faa8cf7d312029debea1214c96d1f50387965155
William <williamjog@hotmail.com>
2020-07-27 18:44:02 -0300

    2nd Requirement Image Selection

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const memeImg = document.querySelector('#meme-image');
+const memeInsertImg = document.querySelector('#meme-insert');
 
 textInput.addEventListener('input', function () {
   memeText.innerText = textInput.value;
+});
+
+
+memeInsertImg.addEventListener('input', function () {
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
 });
