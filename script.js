b22123ada48ac6f5878c6c1f5b6ccce5aaf0d763
Anderson <and.stuber@gmail.com>
2020-07-27 15:09:00 -0300

    Input done

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function getText() {
+  const textInput = document.getElementById('text-input');
+  const memeText = document.getElementById('meme-text');
+  textInput.addEventListener('change', () => {
+    memeText.innerText = textInput.value;
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = () => {
+  console.log('carregou')
+  getText();
+}
