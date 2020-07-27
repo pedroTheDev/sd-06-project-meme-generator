9c7caabce73509e3bfa0ef7c34ecabc434650bf9
Paulo Frazão <paulo4187@gmail.com>
2020-07-27 19:54:31 -0300

    correção dos três primeiros requisitos mais uma vez

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 
 function loadImage() {
-    let reader = new FileReader();
+    const reader = new FileReader();
     reader.onload = function () {
-        let saida = document.getElementById('meme-image');
-        saida.src = reader.result;
-    }
+    const saida = document.getElementById('meme-image');
+      saida.src = reader.result;}
     if (reader) {
-        reader.readAsDataURL(event.target.files[0]);
-    }
+        reader.readAsDataURL(event.target.files[0]);}
 }
-
 function loadText() {
-    let inputText = document.querySelector('#text-input').value;
-    let pText = document.querySelector('#meme-text');
-    pText.innerHTML = inputText;
-}
+  const inputText = document.querySelector('#text-input').value;
+  const pText = document.querySelector('#meme-text');
+  pText.innerHTML = inputText;}
