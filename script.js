05698a797744d32c8c20da60254ee336f207368a
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-27 19:22:20 -0300

    Fazendo pequenas mudanças pelo Code Climate

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
-window.onload = function() {
-
-  let text = document.querySelector("#text-input");
-  let memeText = document.querySelector("#meme-text");
-  let memeCont = document.querySelector("#meme-image-container");
-  let fire = document.querySelector("#fire");
-  let water = document.querySelector("#water");
-  let earth = document.querySelector("#earth");
-  let memeImage = document.getElementById('meme-image');
-
-  let imgP = document.querySelector("#imgP");
-
-
-  text.addEventListener("keyup", function() {
+window.onload = function () { 
+  const text = document.querySelector('#text-input');
+  const memeText = document.querySelector('#meme-text');
+  const memeCont = document.querySelector('#meme-image-container');
+  const fire = document.querySelector('#fire');
+  const water = document.querySelector('#water');
+  const earth = document.querySelector('#earth');
+  const memeImage = document.getElementById('meme-image');
+  const imgP = document.querySelector('#imgP');
+  text.addEventListener('keyup', function() {
     memeText.innerText = text.value;
   });
-
-  fire.addEventListener("click", function() {
-    memeCont.style.border = "3px dashed red";
+  fire.addEventListener('click', function () {
+    memeCont.style.border = '3px dashed red';
   });
-  
-  water.addEventListener("click", function() {
-    memeCont.style.border = "5px double blue";
+  water.addEventListener('click', function () {
+    memeCont.style.border = '5px double blue';
   });
-
-  earth.addEventListener("click", function() {
-    memeCont.style.border = "6px groove green";
+  earth.addEventListener('click', function () {
+    memeCont.style.border = '6px groove green';
   });
-
-  imgP.addEventListener("click", function(event) {
+  imgP.addEventListener('click', function (event) {
     memeImage.src = event.target.src;
-  })  
+  }); 
 }
 
-let loadFile = function(event) {
-  let memeImage = document.getElementById('meme-image');
+const loadFile = function (event) {
+  const memeImage = document.getElementById('meme-image');
   memeImage.src = URL.createObjectURL(event.target.files[0]);
-  memeImage.onload = function() {
-    URL.revokeObjectURL(memeImage.src)
-  }
-}
+  memeImage.onload = function () {
+    URL.revokeObjectURL(memeImage.src);
+  };
+};
