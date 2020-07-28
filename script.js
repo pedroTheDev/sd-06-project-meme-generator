e1e5dc505389dd4b6eba7e97eb481bd495592eba
ricknois <rickssti@outlook.com>
2020-07-28 01:05:06 -0300

    Correção do imput de imagem e de texto

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
-function previewFile() {
-    var preview = document.querySelector('img');
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-  
-    reader.onloadend = function () {
-      preview.src = reader.result;
-    }
-    
-    if (file) {
-        console.log(reader.readAsDataURL(file));
-    } else {
-      preview.src = "";
-    }
-  }
+const imgUp = document.querySelector('#meme-insert');
+const imgShow = document.querySelector('#meme-image');
+const txtInput = document.querySelector('#text-input');
+const txtOutput = document.querySelector('#meme-text')
+
+function showImage(event) {
+    let imgSrc = event.target.files[0];
+    console.log(imgSrc);
+    imgShow.src = URL.createObjectURL(imgSrc);
+}
+
+txtInput.addEventListener('keyup', function () {
+    txtOutput.innerHTML = txtInput.value;
+})
