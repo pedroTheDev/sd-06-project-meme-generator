ee4608b1945537d1cd233455044c430961aa1f9a
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-28 11:06:59 -0300

    carregando imagem do usuario na pagina

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ window.onload = function() {
     let textoMostrado = document.getElementById("meme-text");
     let imagemSelecionada = document.getElementById("meme-insert");
     let imagemMostrada = document.getElementById("meme-image");
-    escreverTexto.addEventListener("change", function() {
-        textoMostrado.innerHTML = escreverTexto.value;
-    });
-    imagemSelecionada.addEventListener("input", function() {
-        console.log(imagemSelecionada.file);
-    });
     
-
+    escreverTexto.addEventListener("input", function() {
+        textoMostrado.innerText = escreverTexto.value;
+    });
+    imagemSelecionada.addEventListener("change", function() {
+        console.log("função chamada");
+        let reader = new FileReader();
+        reader.onload = function(){
+            imagemMostrada.src = reader.result;
+        };        
+        reader.readAsDataURL(imagemSelecionada.files[0]);
+    });      
 }
