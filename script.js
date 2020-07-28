dccb77b3cf8c718cd318aad17bd7e61edf605866
Leandro <leandro.feitoza8018@gmail.com>
2020-07-28 15:11:11 -0300

    Correções do Codeclimate

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-let loadFile = function(event) {
-    let output = document.getElementById('meme-image');
+const loadFile = function (event) {
+    let output = document.getElementById ('meme-image');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
-        URL.revokeObjectURL(output.src)
+      URL.revokeObjectURL(output.src);
     }
-};
-
-const inputTextBox = document.getElementById('text-input')
-const paragraph = document.getElementById("output-text")
-
-function insertText(event) {
-    document.querySelector('text-input')
-    paragraph.innerHTML = inputTextBox.value
-}
-
-inputTextBox.addEventListener('keyup',insertText);
-
+  };
+  
+  const inputTextBox = document.getElementById ('text-input');
+  const paragraph = document.getElementById ("output-text");
+  
+  function insertText (event) {
+    document.querySelector ('text-input');
+    paragraph.innerHTML = inputTextBox.value;
+  };
+  
+  inputTextBox.addEventListener ('keyup', insertText);
