d599d9330fa209e86d6eb52726b15988f6237dbc
ricknois <rickssti@outlook.com>
2020-07-28 01:27:27 -0300

    Code Climate

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const imgUp = document.querySelector('#meme-insert');
 const imgShow = document.querySelector('#meme-image');
 const txtInput = document.querySelector('#text-input');
 const txtOutput = document.querySelector('#meme-text')
 
 function showImage(event) {
-  let imgSrc = event.target.files[0];
-  console.log(imgSrc);
+  const imgSrc = event.target.files[0];
   imgShow.src = URL.createObjectURL(imgSrc);
 }
 
