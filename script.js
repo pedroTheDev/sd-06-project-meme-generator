b700dcad9f5def63b9e6fbe2dd2d16571041cc69
Danilo <dansantosalfa@gmail.com>
2020-07-28 10:34:15 -0300

    Tentando alcançar requisitos 3.0

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const textInput = document.querySelector('#text-input');
 const textmeme = document.querySelector('#meme-text');
-let imageMeme = document.querySelector('#meme-insert');
-let preview = document.queryCommandEnabled('#meme-image');
+const imageMeme = document.('#meme-insert');
+const preview = document.getElementById('meme-image');
 
 textInput.addEventListener('keyup', function () {
     textmeme.innerHTML = textInput.value;
 });
-
-imageMeme.addEventListener('change', function (event) {
-preview.src = URL.createObjectURL(event.target.files[0]);
+memeInput.addEventListener('change', function (event) {
+image.src = URL.createObjectURL(event.target.files[0]);
 });
