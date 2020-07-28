d221ef7166f172d84e1d8b46b4803598154129ce
Igor Leão <igorleao.eng@gmail.com>
2020-07-27 22:19:39 -0300

    segundo teste

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var loadFile = function(event) {
+const loadFile = function(event) {
   var reader = new FileReader();
   reader.onload = function(){
     var output = document.getElementById('output');
@@ -8,7 +8,10 @@ var loadFile = function(event) {
 };
 const text = document.getElementById('text-input');
 const memeInsert = document.querySelector('#meme-insert');
-text.addEventListener('', function(){
+
+text.addEventListener('keyup', function(){
   memeIsert.innerText = text.value;
 })
 const containder = document.getElementById('meme-image-container');
+
+const imgMemeInsert = document.querySelector('#meme-insert');
