a983b3b0816b7cde627975897813f0386cee37b8
us19861229c <rcks.santos@gmail.com>
2020-07-28 16:22:28 -0300

    Realiza edições referentes ao texto da legenda do meme. Corrige erros apontados pelo codeClimate.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputDoMeme = document.querySelector('#text-input');
 const paragrafoDoMeme = document.querySelector('#meme-text');
 
 const inputDaImagem = document.querySelector('#meme-insert');
-const imagemDoMeme = document.querySelector('#meme-image')
+
 
 inputDoMeme.addEventListener('keyup', function () {
   paragrafoDoMeme.innerText = inputDoMeme.value;
@@ -12,6 +12,6 @@ inputDoMeme.addEventListener('change', function () {
 });
 
 inputDaImagem.addEventListener('change', function () {
-  let arquivoImagem = inputDaImagem.files[0];
-  document.querySelector('img').src = URL.createObjectURL(arquivoImagem)
+  const arquivoImagem = inputDaImagem.files[0];
+  document.querySelector('img').src = URL.createObjectURL(arquivoImagem);
 });
