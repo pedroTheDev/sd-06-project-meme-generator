b31c70febe407e7416d38578810d516a506562b8
Paulo Frazão <paulo4187@gmail.com>
2020-07-28 12:45:45 -0300

    Bonus 07 icons dos Memes e link referência para apresentação dentro da Div master

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const memeContainer = document.querySelector('#meme-image-container');
 const buttonFire = document.querySelector('#fire');
 const buttonWater = document.querySelector('#water');
 const buttonEarth = document.querySelector('#earth');
+const imageMemeUm = document.querySelector('#meme-1');
+const imageMemeDois = document.querySelector('#meme-2');
+const imageMemeTres = document.querySelector('#meme-3');
+const imageMemeQuatro = document.querySelector('#meme-4');
 
 inputImage.addEventListener('change', function (event) {
   const reader = new FileReader();
@@ -31,4 +35,20 @@ buttonWater.addEventListener('click', function () {
 
 buttonEarth.addEventListener('click', function () {
   memeContainer.className = 'borderEarth';
+});
+
+imageMemeUm.addEventListener('click', function () {
+  saida.src = "/imgs/meme1.png";
+});
+
+imageMemeDois.addEventListener('click', function () {
+  saida.src = "/imgs/meme2.png";
+});
+
+imageMemeTres.addEventListener('click', function () {
+  saida.src = "/imgs/meme3.png";
+});
+
+imageMemeQuatro.addEventListener('click', function () {
+  saida.src = "/imgs/meme4.png";
 });
