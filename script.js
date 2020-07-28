0a1f729aa2401aa040e237c62a8f3c929beca5ed
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-28 18:30:22 -0300

    Adcionado javaScript

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const memeInsert = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
 const memeImg = document.getElementById('meme-image');
 
-textInput.addEventListener('change', function () {
+textInput.addEventListener('input', function () {
     memeText.innerHTML = textInput.value;
 })
 
 memeInsert.addEventListener('change', function (event) {
-    const imgAux = event.target.files[0];
-    memeImg.src = URL.createObjectURL(imgAux);
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
+  memeImg.onload = function () {
+    URL.revokeObjectURL(memeImg.src);
+  };
 });
