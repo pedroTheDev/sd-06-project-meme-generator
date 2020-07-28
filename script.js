73e13553408d96707e5ae44d5f88c885bec83a65
Alana Monteiro <lanaah.15@gmail.com>
2020-07-28 15:00:50 -0300

    imagem

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function textAdd() {
-    let inputText = document.getElementById("text-input").value;
-    let textContainer = document.createElement("p");
-    textContainer.id = ("text-container");
-    let parentText  = document.getElementById("meme-text");
-    parentText.appendChild(textContainer);
+  let inputText = document.getElementById("text-input").value;
+  let textContainer = document.createElement("p");
+  textContainer.id = ("text-container");
+  let parentText = document.getElementById("meme-text");
+  parentText.appendChild(textContainer);
 
-      document.getElementById("text-container").innerHTML = inputText;
-    }
+  document.getElementById("text-container").innerHTML = inputText;
+}
 
-function imageAdd(){
-    
+function imageAdd() {
+  const imageInput = document.getElementById("image-input").files;
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    document.getElementById("meme-image-container").style.backgroundImage = "url(" + e.target.result + ")";
+  }
+  reader.readAsDataURL(imageInput[0]);
 }
