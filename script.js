8f38352d9c1cd62c04955f98e1bd58ed41ba9229
vania patricia <vapatty@gmail.com>
2020-07-28 20:48:32 -0300

    Quadrados ccom imagens, seleciona imagem do computador

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 
+var textInput = document.getElementById('text-input');
+var memeText = document.getElementById('meme-text');
+
+textInput.addEventListener('input', memeTextInsert);
+function memeTextInsert() {
+  memeText.innerHTML = textInput.value;
+}
 
