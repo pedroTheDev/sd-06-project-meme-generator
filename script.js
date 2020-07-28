623895e99a8c4cf745423feb3c8389071ffff8a0
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-28 18:57:11 -0300

    Desafio 2.8

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const memeText = document.getElementById('meme-text');
 const textImput = document.getElementById('text-input');
 const memeImageContainer = document.getElementById('meme-image-container');
+// const memeImage = document.getElementById('meme-image');
 
 textImput.addEventListener('keyup', function () {
   memeText.innerHTML = textImput.value;
 });
 
 const loadFile = function (event) {
-  const output = document.getElementById('meme-insert');
+  const output = document.getElementById('meme-image');
   output.src = URL.createObjectURL(event.target.files[0]);
   output.style.height = '450px';
   output.style.width = '450px';
