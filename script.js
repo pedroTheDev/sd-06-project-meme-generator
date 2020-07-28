8005bbdd36795d6a2e80fdc132260303d091fff8
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-07-28 19:57:40 -0300

    PROJETO MEMEGEN - html, java, css+-

---
+++ script.js
@@ -0,0 +1,14 @@
+let textInput=document.getElementById("text-input");
+  let memeText=document.getElementById("meme-text");
+  textInput.addEventListener("input", textInsert);
+  function textInsert(e) {
+    memeText.textContent=e.target.value;
+  }
+
+let memeInsert = function(event) {
+  let memeImage = document.getElementById('meme-image');
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function() {
+    URL.revokeObjectURL(memeImage.src);
+  }
+}
