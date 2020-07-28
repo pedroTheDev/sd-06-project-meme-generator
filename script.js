7f951c8913be90bcfe5a5619dc5afb7c397212a9
Paulo Frazão <paulo4187@gmail.com>
2020-07-28 10:42:13 -0300

    tratamento regras CodeClimate persistente, js e css, ultimos espaços nos arquivos

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const saida = document.getElementById('meme-image');
 const pText = document.querySelector('#meme-text');
 const inputImage = document.querySelector('#meme-insert');
 
-inputImage.addEventListener('change', function(event) {
+inputImage.addEventListener('change', function (event) {
   const reader = new FileReader();
-  reader.onload = function() {
+  reader.onload = function () {
     saida.src = reader.result;
   }
   if (reader) {
     reader.readAsDataURL(event.target.files[0]);
-  }
+  };
 });
 
-inputText.addEventListener('keyup', function() {
+inputText.addEventListener('keyup', function () {
   pText.innerHTML = inputText.value;
 });
