bfb38cc1cd0dbe65e48b2a5125bdc0f06cf8a83e
Alana Monteiro <lanaah.15@gmail.com>
2020-07-28 17:06:40 -0300

    ajuste nos eventos e imagem

--- script.js
+++ script.js
@@ -8,18 +8,16 @@ function textAdd() {
   document.getElementById("text-container").innerHTML = inputText;
 }
 
+const imageInput = document.getElementById("image-input")
+
+imageInput.addEventListener('change', imageAdd);
+
 function imageAdd() {
-  const imageInput = document.getElementById("image-input").files;
   const reader = new FileReader();
-  let parentImage = document.getElementById("meme-image");
-  let childImage = document.createElement("img");
-  childImage.id = ("uploaded-image")
-  parentImage.appendChild(childImage);
-  childImage.setAttribute("height","350px");
-  childImage.setAttribute("width","350px");
+
   reader.onload = function (e) {
     document.getElementById("uploaded-image").src = e.target.result;
   }
-  reader.readAsDataURL(imageInput[0]);
+  reader.readAsDataURL(imageInput.files[0]);
 }
 
