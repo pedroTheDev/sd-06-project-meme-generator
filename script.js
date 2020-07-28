d7e2320c09d1efdb54a5c9647318f24c5e4f426a
LeonardoCavachini <cavachini240@gmail.com>
2020-07-28 00:24:43 -0300

    texto

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ let text = document.getElementById('text-input');
 text.addEventListener('input',function(){
     let note = document.getElementById('meme-text');
     note.innerHTML = text.value;
-    note.style.color = 'white';
-    note.style.fontSize = '30px';
-    note.style.textShadow = '5px 5px 5px black';
-    note.style.position = 'absolute';
-    note.style.width = 'inherit';
-  
+   
 });
 //Adicionando imagem.
-
+let box = document.getElementById('meme-image-container');
+box.style.maxWidth = '100%';
 let btnImg = document.getElementById('meme-insert');
 btnImg.addEventListener('input', function(event) {
     
     
    let photo = document.getElementById('meme-image');
+   photo.style.maxWidth = 'inherit';
     
     photo.src = URL.createObjectURL(event.target.files[0]);
     photo.onload = function() {
