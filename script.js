20173f0b89de7a87d3570d193b0474f18d2f5a01
Alana Monteiro <lanaah.15@gmail.com>
2020-07-28 18:38:57 -0300

    ajuste no texto do meme

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 function textAdd() {
   let inputText = document.getElementById("text-input").value;
-  let textContainer = document.createElement("p");
-  textContainer.id = ("text-container");
-  let parentText = document.getElementById("meme-text");
-  parentText.appendChild(textContainer);
-
-  document.getElementById("text-container").innerHTML = inputText;
+    document.getElementById("meme-text").innerHTML = inputText;
 }
 
 const imageInput = document.getElementById("image-input")
