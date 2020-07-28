67cf6f17996bbd44ec3d2f726b8598ef9c59ca0e
Paulo Frazão <paulo4187@gmail.com>
2020-07-28 09:20:14 -0300

    alteração posicionamento css section meme-image-container

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function loadImage(event) {
     const reader = new FileReader();
     reader.onload = function () {
     const saida = document.getElementById('meme-image');
-      saida.src = reader.result;}
+      saida.src = reader.result }
     if (reader) {
-        reader.readAsDataURL(event.target.files[0]);}
+        reader.readAsDataURL(event.target.files[0])}
 }
 function loadText() {
   const inputText = document.querySelector('#text-input').value;
   const pText = document.querySelector('#meme-text');
-  pText.innerHTML = inputText;} 
+  pText.innerHTML = inputText } 
