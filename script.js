c59ccc7d99b0b9c0a71b0467a33cd175d223ef58
ricknois <rickssti@outlook.com>
2020-07-28 01:20:03 -0300

    Formatação do documento

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
 const imgUp = document.querySelector('#meme-insert');
 const imgShow = document.querySelector('#meme-image');
 const txtInput = document.querySelector('#text-input');
 const txtOutput = document.querySelector('#meme-text')
 
 function showImage(event) {
-    let imgSrc = event.target.files[0];
-    console.log(imgSrc);
-    imgShow.src = URL.createObjectURL(imgSrc);
+  let imgSrc = event.target.files[0];
+  console.log(imgSrc);
+  imgShow.src = URL.createObjectURL(imgSrc);
 }
 
 txtInput.addEventListener('keyup', function () {
-    txtOutput.innerHTML = txtInput.value;
+  txtOutput.innerHTML = txtInput.value;
 })
