f8b275013ad948e8bfa87447c56b22027b231258
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-07-28 16:40:34 -0300

    First Task Fixes 2

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 
 textInput.addEventListener('keyup', function () {
-  memeText.value = textInput.value;
+  memeText.innerText = textInput.value;
 });
