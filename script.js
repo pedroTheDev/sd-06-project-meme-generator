9f0d06d350d0525fe28406a74d585fff865d1407
Anderson R <misterregisslv@gmail.com>
2020-07-28 12:04:12 -0300

    coriigindo issues do codeclimate

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-window.onload = function() {
-    // função para inserir texto no meme
-     let campotexto = document.querySelector('#text-input');
-     console.log(campotexto);
-     let tagP = document.createElement('p');
-    campotexto.addEventListener('keyup', function () {
-        let conteudotexto = document.querySelector('#text-input').value;
-        tagP.innerHTML = conteudotexto
-        document.querySelector('#meme-text').appendChild(tagP);
-    });
-    //função para inserir imagem
-     inserirIMG = function(event) {
-        let imagem = document.querySelector('#meme-image');
-        imagem.src = URL.createObjectURL(event.target.files[0]);
-        imagem.onload = function() {
-          URL.revokeObjectURL(imagem.src);
-    }
-   
-}
-
-}
+window.onload = function () {
+// função para inserir texto no meme
+  const campotexto = document.querySelector('#text-input');
+  const tagP = document.createElement('p');
+  campotexto.addEventListener('keyup', function () {
+    const conteudotexto = document.querySelector('#text-input').value;
+    tagP.innerHTML = conteudotexto;
+    document.querySelector('#meme-text').appendChild(tagP);
+  });
+// função para inserir imagem
+  inserirIMG = function (event) {
+    const imagem = document.querySelector('#meme-image');
+    imagem.src = URL.createObjectURL(event.target.files[0]);
+    imagem.onload = function () {
+      URL.revokeObjectURL(imagem.src);
+    };
+  };
+};
