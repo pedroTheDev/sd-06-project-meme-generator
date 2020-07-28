fe6ec62055d41067c553dceff6cecb440458719a
LeonardoCavachini <cavachini240@gmail.com>
2020-07-27 22:49:25 -0300

    update3

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 //Adicionando texto.
 let text = document.getElementById('text-input');
 let texto = document.createElement('h1');
+texto.style.color ='white';
+texto.style.textShadow = '5px 5px 5px black';
+texto.style.fontSize = '30px';
+texto.style.position = 'absolute';
 document.getElementById('meme-text').appendChild(texto);
 
 text.addEventListener('input',function(){
-    
-    
-   
     texto.innerHTML = text.value;
-    texto.style.position = 'absolute';
-    texto.style.boxShadow = 'black 5px 5px 5px';
-    texto.style.fontSize = '30px';
-    texto.style.color = 'white';
-    
 });
 //Adicionando imagem.
-let container = document.getElementById('meme-image-container');
-container.style.border = '1px solid black';
-container.style.backgroundColor = 'white';
 
 let btnImg = document.getElementById('meme-insert');
 btnImg.addEventListener('input', function(event) {
-    let img = document.getElementById('meme-image');
+    let photo = document.createElement('img');
+    photo.classList.add('picture');
+    document.getElementById('meme-image').appendChild(photo);
     
-
-    img.src = URL.createObjectURL(event.target.files[0]);
-    img.onload = function() {
-        URL.revokeObjectURL(img.src)
+    photo.src = URL.createObjectURL(event.target.files[0]);
+    photo.onload = function() {
+        URL.revokeObjectURL(photo.src)
     }
-    img.style.maxWidth = '30%';
-    console.log()
+    
 });
