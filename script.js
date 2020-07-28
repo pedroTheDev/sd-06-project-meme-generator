ce63404fe707c41ed3fbbde72caf2857d0999625
thalesgr <thalesgr@gmail.com>
2020-07-28 13:34:01 -0300

    botoes de borda

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputTexto = document.querySelector('#text-input');
 const outputTexto = document.querySelector('#meme-text');
+const container = document.querySelector('.meme-container');
 
 inputTexto.addEventListener('input', (e) => {
   outputTexto.innerHTML = e.target.value;
@@ -11,3 +12,21 @@ const outputImagem = document.querySelector('#meme-image');
 inputImagem.addEventListener('change', (e) => {
   outputImagem.src = URL.createObjectURL(e.target.files[0]);
 });
+
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+
+fireButton.addEventListener('click', () => {
+  container.style.border = 'solid 5px red';
+});
+  
+waterButton.addEventListener('click', () => {
+  container.style.border = 'solid 5px blue';
+});
+
+earthButton.addEventListener('click', () => {
+  container.style.border = 'solid 5px green'
+});
+
+
