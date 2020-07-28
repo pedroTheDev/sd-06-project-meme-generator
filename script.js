d8443fd00abdc506ca4eb448e652b81f0205ae73
dilenio <dilenio@gmail.com>
2020-07-28 11:11:06 -0300

    1 - Correções #1

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Meme Generator
+const textInput = document.querySelector('#text-input');
+textInput.addEventListener('keyup', function() {
+  const showText = document.querySelector('#meme-text');
+  showText.innerHTML = textInput.value;
+});
