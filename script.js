3023221a6181ffb7beb12c6af5a8a1928a5095ba
ricknois <rickssti@outlook.com>
2020-07-28 18:18:11 -0300

    ...

--- script.js
+++ script.js
@@ -17,10 +17,7 @@ txtInput.addEventListener('keyup', function () {
   }
 });
 
-// imgData.addEventListener('click',function (event) {
-//   let teste =event.target.src.split("0", 5)
-//   
-// })
+
 function imgDb(event) {
   let teste =event.target.src.split("0", 5)
   imgShow.src = teste[4];
