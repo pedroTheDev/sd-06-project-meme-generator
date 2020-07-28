fb1cf3dbc57f0c26ce385ebd2b371ec4f95903a6
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-28 15:26:44 -0300

    adding imgs

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const container = document.querySelector('#meme-image-container');
 const fireButton = document.getElementById('fire');
 const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
+const preDefinedMemeOne = document.getElementById('meme-1');
+const preDefinedMemeTwo = document.getElementById('meme-2');
+const preDefinedMemeThree = document.getElementById('meme-3');
+const preDefinedMemeFour = document.getElementById('meme-4');
 
 textInput.addEventListener('keyup', function () {
   memeText.innerText = textInput.value;
@@ -32,4 +36,20 @@ waterButton.addEventListener('click', function () {
 
 earthButton.addEventListener('click', function () {
   container.style.border = 'groove green 6px';
+});
+
+preDefinedMemeOne.addEventListener('click', function () {
+  image.src = './imgs/meme1.png';
+});
+
+preDefinedMemeTwo.addEventListener('click', function () {
+  image.src = './imgs/meme2.png';
+});
+
+preDefinedMemeThree.addEventListener('click', function () {
+  image.src = './imgs/meme3.png';
+});
+
+preDefinedMemeFour.addEventListener('click', function () {
+  image.src = './imgs/meme4.png';
 });
