87d732681a057f3ae831e0c822ecb829b932406f
Paulo Frazão <paulo4187@gmail.com>
2020-07-28 09:39:15 -0300

    tratamento regras CodeClimate

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 function loadImage(event) {
-    const reader = new FileReader();
-    reader.onload = function () {
-    const saida = document.getElementById('meme-image');
-      saida.src = reader.result }
-    if (reader) {
-        reader.readAsDataURL(event.target.files[0])}
+  const reader = new FileReader();
+  reader.onload = function () {
+      const saida = document.getElementById('meme-image');
+    saida.src = reader.result;
+   }
+  if (reader) {
+    reader.readAsDataURL(event.target.files[0]);
+  }
 }
 function loadText() {
   const inputText = document.querySelector('#text-input').value;
   const pText = document.querySelector('#meme-text');
-  pText.innerHTML = inputText } 
+  pText.innerHTML = inputText;
+}
