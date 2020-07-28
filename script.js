ff5264f78270d48421d0246e7fbc0980bf65fa7e
LeonardoCavachini <cavachini240@gmail.com>
2020-07-27 23:15:24 -0300

    texto e imagem

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //Adicionando texto.
 let text = document.getElementById('text-input');
-let texto = document.createElement('h1');
-texto.style.color ='white';
-texto.style.textShadow = '5px 5px 5px black';
-texto.style.fontSize = '30px';
-texto.style.position = 'absolute';
-document.getElementById('meme-text').appendChild(texto);
 
 text.addEventListener('input',function(){
-    texto.innerHTML = text.value;
+    let note = document.getElementById('meme-text');
+    note.innerHTML = text.value;
+    note.style.color = 'white';
+    note.style.fontSize = '30px';
+    note.style.textShadow = '5px 5px 5px black';
+    note.style.position = 'absolute';
+    note.style.marginTop = '-40px';
 });
 //Adicionando imagem.
 
 let btnImg = document.getElementById('meme-insert');
 btnImg.addEventListener('input', function(event) {
     let photo = document.createElement('img');
-    photo.classList.add('picture');
+    
     document.getElementById('meme-image').appendChild(photo);
     
     photo.src = URL.createObjectURL(event.target.files[0]);
