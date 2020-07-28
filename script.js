2422d8a9dbc811584e419d7caedac99b055ec63e
LeonardoCavachini <cavachini240@gmail.com>
2020-07-28 00:49:32 -0300

    texto sobre img

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 //Adicionando texto.
 let text = document.getElementById('text-input');
-
 text.addEventListener('input',function(){
     let note = document.getElementById('meme-text');
     note.innerHTML = text.value;
-   
 });
 //Adicionando imagem.
-let box = document.getElementById('meme-image-container');
-box.style.maxWidth = '100%';
 let btnImg = document.getElementById('meme-insert');
 btnImg.addEventListener('input', function(event) {
-    
-    
-   let photo = document.getElementById('meme-image');
-   /*photo.style.maxWidth = 'inherit';
-   photo.style.backgroundColor = 'white';
-   photo.style.border = '1px solid black';
-    */
-    photo.src = URL.createObjectURL(event.target.files[0]);
-    photo.onload = function() {
-        URL.revokeObjectURL(photo.src)
+let photo = document.getElementById('meme-image');
+photo.src = URL.createObjectURL(event.target.files[0]);
+photo.onload = function() {
+    URL.revokeObjectURL(photo.src)
     }
     
 });
