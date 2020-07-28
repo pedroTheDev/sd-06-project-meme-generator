7aa562cde23be91a920b25b2c982a4d6eafb7900
Veruska Bittencourt <sousaveruska@gmail.com>
2020-07-28 20:26:41 -0300

    modificaçao3

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function printTexto(){
     let textoMeme = document.querySelector("#meme-text");
     textoMeme.innerHTML = texto;
 }
-
+let imgMeme = function(event) {
+    var memeImage = document.getElementById('meme-image');
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    memeImage.onload = function() {
+      URL.revokeObjectURL(memeImage.src) 
+    }
+};
