b8fe9a465cc77c5597280f86f04265c2f5ce0d0f
LeonardoCavachini <cavachini240@gmail.com>
2020-07-28 00:27:25 -0300

    photo

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ btnImg.addEventListener('input', function(event) {
     
    let photo = document.getElementById('meme-image');
    photo.style.maxWidth = 'inherit';
+   photo.style.backgroundColor = 'white';
+   photo.style.border = '1px solid black';
     
     photo.src = URL.createObjectURL(event.target.files[0]);
     photo.onload = function() {
