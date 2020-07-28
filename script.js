2c3872937539434ab70464cc1fc895e26b3f209f
Ana Flávia <anaflvia@gmail.com>
2020-07-28 17:33:13 -0300

    Estético

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let textoDoMeme = document.querySelector('#text-input');
 let inputImg = document.querySelector('#meme-insert');
-textoDoMeme.addEventListener('input', function () {
+
+textoDoMeme.addEventListener('input', function (event) {
   let texto = document.querySelector('#meme-text');
-  texto.innerHTML = textoDoMeme.value;
+  texto.innerHTML = event.target.value;
 });
+
 function visualizarImg(event) {
   let preview = document.querySelector('img');
+  preview.style.backgroundColor
   preview.src = URL.createObjectURL(event.target.files[0]);
   preview.onload = function() {
     URL.revokeObjectURL(preview.src);
   }
 }
+
 inputImg.addEventListener('change', visualizarImg)
