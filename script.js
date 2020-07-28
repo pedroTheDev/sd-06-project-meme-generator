1652a75cfd5f81d414e32f71f788767951d9898b
CristianoNunes <cristianonunes99@gmail.com>
2020-07-27 22:47:15 -0300

    Requisito 7 - Memes Pre Prontos

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ function changeBorderWater() {
 function changeBorderEarth() {
     document.getElementById('meme-image-container').style.border = "6px groove green";
 }
+
+function selectImg1() {
+    let url = document.getElementById('meme-1').src;
+    let img = document.getElementById('meme-image');
+    img.src = url;
+}
+
+function selectImg2() {
+    let url = document.getElementById('meme-2').src;
+    let img = document.getElementById('meme-image');
+    img.src = url;
+}
+
+function selectImg3() {
+    let url = document.getElementById('meme-3').src;
+    let img = document.getElementById('meme-image');
+    img.src = url;
+}
+
+function selectImg4() {
+    let url = document.getElementById('meme-4').src;
+    let img = document.getElementById('meme-image');
+    img.src = url;
+}
