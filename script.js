55ecf7c1d357f55fb6c043698d5f94cea6c39d63
Pablo Santos <pablo.trybe@gmail.com>
2020-07-28 16:42:02 -0300

    Requisitos obrigatorios concluidos

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ function printTextMeme() {
   memeText.innerHTML = textInMeme;
 }
 
-// const loadFile = function (event) {
-//   const output = document.getElementById('output');
-//   output.src = URL.createObjectURL(event.target.files[0]);
-//   output.onload = function () {
-//     URL.revokeObjectURL(output.src);
-//   }
-// };
-
 inputText.addEventListener('keyup', printTextMeme);
 
 const output = document.getElementById('meme-insert');
