97b0cfc280c33d91c7ac3bad47c9a4d22e14f019
us19861229c <rcks.santos@gmail.com>
2020-07-28 16:10:11 -0300

    Corrige posição dos inputs, com relação aos labels. Habilita a funcionalidade de upload de imagem.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const inputDoMeme = document.querySelector('#text-input');
 const paragrafoDoMeme = document.querySelector('#meme-text');
 
-inputDoMeme.addEventListener('focusout', function () {
+const inputDaImagem = document.querySelector('#meme-insert');
+const imagemDoMeme = document.querySelector('#meme-image')
+
+inputDoMeme.addEventListener('keyup', function () {
   paragrafoDoMeme.innerText = inputDoMeme.value;
+});
+inputDoMeme.addEventListener('change', function () {
+  paragrafoDoMeme.innerText = inputDoMeme.value;
+});
+
+inputDaImagem.addEventListener('change', function () {
+  let arquivoImagem = inputDaImagem.files[0];
+  document.querySelector('img').src = URL.createObjectURL(arquivoImagem)
 });
