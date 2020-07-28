b26c2d867dc9ffa3e2e03931b96d54266fae6224
Igor Leão <igorleao.eng@gmail.com>
2020-07-28 08:25:00 -0300

    size words and text1

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const loadFile = function(event) {
-  var reader = new FileReader();
-  reader.onload = function(){
-    var output = document.getElementById('output');
-    output.src = reader.result;
+const loadFile = function (event) {
+  image.src = URL.createObjectURL(event.target.files[0]);
+  image.onload = function () {
+    URL.revokeObjectURL(image.src);
   };
-  reader.readAsDataURL(event.target.files[0]);
 };
 const textInput = document.querySelector('#text-input');
 const memeInsert = document.querySelector('#meme-text');
 
-textInput.addEventListener('keyup', function(){
+textInput.addEventListener('keyup', function () {
   memeInsert.innerText = textInput.value;
 })
 
