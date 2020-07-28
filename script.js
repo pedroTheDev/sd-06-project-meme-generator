8a8bcd88e2d59935afa2c5210b0b8ef056977a04
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-07-28 16:13:04 -0300

    ajustado html, adicionado eventos no js, adicionado classes no style

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let text = document.getElementById('text-input');
+window.onload = function() {
+
+  const inputDeTextop = document.querySelector("#text-input")
+  inputDeTextop.addEventListener("keyup", function() {
+    document.querySelector("#meme-text").innerHTML = inputDeTextop.value
+  })
+
+  const localImagem = document.querySelector("#meme-insert")
+  localImagem.addEventListener("change", function(event) {
+    document.getElementById("meme-image").src = URL.createObjectURL(event.target.files[0]);
+  })
+
+}
