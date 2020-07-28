8adb39ef83b4121a2baa81d3f1e807ac3a51591e
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-28 01:19:26 -0300

    bonus

--- script.js
+++ script.js
@@ -33,3 +33,63 @@ let textInput = document.querySelector("#text-input");
 textInput.addEventListener("input", onChangeText);
 
 
+//avatar-buttons
+
+//fire-button
+
+function changeFireBorder(){
+    document.getElementById("meme-image").style.border = "3px dashed red";
+}
+
+//
+
+let fireBorder = document.getElementById("fire");
+fireBorder.addEventListener("click", function(){
+    changeFireBorder();
+})
+
+
+
+//water-button
+
+function changeWaterBorder(){
+    document.getElementById("meme-image").style.border = "5px double blue";
+}
+
+//
+
+let waterBorder = document.getElementById("water");
+waterBorder.addEventListener("click", function(){
+    changeWaterBorder();
+})
+
+
+
+//earth-button
+
+function changeEarthBorder(){
+    document.getElementById("meme-image").style.border = "6px groove green";
+}
+
+//
+
+let earthBorder = document.getElementById("earth");
+earthBorder.addEventListener("click", function(){
+    changeEarthBorder();
+})
+
+
+//earth-button
+
+function changeAirBorder(){
+    document.getElementById("meme-image").style.border = "3px dotted yellow";
+}
+
+//
+
+let airBorder = document.getElementById("air");
+airBorder.addEventListener("click", function(){
+    changeAirBorder();
+})
+
+//
