226c4ef3d0ed737386c3780c67a86fbe077bd946
Stefano Branz <stefano.branz@gmail.com>
2020-07-28 17:24:24 -0300

    Requisit-2 first try

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 window.onload = function() {
-
+  // Variables to interact with inputed text and image
   let userTextInput = document.getElementById("text-input");
   let memeTextInputed = document.getElementById("meme-text");
 
+  // Variables to interact with inputed upload image
+  let imageUploader = document.getElementById('meme-insert');
+  let inputedImage = document.getElementById('meme-image');
+
+  // Function to interact with inputed text and image
   userTextInput.addEventListener("keyup", function() {
     memeTextInputed.innerText = userTextInput.value;
+  });
+
+  // Function to change current image to uploaded image
+  imageUploader.addEventListener("change", function(event) {
+    inputedImage.src = URL.createObjectURL(event.target.files[0]);
   });
 }
