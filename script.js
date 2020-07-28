19b7e9d59c48d65aee0c291c33aca35e47a88232
Igor Leão <igorleao.eng@gmail.com>
2020-07-27 21:57:13 -0300

    meme vamos la

---
+++ script.js
@@ -0,0 +1,14 @@
+var loadFile = function(event) {
+  var reader = new FileReader();
+  reader.onload = function(){
+    var output = document.getElementById('output');
+    output.src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
+};
+const text = document.getElementById('text-input');
+const memeInsert = document.querySelector('#meme-insert');
+text.addEventListener('', function(){
+  memeIsert.innerText = text.value;
+})
+const containder = document.getElementById('meme-image-container');
