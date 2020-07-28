6930cff8b63df80dab109351079cacc0b6e2cd22
LeonardoCavachini <cavachini240@gmail.com>
2020-07-28 10:23:52 -0300

    memes pré prontos

--- script.js
+++ script.js
@@ -30,3 +30,20 @@ btnEarth.addEventListener('click',function() {
     let estilo = document.getElementById('meme-image-container');
     estilo.style.border = '6px groove green';
 });
+// memes pŕe prontos.
+let pic1 = document.getElementById('meme-1');
+pic1.addEventListener('click',function() {
+   document.getElementById('meme-image').src = pic1.src;
+});
+let pic2 = document.getElementById('meme-2');
+pic2.addEventListener('click',function() {
+   document.getElementById('meme-image').src = pic2.src;
+});
+let pic3 = document.getElementById('meme-3');
+pic3.addEventListener('click',function() {
+   document.getElementById('meme-image').src = pic3.src;
+});
+let pic4 = document.getElementById('meme-4');
+pic4.addEventListener('click',function() {
+   document.getElementById('meme-image').src = pic4.src;
+});
