15e4d2806b487b669f03a2e2525e094641a77622
Victor Bergamini <vbergadev@gmail.com>
2020-07-28 15:19:31 -0300

    commit upload image

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-function gerarmeme() {
-    let imagemMeme = document.getElementById("imagem-meme").files[0];
+/*function gerarmeme() {
+    let imagemMeme = document.getElementById("imagem-meme");
     //alert(imagemMeme);
     let meme = document.getElementById("upload");
     meme.src = imagemMeme;
-   
-}
+    meme.addEventListener('change')   
+}*/
+var loadFile = function(event) {
+    var upload = document.getElementById('upload');
+    upload.src = URL.createObjectURL(event.target.files[0]);
+    upload.onload = function() {
+      URL.revokeObjectURL(upload.src) // free memory
+    }
+  };
 function texto() {
     let textoMeme = document.getElementById("text-input").value;
      document.getElementById("meme-text").innerHTML=textoMeme;
 }
+
+
+  
