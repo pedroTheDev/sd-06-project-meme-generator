63ec06f0fb82c7c29622974e424f6cd21dd2487a
ilanribeiro <ilanribeiro@gmail.com>
2020-07-28 17:51:16 -0300

    updating correction codeclimate issues

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ window.onload = function () {
   const memeTxtInImg = document.querySelector('#meme-text');
   const imgInContainer = document.querySelector('#meme-image');
 
-  // events
-  // hear typed text and transfer to image
-  txtToInput.addEventListener('keyup', gettxtToInput);
-  // transfer the chosen image to the meme container
-  imgToInput.addEventListener('change', getImgToInput);
-
   // functions
   // add txt to image in meme container
   function gettxtToInput() {
@@ -21,36 +15,9 @@ window.onload = function () {
     imgInContainer.src = URL.createObjectURL(imgToInput.files[0]);
   }
 
+  // events
+  // hear typed text and transfer to image
+  txtToInput.addEventListener('keyup', gettxtToInput);
+  // transfer the chosen image to the meme container
+  imgToInput.addEventListener('change', getImgToInput);
 }
-
-
-
-
-
-
-
-
-
-
-
-// window.onload = function() {
-
-//   let textInput = document.querySelector('#text-input');
-//   let memeText = document.querySelector('#meme-text');
-//   let imgInput = document.querySelector('.imageAddButton');
-//   let memeImg = document.querySelector('.meme-img');
-
-//   textInput.addEventListener('keyup', memeHearText);
-//   function memeHearText() {
-//     memeText.innerText = textInput.value;
-//   }
-
-//   imgInput.addEventListener('change', changeImg);
-//   function changeImg() {
-//     memeImg.src = window.URL.createObjectURL(this.files[0])
-
-//   }
-//   // imgInput.addEventListener('change', function() {
-//   //   memeImg.src = imgInput.files[0].name;
-//   // })
-// }
