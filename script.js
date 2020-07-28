4ae3490695f4b2563b323bb0637d5db561828f3f
dilenio <dilenio@gmail.com>
2020-07-28 15:43:35 -0300

    3 - O site deve ter uma moldura no container. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco.

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ textInput.addEventListener('keyup', function () {
   const showText = document.querySelector('#meme-text');
   showText.innerHTML = textInput.value;
 });
+
+const imageUpload = document.querySelector('#meme-insert');
+imageUpload.addEventListener('change', function (event) {
+  const output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src);
+  };
+});
