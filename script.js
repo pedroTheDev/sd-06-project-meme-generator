ad4f2dc8b97f35ce2eba6569c5e1bb3f06f9675b
Rods <rods.leite27@gmail.com>
2020-07-29 18:25:53 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const textInput = document.getElementById('text-input')
 const memeText = document.getElementById('meme-text')
+const fire = document.getElementById('fire')
+const water = document.getElementById('water')
+const earth = document.getElementById('earth')
+const imgContainer = document.getElementById('meme-image-container')
+
 textInput.addEventListener('input', function(){
-    memeText.innerText = textInput.value
+  memeText.innerText = textInput.value
+  memeText.style.width = '20px'
 })
+
 const photoInsert = document.getElementById('meme-insert');
 const memeImage =  document.getElementById('meme-image');
 photoInsert.addEventListener('change', function() {
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    memeImage.style.maxWidth = '100%';
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+})
+
+fire.addEventListener('click', function () {
+  imgContainer.style.border = '3px dashed crimson'
+})
+
+water.addEventListener('click', function () {
+  imgContainer.style.border = '5px double royalblue'
+})
+
+earth.addEventListener('click', function () {
+  imgContainer.style.border = '6px groove seagreen'
 })
