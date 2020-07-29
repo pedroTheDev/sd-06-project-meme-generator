53ce4160af5d0ded06a11a61c6a117a08f3d4e37
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-29 16:33:11 -0300

    fix codeclimate

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createMemeText(event) {
 }
 
 function memeInsert(event) {
-  let output = document.getElementById('meme-image');
+  const output = document.getElementById('meme-image');
   output.src = URL.createObjectURL(event.target.files[0]);
   output.onload = function () {
     URL.revokeObjectURL(output.src);
