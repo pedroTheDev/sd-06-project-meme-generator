292ab380e722b22eb5dafac79f55c2593ac052eb
bragammartins <bragammartins@gmail.com>
2020-07-29 17:34:28 -0300

    4 - correcting codeclimate errors

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const textInput = document.querySelector('#text-input');
 const textInside = document.querySelector('#meme-text');
-// let imageInput = document.querySelector("#meme-insert");
-// let imageInside = document.querySelector("#meme-image");
+const output = document.getElementById('meme-image');
 
 function changeText() {
   textInside.innerText = textInput.value;
   textInside.className = 'meme-text';
 }
-textInput.addEventListener('keyup', changeText);
+textInput.addEventListener('keyup', changeText)
 
-
-function (event) {
-  const output = document.getElementById('meme-image');
+let loadFile = function (event) {
   output.src = URL.createObjectURL(event.target.files[0]);
   output.onload = function () {
     URL.revokeObjectURL(output.src); // free memory
