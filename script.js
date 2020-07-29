8bf1bd41f1aef43cfc20229f3e8499c0aa371d8a
Veruska Bittencourt <sousaveruska@gmail.com>
2020-07-28 22:50:32 -0300

    correçoes Html/JS/CSS

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-function printTexto(){
-    let textoEscrito = document.getElementById("text-input");
-    let textoMeme = document.getElementById("meme-text");
-    let memeInsert= document.getElementById("meme-insert")
+    
+window.onload=function(){
+    const textoEscrito = document.getElementById("text-input");
+    const textoMeme = document.getElementById("meme-text");
+    const memeInsert = document.getElementById("meme-insert");
+    const memeImg = document.getElementById("meme-image");
 
-    textoEscrito.addEventListener('change', function(){
-        const texto = textoEscrito.value;
-        if(texto.length >60){
-            alert('Limite de caracteres ultrapassado!');
-        }else{
-            textoMeme.innerHTML = texto;
-        }
-    }
-    )
-   
-}
-let imgMeme = function(event) {
-    var memeImage = document.getElementById('meme-image');
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    memeImage.onload = function() {
-      URL.revokeObjectURL(memeImage.src) 
-    }
+    textoEscrito.addEventListener('input', function() {
+        textoMeme.innerHTML = textoEscrito.value; 
+    })
+
+    memeInsert.addEventListener('change', function(event) {
+    memeImg.src = URL.createObjectURL(event.target.files[0]);
+    memeImg.onload = function() {
+      URL.revokeObjectURL(memeImg.src);
+    };
+    });
 };
