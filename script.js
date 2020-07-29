b73d464a81c45a4a5648ec34ae093dac9383888c
bragammartins <bragammartins@gmail.com>
2020-07-29 12:53:47 -0300

    nexto na tela, mas figura nao carrega

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.write('Vamos começar o nosso projeto')
+function carregar(){
+  var textInput = document.querySelector("#text-input");
+  var textInside = document.querySelector("#meme-text");
+
+  textInput.addEventListener("keyup", function(){
+      textInside.innerText = textInput.value;
+      textInside.className = "meme-text";
+  })
+
+  // var imageInput = document.querySelector("#meme-insert");
+  // var imageInside = document.querySelector("#meme-image");
+  //   var loadFile = function(event) {
+  //   var output = document.getElementById('output');
+  //   output.src = URL.createObjectURL(event.target.files[0]);
+  //   output.onload = function() {
+  //     URL.revokeObjectURL(output.src)
+  //   }
+  // };
+
+
+  imageInput.addEventListener("input", function(event){
+  imageInside.src = URL.createObjectURL(event.target.files[0])
+  })
+}
