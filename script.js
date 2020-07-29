60c308f43d0a98e2937312a3ecfbd4603f4f5d14
luiz <phellipe501@gmail.com>
2020-07-29 14:57:57 -0300

    projeto completo

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ window.onload = function(){
     var textInside = document.querySelector("#meme-text");
     var imageInput = document.querySelector("#meme-insert");
     var imageInside = document.querySelector("#meme-image");
+    var fire = document.querySelector("#fire");
+    var water = document.querySelector("#water");
+    var earth = document.querySelector("#earth");
+    var caixa = document.querySelector("#meme-image-container");
+    var meme1 = document.querySelector("#meme-1");
+    var meme2 = document.querySelector("#meme-2");
+    var meme3 = document.querySelector("#meme-3");
+    var meme4 = document.querySelector("#meme-4");
+
+
+
+
     textInput.addEventListener("keyup", function(){
         textInside.innerText = textInput.value;
         textInside.className = "espec-text";
@@ -11,4 +23,37 @@ window.onload = function(){
     imageInput.addEventListener("change", function(event){
         imageInside.src = URL.createObjectURL(event.target.files[0])
     })
+
+
+    fire.addEventListener("click", function(){
+        caixa.style.border = "3px dashed red";
+    })
+
+    water.addEventListener("click", function(){
+        caixa.style.border = "5px double blue";
+    })
+
+    earth.addEventListener("click", function(){
+        caixa.style.border = "6px groove green";
+    })
+
+
+
+
+    meme1.addEventListener("click", function(){
+        imageInside.src = "imgs/meme1.jpg"
+    })
+
+    meme2.addEventListener("click", function(){
+        imageInside.src = "imgs/meme2.jpg"
+    })
+
+    meme3.addEventListener("click", function(){
+        imageInside.src = "imgs/meme3.jpg"
+    })
+
+    meme4.addEventListener("click", function(){
+        imageInside.src = "imgs/meme4.jpg"
+    })
+
 }
