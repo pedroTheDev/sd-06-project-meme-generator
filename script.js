b39e1488e3265f4ad10a05e5cd7ec3b64822a747
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-29 16:05:21 -0300

    add meme-galery

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-window.onload = function () {
-  function createMemeText(event) {
-    document.getElementById('meme-text').innerText = event.target.value;
-  }
-  function memeInsert(event) {
-    var output = document.getElementById('meme-image');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      URL.revokeObjectURL(output.src)
-    }
-  }
+function selectMemeImage(event) {
+  document.getElementById('meme-image').src = event.target.src;
+}
 
-  function borderInsert(event) {
-    document.getElementById('meme-image-container').classList.remove('fire', 'earth', 'water');
-    document.getElementById('meme-image-container').classList.add(event.target.id);
-  }
-  
+function createMemeText(event) {
+  document.getElementById('meme-text').innerText = event.target.value;
+}
+
+function memeInsert(event) {
+  let output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src);
+  };
+}
+
+function borderInsert(event) {
+  document.getElementById('meme-image-container').classList.remove('fire', 'earth', 'water');
+  document.getElementById('meme-image-container').classList.add(event.target.id);
+}
+
+window.onload = function () {
   document.getElementById('text-input').addEventListener('input', createMemeText);
   document.getElementById('meme-insert').addEventListener('change', memeInsert);
   document.getElementById('fire').addEventListener('click', borderInsert);
   document.getElementById('water').addEventListener('click', borderInsert);
   document.getElementById('earth').addEventListener('click', borderInsert);
+  document.getElementById('meme-galery').addEventListener('click', selectMemeImage);
 };
