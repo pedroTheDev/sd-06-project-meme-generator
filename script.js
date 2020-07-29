9b7791cf2fb7467a5f911c7f2472238280632ac7
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-29 14:55:06 -0300

    add style buttons, borderInsert func

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ window.onload = function() {
     var output = document.getElementById('meme-image');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
-      URL.revokeObjectURL(output.src) // free memory
+      URL.revokeObjectURL(output.src)
     }
+  }
+
+  function borderInsert(event) {
+    document.getElementById('meme-image-container').classList.remove('fire', 'earth', 'water');
+    document.getElementById('meme-image-container').classList.add(event.target.id);
   }
   
   document.getElementById('text-input').addEventListener('input', createMemeText);
   document.getElementById('meme-insert').addEventListener('change', memeInsert);
+  document.getElementById('fire').addEventListener('click', borderInsert);
+  document.getElementById('water').addEventListener('click', borderInsert);
+  document.getElementById('earth').addEventListener('click', borderInsert);
 };
