06eea4dbc8b9a51d2e45a686fba0404d75e9b376
Julia Porto <juportob@gmail.com>
2020-07-29 18:15:53 -0300

    Requisito 6 - bônus

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ window.onload = function() {
   document.getElementById("meme-image").src = URL.createObjectURL(event.target.files[0]);
   })
   
+  let btnFire = document.querySelector("#fire")
+  let btnWater = document.querySelector("#water")
+  let btnEarth = document.querySelector("#earth")
+  let container = document.querySelector("#meme-image-container")
+
+  btnFire.addEventListener("click", function (){
+    container.style.border = "3px dashed red"
+  })
+
+  btnWater.addEventListener("click", function (){
+    container.style.border = "5px double blue"
+  })
+
+  btnEarth.addEventListener("click", function (){
+    container.style.border = "6px groove green"
+  })
 }
