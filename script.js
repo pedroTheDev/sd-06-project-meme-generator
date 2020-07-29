516101e929d6bd1bd45c8208bbf1af5cae4f44d9
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-29 15:57:05 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-window.onload = function(){
-    const textInput = document.querySelector("#text-input");
-    textInput.addEventListener("keyup", function(){
-        const text = document.querySelector("#meme-text");
-        text.innerHTML = textInput.value;
-    })
-    const imageInput = document.querySelector("#meme-insert");
-    imageInput.addEventListener("change", function(){
-        const image = document.querySelector("#meme-image");
-        image.src = imageInput.files[0].name;
-    })    
+const textInput = document.querySelector("#text-input");
+textInput.addEventListener("keyup", function(){
+    const text = document.querySelector("#meme-text");
+    text.innerHTML = textInput.value;
+})
+const imageInput = document.querySelector("#meme-insert");
+imageInput.addEventListener("change", function(){
+    const image = document.querySelector("#meme-image");
+    image.src = imageInput.files[0].name;
+})
+window.onclick = function() {
+    const container = document.querySelector("#meme-image-container");
+    if(event.target.id === "fire") {
+        container.style.border = "dashed 3px red";
+    }
+    if(event.target.id === "water") {
+        container.style.border = "double 5px blue";
+    }
+    if(event.target.id === "earth") {
+        container.style.border = "groove 6px green";
+    }    
 }
