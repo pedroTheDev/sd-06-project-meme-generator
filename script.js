274fc325324d4c727f88d21312ef614560ddfb3f
IzeldaValeriano <zel.valeriano@gmail.com>
2020-07-29 16:36:07 -0300

    iseri texto e imagem

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+let insiraSeuTexto = document.getElementById("text-imput");
+let insiraSuaimagem = document.getElementById("meme-insert");
+let apareceTextonaImagem = document.getElementById("meme-text");
+let carregaMeMeimagem = document.getElementById("meme-image");
+let imagemNaCaixa = document.getElementById("meme-image-container");
+
+//insiraSeuTexto.addEventListener("evento",function() {} ) // imput/change
+
+insiraSeuTexto.addEventListener("input",function(){
+apareceTextonaImagem.innerText = insiraSeuTexto.value;
+}); 
+
+insiraSuaimagem.addEventListener("change",function(){
+carregaMeMeimagem.src = URL.createObjectURL(event.target.files[0]);
+carregaMeMeimagem.style.maxWidth = "100%";
+carregaMeMeimagem.onload = function(){
+  URL.revokeObjectURL(carregaMeMeimagem.src)
+}
+});
+
+//carregaMeMeimagem.addEventListener("evento",function(){})
+
+//imagemNaCaixa.addEventListener("evento",function(){})      
