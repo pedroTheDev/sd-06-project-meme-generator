017ebf0a55cd7d0cb1eb20887d4018ce30dd30fa
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-07-28 23:31:26 -0300

    Bonus Task 2

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const memeImage = document.getElementById('meme-image');
 const btnFire = document.getElementById('fire');
 const btnWater = document.getElementById('water');
 const btnEarth = document.getElementById('earth');
+const memeOne = document.getElementById('meme-1');
+const memeTwo = document.getElementById('meme-2');
+const memeThree = document.getElementById('meme-3');
+const memeFour = document.getElementById('meme-4');
 
 textInput.addEventListener('keyup', function () {
   if (document.querySelector('img')) {
@@ -33,4 +37,20 @@ btnWater.addEventListener('click', function () {
 
 btnEarth.addEventListener('click', function () {
   document.getElementById('meme-image-container').style.border = '6px groove green';
+});
+
+memeOne.addEventListener('click', function () {
+  memeImage.src = memeOne.src;
+});
+
+memeTwo.addEventListener('click', function () {
+  memeImage.src = memeTwo.src;
+});
+
+memeThree.addEventListener('click', function () {
+  memeImage.src = memeThree.src;
+});
+
+memeFour.addEventListener('click', function () {
+  memeImage.src = memeFour.src;
 });
