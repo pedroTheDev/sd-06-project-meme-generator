95ece9ce7a598c327aab471544cc7208c0be7622
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-29 18:15:26 -0300

    Add shadow in text and fix font size

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const inputText = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+
 inputText.addEventListener('keyup', function () {
   memeText.innerHTML = inputText.value;
+});
+
+const memeInsert = document.querySelector('#meme-insert');
+const memeImg = document.querySelector('#meme-image');
+
+memeInsert.addEventListener('change', function(event){
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
 });
