8cc9cb90d732b9204cfd219c03f6e063eea1cf13
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-29 17:16:34 -0300

    identacao

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const textInput = document.querySelector('#text-input');
 textInput.addEventListener('keyup', function () {
-	const text = document.querySelector('#meme-text');
-	text.innerHTML = textInput.value;
+  const text = document.querySelector('#meme-text');
+  text.innerHTML = textInput.value;
 })
 const imageInput = document.querySelector('#meme-insert');
 imageInput.addEventListener('change', function () {
-	const image = document.querySelector('#meme-image');
-	image.src = imageInput.files[0].name;
+  const image = document.querySelector('#meme-image');
+  image.src = imageInput.files[0].name;
 })
 window.onclick = function () {
-	const container = document.querySelector('#meme-image-container');
-	if (event.target.id === 'fire') {
-		container.style.border = 'dashed 3px red';
-	}
-	if (event.target.id === 'water') {
-		container.style.border = 'double 5px blue';
-	}
-	if (event.target.id === 'earth') {
-		container.style.border = 'groove 6px green';
-	}
-	for (let i = 1; i < 5; i++) {
-		if (event.target.id === 'meme-' + i) {
-			const image = document.querySelector('#meme-image');
-			image.src = './imgs/meme' + i + '.png';
-		}
-	}
+  const container = document.querySelector('#meme-image-container');
+  if (event.target.id === 'fire') {
+    container.style.border = 'dashed 3px red';
+  }
+  if (event.target.id === 'water') {
+    container.style.border = 'double 5px blue';
+  }
+  if (event.target.id === 'earth') {
+    container.style.border = 'groove 6px green';
+  }
+  for (let i = 1; i < 5; i++) {
+    if (event.target.id === 'meme-' + i) {
+      const image = document.querySelector('#meme-image');
+      image.src = './imgs/meme' + i + '.png';
+    }
+  }
 }
