547f021026cf4a3227b02cdaa9611936ce316b24
lucasvr39 <lucasveras1994@gmail.com>
2020-07-29 18:54:37 -0300

    one more evaluator test

--- script.js
+++ script.js
@@ -5,14 +5,10 @@ userTextInput.addEventListener('input', function () {
   memeTextInput.innerText = userTextInput.value;
 });
 
-// return img choose by the user
-
-function loadFile(event) {
+const loadFile = function (event) {
   const uploadImg = document.getElementById('memesrc');
   uploadImg.src = URL.createObjectURL(event.target.files[0]);
   uploadImg.onload = function () {
     URL.revokeObjectURL(uploadImg.src);
   };
 }
-
-loadFile();
