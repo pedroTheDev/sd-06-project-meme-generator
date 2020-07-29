db1edcf4c52bf6ed1d92eb71d80fe594af88db15
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-29 13:57:53 -0300

    adcionado bonus 7

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ window.onload = function () {
     const waterButton = document.getElementById('water');
     const earthButton = document.getElementById('earth');
     const imgContainer = document.getElementById('meme-image-container');
+    const imgMeme1 = document.getElementById('meme-1');
+    const imgMeme2 = document.getElementById('meme-2');
+    const imgMeme3 = document.getElementById('meme-3');
+    const imgMeme4 = document.getElementById('meme-4');
 
     textInput.addEventListener('input', function () {
         memeText.innerHTML = textInput.value;
@@ -31,4 +35,16 @@ window.onload = function () {
         imgContainer.style.border = '6px groove green';
     });
 
+    imgMeme1.addEventListener('click', function() {
+        memeImg.src = imgMeme1.src;
+    });
+    imgMeme2.addEventListener('click', function () {
+        memeImg.src = imgMeme2.src;
+    });
+    imgMeme3.addEventListener('click', function () {
+        memeImg.src = imgMeme3.src;
+    });
+    imgMeme4.addEventListener('click', function () {
+        memeImg.src = imgMeme4.src;
+    });
 }
