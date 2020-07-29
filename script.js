fac9f5f5f6c08a42d5f86522e2c442c1b1f224ed
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-29 17:53:14 -0300

    final

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.onclick = function () {
   }
 };
 const imagens = document.querySelector('.imagens');
-imagens.addEventListener('click', function() {
+imagens.addEventListener('click', function () {
   for (let i = 1; i < 5; i += 1) {
     if (event.target.id === `meme-${i}`) {
       const image = document.querySelector('#meme-image');
