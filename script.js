3839503699dd81c0dd8b1c19bb4645e27d4fbb9d
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-07-28 23:11:51 -0300

    styling improvement & download option added

--- script.js
+++ script.js
@@ -36,12 +36,15 @@ function clickSuggestedMemes() {
   const meme2 = document.getElementById('meme-2');
   const meme3 = document.getElementById('meme-3');
   const meme4 = document.getElementById('meme-4');
-  const mainMeme = document.getElementById('meme-image');
+  const mainMeme = document.getElementById('placeholder');
   const memeCollection = [meme1, meme2, meme3, meme4];
 
   memeCollection.forEach((img) => {
     img.addEventListener('click', (event) => {
       mainMeme.src = event.target.src;
+      mainMeme.onload = () => {
+        drawImg();
+      }
     })
   });
 }
@@ -50,10 +53,14 @@ function clickSuggestedMemes() {
 
 function receiveImg() {
   const uploadFile = document.getElementById('meme-insert');
-  const mainMeme = document.getElementById('meme-image');
+  const mainMeme = document.getElementById('placeholder');
 
   uploadFile.addEventListener('change', (event) => {
     mainMeme.src = URL.createObjectURL(event.target.files[0]);
+    //sem essa parte do onload não funciona!
+    mainMeme.onload = () => {
+      drawImg();
+    }
   });
 }
 
@@ -61,16 +68,77 @@ function receiveImg() {
 
 function addText() {
   const inputText = document.getElementById('text-input');
+  const extraText = document.getElementById('extra-input');
+
+  extraText.addEventListener('input', (event) => {
+    // const memeText = document.getElementById('meme-text');
+    // memeText.innerHTML = event.target.value;
+    drawImg(event.target.value, 'bottom')
+  });
 
   inputText.addEventListener('input', (event) => {
-    const memeText = document.getElementById('meme-text')
-    memeText.innerHTML = event.target.value;
+    // const memeText = document.getElementById('meme-text');
+    // memeText.innerHTML = event.target.value;
+    drawImg(event.target.value, 'top')
   })
 }
 
+function makeItDownloadable(canvas) {
+  const downloadLink = document.querySelector("#download");
+  downloadLink.addEventListener('click', () => {
+    downloadLink.href = canvas.toDataURL();
+    downloadLink.download = 'meme.png';
+  });
+}
+
+function handleText(canvas, ctx, text, pos) {
+  switch (pos) {
+    case 'top':
+      if (text.length < 15) {
+        ctx.fillText(text, canvas.width/2, 40);
+      } else {
+        const text1 = text.slice(0, 15);
+        const text2 = text.slice(15, text.length);
+        ctx.fillText(text1, canvas.width/2, 40);
+        ctx.fillText(text2, canvas.width/2, 80);
+      }
+      break;
+    default:
+      if (text.length < 15) {
+        ctx.fillText(text, canvas.width/2, canvas.height - 20);
+      } else {
+        const text1 = text.slice(0, 15);
+        const text2 = text.slice(15, text.length);
+        ctx.fillText(text1, canvas.width/2, canvas.height - 60);
+        ctx.fillText(text2, canvas.width/2, canvas.height - 20);
+      }
+      break;
+  }
+
+}
+
+function drawImg(text = "", pos) {
+  const canvas = document.getElementById('meme-image');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const img = document.getElementById('placeholder');
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  ctx.font = 'bold 50px Rowdies, cursive';
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+
+  text = text.toUpperCase();
+
+  handleText(canvas, ctx, text, pos);
+
+  makeItDownloadable(canvas);
+
+}
+
 window.onload = () => {
+  drawImg();
+  receiveImg();
   clickSuggestedMemes();
   addBorders();
-  receiveImg();
   addText();
-};
+}
