01a15b66422c74368fc29854f3835506bf1368e7
Daniele Perse <danieleperse3@gmail.com>
2020-07-30 11:25:56 -0300

    adicionei um dos requsiitos bonus - margin no container, conforme seleçao do usuario

--- script.js
+++ script.js
@@ -9,10 +9,34 @@ function teclado() {
   texto.innerHTML = textoInput.value;
 }
 
+function changeMolduraFire() {
+  const container = document.getElementById('meme-image-container');
+  container.style.border = '3px dashed red';
+}
+
+function changeMolduraWater() {
+  const container = document.getElementById('meme-image-container');
+  container.style.border = '5px double blue';
+}
+
+function changeMolduraEarth() {
+  const container = document.getElementById('meme-image-container');
+  container.style.border = '6px groove green';
+}
+
 window.onload = function () {
   const textoInput = document.getElementById('text-input');
   textoInput.addEventListener('keyup', teclado);
 
   const memeInsert = document.getElementById('meme-insert');
   memeInsert.addEventListener('change', addImagem);
+
+  const fireButton = document.getElementById('fire');
+  fireButton.addEventListener('click', changeMolduraFire);
+
+  const waterButton = document.getElementById('water');
+  waterButton.addEventListener('click', changeMolduraWater);
+
+  const earthButton = document.getElementById('earth');
+  earthButton.addEventListener('click', changeMolduraEarth);
 };
