913e9d6326fd4c61c2b61568513e9e799e0f0662
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-29 21:11:13 -0300

    comencando codigo de botao de borda

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  //  Tomando imagen e colocando no container
+  //  Tomando imagen e colocando no container.
   const memeImage = document.createElement('img');
   loadFile = function (event) {
     const reader = new FileReader();
@@ -9,10 +9,21 @@ window.onload = function () {
     };
     reader.readAsDataURL(event.target.files[0]);
   };
-  //  Colocando o texto inserido em no container
+  //  Colocando o texto inserido no container.
   const textoInput = document.querySelector('#text-input');
   textoInput.addEventListener('keyup', function () {
     const texto = document.querySelector('#meme-text');
     texto.innerHTML = textoInput.value;
   });
+  // funcionalidad de agregar borda a la foto com os 
+  //respectivos botones.
+  let botaoFire = document.querySelector('#fire');
+  const botaoWater = document.querySelector('#water');
+  const botaoEarth = document.querySelector('#earth');
+  botaoFire.addEventListener('click', function () {
+    let theCSSprop = window.getComputedStyle(botaoFire, null).getPropertyValue("border");
+    document.querySelector('#meme-image-container').style.border = theCSSprop;
+
+  });
+
 };
