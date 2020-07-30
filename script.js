650e332464c7e9b58e7f52d49d233cc822f2cdde
bragammartins <bragammartins@gmail.com>
2020-07-29 23:08:36 -0300

    9 - subindo requerimento 7

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textInput = document.querySelector('#text-input');
 const textInside = document.querySelector('#meme-text');
 const imageInput = document.getElementById('meme-image');
+const imagePre = document.getElementById('meme-image');
 
 function changeText() {
   textInside.innerText = textInput.value;
@@ -14,3 +15,15 @@ const loadFile = function (event) {
   //   URL.revokeObjectURL(imageInput.src); // free memory
   // };
 };
+
+function earth() {
+  imgPre.className = 'meme-container earth';
+}
+
+function fire() {
+  imgPre.className = 'meme-container fire';
+}
+
+function water() {
+  imgPre.className = 'meme-container water';
+}
