1b53012e28fbacd61c2b971b68e95e2bdd66df83
IzeldaValeriano <zel.valeriano@gmail.com>
2020-07-30 16:17:19 -0300

    nova função

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-// criar variáveis 
-const textInput = document.getElementById("text-input");
-const memeText = document.getElementById("meme-text");
-const memeImage = document.getElementById("meme-image");
-const memeInsert = document.getElementById("meme-isert");
 
-// criar funções 
-
-// função para inserir frase na imagem:
-function iserirfrase(){
-    memeText.innerHTML = textInput.Value;
+window.onload = function(){
+    let insiraSeuTexto = document.querySelector("#text-input");
+    
+    insiraSeuTexto.addEventListener("keyup", function(){
+ document.querySelector("#meme-text").innerHTML = insiraSeuTexto.value 
+    })
 }
-textInput.addEventListener("keyup", iserirfrase);
-
-// função upload imagem 
-function oploadimage(){
-    memeImage.src = URL.createObjectURL(memeInsert.files[0]);
-}
-memeInsert.addEventListener("change", oploadimage);
