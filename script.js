584c8fa5a465b5e203ae95085b65e95762ebb000
DekoPossas <andrepossaster@gmail.com>
2020-07-29 23:23:27 -0300

    adicionei funcoes de aparecer imagem junto com o click do teclado, alterei algumas css.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const txtInput = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text ');
+const memeText = document.querySelector('#meme-text');
+const imageShow = document.querySelector('#meme-image');
 
 txtInput.addEventListener('keyup', function () {
   memeText.innerHTML = txtInput.value;
+  if (memeText === 1) {
+    showImg (event);
+  }
 });
+
+function showImg(event) {
+  const srcImg = event.target.files[0];
+  imageShow.src = URL.createObjectURL(srcImg);
+}
