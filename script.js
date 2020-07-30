e47f9a475a27271a9205abb3cbac1d7b1e51b0c0
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-30 17:16:53 -0300

    Fixed the container border issue (was on the image, now is on the container)

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ window.onload = function () {
 const textInput = document.querySelector('#text-input');
 const textContainer = document.querySelector('#meme-text');
 const imgInput = document.querySelector('#meme-insert');
-const imgContainer = document.querySelector('#meme-image');
+const memeImage = document.querySelector('#meme-image');
+const imgContainer = document.querySelector('#meme-image-container');
 
 function addImage(event) {
-  imgContainer.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
 }
 
 function handleImageUpload() {
