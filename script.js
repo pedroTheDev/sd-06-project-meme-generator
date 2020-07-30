9ad0a18914a0dbf89a4fd68303227ea10d55308f
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-30 14:59:24 -0300

    Lets see if i get back to 100%

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 window.onload = function () {
-  const textInput = document.querySelector('#text-input');
-  const textContainer = document.querySelector('#meme-text');
-  const imgInput = document.querySelector('#meme-insert');
-  const imgContainer = document.querySelector('#meme-image');
-
-  function addText() {
-    textContainer.innerText = textInput.value;
-  }
-
-  function addImage(event) {
-    imgContainer.src = URL.createObjectURL(event.target.files[0]);
-  }
-  textInput.addEventListener('input', addText);
-  imgInput.addEventListener('change', addImage);
+  handleImageUpload();
+  handleTextInput();
 };
+
+const textInput = document.querySelector('#text-input');
+const textContainer = document.querySelector('#meme-text');
+const imgInput = document.querySelector('#meme-insert');
+const imgContainer = document.querySelector('#meme-image');
+
+function addImage(event) {
+  imgContainer.src = URL.createObjectURL(event.target.files[0]);
+}
+
+function handleImageUpload() {
+  imgInput.addEventListener('change', addImage);
+}
+
+function addText() {
+  textContainer.innerText = textInput.value;
+}
+
+function handleTextInput() {
+  textInput.addEventListener('input', addText);
+}
