6ecca6089795a88ad410cd7f0d5b1f7cab5c3b83
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-30 16:19:58 -0300

    Better button function

--- script.js
+++ script.js
@@ -25,20 +25,31 @@ function handleTextInput() {
   textInput.addEventListener('input', addText);
 }
 
+function clearButtonBackgroundColor() {
+  const borderButtons = document.querySelectorAll('.border-button');
+  for (let i = 0; i < borderButtons.length; i += 1) {
+    borderButtons[i].style.backgroundColor = '';
+  }
+}
+
 function changeBorder(event) {
   const selectedBorder = event.target;
   if (selectedBorder.id === 'fire') {
     imgContainer.style.border = '3px dashed red';
+    selectedBorder.style.backgroundColor = 'red';
   } else if (selectedBorder.id === 'water') {
     imgContainer.style.border = '5px double blue';
+    selectedBorder.style.backgroundColor = 'blue';
   } else {
     imgContainer.style.border = '6px groove green';
+    selectedBorder.style.backgroundColor = 'green';
   }
 }
 
 function handleBorderSelection() {
   document.addEventListener('click', function (event) {
     if (event.target.classList.contains('border-button')) {
+      clearButtonBackgroundColor();
       changeBorder(event);
     }
   });
