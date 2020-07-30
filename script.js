992885448359acc12bb3fda9cb7c0f68cf3411c8
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-30 15:27:57 -0300

    Added border buttons, and fixed img container fitting

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   handleImageUpload();
   handleTextInput();
+  handleBorderSelection()
 };
 
 const textInput = document.querySelector('#text-input');
@@ -22,4 +23,23 @@ function addText() {
 
 function handleTextInput() {
   textInput.addEventListener('input', addText);
+}
+
+function changeBorder(event) {
+  const selectedBorder = event.target;
+  if (selectedBorder.id ==='fire-button') {
+    imgContainer.style.border = '3px dashed red';
+  } else if (selectedBorder.id ==='water-button') {
+    imgContainer.style.border = '5px double blue';
+  } else {
+    imgContainer.style.border = '6px groove green';
+  }
+}
+
+function handleBorderSelection() {
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('border-button')) {
+      changeBorder(event);
+    }
+  })
 }
