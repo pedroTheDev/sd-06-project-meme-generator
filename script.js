508d5214bacc229c0b3149d49f1a358c5a3153a3
dilenio <dilenio@gmail.com>
2020-07-30 13:16:20 -0300

    6 - Permita a quem usa customizar o meme escolhido acrescentando a ele uma de três bordas. A página deve ter três botões, que ao serem clicados devem cada um trocar a própria borda ao redor do container.

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ imageUpload.addEventListener('change', function (event) {
     URL.revokeObjectURL(output.src);
   };
 });
+
+const button1 = document.querySelector('#fire');
+button1.addEventListener('click', function () {
+  const memeFire = document.querySelector('#meme-image-container');
+  memeFire.style.border = '3px dashed red';
+});
+
+const buttonWater = document.querySelector('#water');
+buttonWater.addEventListener('click', function () {
+  const memeWater = document.querySelector('#meme-image-container');
+  memeWater.style.border = '5px double blue';
+});
+
+const buttonEarth = document.querySelector('#earth');
+buttonEarth.addEventListener('click', function () {
+  const memeEarth = document.querySelector('#meme-image-container');
+  memeEarth.style.border = '6px groove green';
+});
