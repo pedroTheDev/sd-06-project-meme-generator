f1d38f6d392ed1b4bf538125dd5f7587c66028d6
dilenio <dilenio@gmail.com>
2020-07-30 13:55:27 -0300

    7 - Tenha um conjunto de quatro imagens pré prontas de memes famosos para o usuário escolher. Mostre miniaturas das imagens e, mediante clique do usuário, essa imagem deve aparecer dentro da moldura do elemento de container.

--- script.js
+++ script.js
@@ -31,3 +31,27 @@ buttonEarth.addEventListener('click', function () {
   const memeEarth = document.querySelector('#meme-image-container');
   memeEarth.style.border = '6px groove green';
 });
+
+const meme1 = document.querySelector('#meme-1');
+meme1.addEventListener('click', function () {
+  const replaceImage = document.querySelector('#meme-image');
+  replaceImage.src = 'meme1.png';
+});
+
+const meme2 = document.querySelector('#meme-2');
+meme2.addEventListener('click', function () {
+  const replaceImage = document.querySelector('#meme-image');
+  replaceImage.src = 'meme2.png';
+});
+
+const meme3 = document.querySelector('#meme-3');
+meme3.addEventListener('click', function () {
+  const replaceImage = document.querySelector('#meme-image');
+  replaceImage.src = 'meme3.png';
+});
+
+const meme4 = document.querySelector('#meme-4');
+meme4.addEventListener('click', function () {
+  const replaceImage = document.querySelector('#meme-image');
+  replaceImage.src = 'meme4.png';
+});
