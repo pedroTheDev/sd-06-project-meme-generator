4ef87bb16c791f2ce357a66e56a26d5e4318aaf7
DekoPossas <andrepossaster@gmail.com>
2020-07-29 22:40:41 -0300

    adicionei no script a funcao que muda o texto do meme quando digitado no campo de texto.

---
+++ script.js
@@ -0,0 +1,6 @@
+const txtInput = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text ');
+
+txtInput.addEventListener("keyup", function(){
+  memeText.innerHTML = txtInput.value;
+})
