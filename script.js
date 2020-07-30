5610499298ed09febe1d8d65c5572682d888e593
isabellavjs <isabellavjs@gmail.com>
2020-07-30 11:15:05 -0300

    Bonus 6

--- script.js
+++ script.js
@@ -12,5 +12,31 @@ let uploadedMeme = function(event) {
     insertMeme.src = URL.createObjectURL(event.target.files[0]);
 }
 
+//Customize the container borders
+let memeContainer = document.getElementById('meme-image-container');
+const fireStyleButton = document.getElementById('fire');
+fireStyleButton.style.backgroundColor = 'red';
+fireStyleButton.addEventListener('click', function() {
+   memeContainer.classList.add('fireStyle');
+   memeContainer.classList.remove('waterStyle');
+   memeContainer.classList.remove('earthStyle');
+})
+
+const waterStyleButton = document.getElementById('water');
+waterStyleButton.style.backgroundColor = 'blue';
+waterStyleButton.addEventListener('click', function() {
+    memeContainer.classList.add('waterStyle');
+    memeContainer.classList.remove('earthStyle');
+    memeContainer.classList.remove('fireStyle');
+})
+
+const earthStyleButton = document.getElementById('earth');
+earthStyleButton.style.backgroundColor = 'green';
+earthStyleButton.addEventListener('click', function() {
+    memeContainer.classList.add('earthStyle');
+    memeContainer.classList.remove('waterStyle');
+    memeContainer.classList.remove('fireStyle');
+})
+
 
 
