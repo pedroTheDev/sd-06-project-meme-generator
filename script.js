8dbf71822aca640cb71ed73f8044b9d4630b8cfd
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-30 17:24:41 -0300

    a img debe estar dentro do id  meme-image para ter attributo src, eu tinha criado uma div com essa id tira esa dic e coloque img

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.onload = function () {
   let temImagem = false;
   //  Tomando imagen e colocando no container.
-  const memeImage = document.createElement('img');
+  const memeImage = document.querySelector('img');
   loadFile = function (event) {
     removerAntigua();
     const reader = new FileReader();
     reader.onload = function () {
-      const image = document.getElementById('meme-image').appendChild(memeImage);
+      const image = document.getElementById('meme-image-container').appendChild(memeImage);
       temImagem = true;
       image.src = reader.result;
     };
@@ -23,7 +23,7 @@ window.onload = function () {
   const bordas = document.querySelector('#bordas'); 
   bordas.addEventListener('click', function (event) {
     const bordaSelecionada = event.target;
-    let propBorda = window.getComputedStyle(bordaSelecionada, null).getPropertyValue("border");
+    const propBorda = window.getComputedStyle(bordaSelecionada, null).getPropertyValue("border");
     document.querySelector('#meme-image-container').style.border = propBorda;
   });
   //  Imagens preselecionadas em miniuatura
@@ -31,7 +31,7 @@ window.onload = function () {
   imagenMiniatura.addEventListener('click', function (event) {
     removerAntigua();
     memeImage.src = event.target.src;
-    const image = document.getElementById('meme-image').appendChild(memeImage);
+    document.getElementById('meme-image-container').appendChild(memeImage);
     temImagem = true;
   });
   //  funcao remover foto antigua
