59df0e004641fd06aa72ac7776722635663d50ff
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-30 18:39:27 -0300

    Inserido item bônus borda selecionada

--- script.js
+++ script.js
@@ -13,3 +13,34 @@
     imagePreview.src = window.URL.createObjectURL(fileChooser.files[0]);
   }
   fileChooser.addEventListener('change', readImage);
+
+  // Itens bônus
+  const principal = document.getElementById('meme-image-container');
+  const fire = document.getElementById('fire');
+  const water = document.getElementById('water');
+  const earth = document.getElementById('earth');
+  
+  function selectBorders() {
+    if (event.target === fire) {
+      principal.style.borderColor = 'red';
+      principal.style.borderWidth = '3px';
+      principal.style.borderStyle = 'dashed';
+    }
+  
+    if (event.target === water) {
+      principal.style.borderColor = 'blue';
+      principal.style.borderWidth = '5px';
+      principal.style.borderStyle = 'double';
+    }
+  
+    if (event.target === earth) {
+      principal.style.borderColor = 'green';
+      principal.style.borderWidth = '6px';
+      principal.style.borderStyle = 'groove';
+    }
+  }
+  fire.addEventListener('click', selectBorders);
+  water.addEventListener('click', selectBorders);
+  earth.addEventListener('click', selectBorders);
+
+
