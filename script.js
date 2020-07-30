06bac96f2f8bc8ce1021a67295a48883804c721d
bragammartins <bragammartins@gmail.com>
2020-07-30 02:01:04 -0300

    14 - correcting codeclimate errors

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const textInput = document.querySelector('#text-input');
 const textInside = document.querySelector('#meme-text');
 const imageInput = document.querySelector('#meme-image');
-const imagePre = document.querySelector('#meme-image-container');
+const imagePre = document.querySelector('#meme-insert');
+const botonFire = document.querySelector('#fire')
 
 function changeText() {
   textInside.innerText = textInput.value;
@@ -18,19 +19,23 @@ function loadFile(event) {
 
 function sI(event) {
   imageInput.src = event.target.src;
-}
+};
 
 function earth() {
   imagePre.className = 'meme-container earth';
-}
+};
 
-function fire() {
-  imagePre.className = 'meme-container fire';
-}
+btnFire.addEventListener('click', function () {
+  document.getElementById('meme-image-container').style.border = '3px dashed red';
+});
+
+// function fire() {
+//   imagePre.className = 'meme-container fire';
+// }
 
 function water() {
   imagePre.className = 'meme-container water';
-}
+};
 
 // if (imageInput === 'Agora estou  usando, CodeClimate') {
 //   water();
