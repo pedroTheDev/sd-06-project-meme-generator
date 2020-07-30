17811449249deac95833e3736db1996168baba5e
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-07-30 16:59:22 -0300

    falta consertar as bordas coloridas dos botões

--- script.js
+++ script.js
@@ -14,14 +14,6 @@
   });
 
 
-
-
-
-
-
-
-
-
   var loadFile = function(event) {
       var output = document.getElementById('meme-image');
       output.src = URL.createObjectURL(event.target.files[0]);
@@ -29,3 +21,27 @@
           URL.revokeObjectURL(output.src)
       }
   };
+
+
+
+
+  let buttonRed = document.querySelector("#fire");
+  buttonRed.addEventListener("click", function() {
+
+      let memeFire = document.querySelector('#meme-image-container');
+      memeFire.style.border = '3px dashed red';
+  });
+
+  let buttonBlue = document.querySelector("#water");
+  buttonBlue.addEventListener("click", function() {
+
+      let memeWater = document.querySelector('#meme-image-container');
+      memeWater.style.border = '5px doble blue';
+  });
+
+  let buttonGreen = document.querySelector("#earth");
+  buttonRed.addEventListener("click", function() {
+
+      let memeEarth = document.querySelector('#meme-image-container');
+      memeEarth.style.border = '6px groove green';
+  });
