b9518e97e2abe6d7e69b97c89aa311d1c302c9d0
Pablo Santos <pablo.trybe@gmail.com>
2020-07-30 19:29:41 -0300

    Implementando as bordas ao projeto

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ function showImage(e) {
   memeImg.src = URL.createObjectURL(getFile);
 }
 output.addEventListener('change', showImage);
+
+const btnRed = document.getElementById('fire');
+function changeRed() {
+  document.getElementById('meme-image-container').style.border = '3px dashed red';
+}
+btnRed.addEventListener('click', changeRed);
+
+const btnBlue = document.getElementById('water');
+function changeBlue() {
+  document.getElementById('meme-image-container').style.border = '5px double blue';
+}
+btnBlue.addEventListener('click', changeBlue);
+
+const btnGreen = document.getElementById('earth');
+function changeGreen() {
+  document.getElementById('meme-image-container').style.border = '6px groove green';
+}
+btnGreen.addEventListener('click', changeGreen);
+
+const btnReset = document.getElementById('reset');
+function resetBorder() {
+  document.getElementById('meme-image-container').style.border = '1px solid black';
+}
+btnReset.addEventListener('click', resetBorder);
