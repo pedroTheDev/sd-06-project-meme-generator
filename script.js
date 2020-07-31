bf3a129de2e4adad2310a801c684f6e31490ccaa
lucasvr39 <lucasveras1994@gmail.com>
2020-07-31 18:21:56 -0300

    agora vai

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const memeImg = document.querySelector('#meme-image');
-const imgInsert = document.querySelector('#meme-insert');
-const userTextInput = document.querySelector('#text-input');
-const memeTextInput = document.querySelector('#meme-text');
-const imgContainer = document.querySelector('#meme-image-container');
-const btnFire = document.querySelector('#fire');
-const btnWater = document.querySelector('#water');
-const btnEarth = document.querySelector('#earth');
+const memeImg = document.getElementById('meme-image');
+const imgInsert = document.getElementById('meme-insert');
+const userTextInput = document.getElementById('text-input');
+const memeTextInput = document.getElementById('meme-text');
+const imgContainer = document.getElementById('meme-image-container');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
 
 function uploadFile() {
   memeImg.src = window.URL.createObjectURL(this.files[0]);
