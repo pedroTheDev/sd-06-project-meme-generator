a9f24e7095a5bbac146e5f8569a582f3376563a5
Pablo Santos <pablo.trybe@gmail.com>
2020-07-30 21:31:32 -0300

    Finalizando os requisitos bonus

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Método para pegar o texto digitado e exibir no container */
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 let textInMeme = '';
@@ -8,6 +9,7 @@ function printTextMeme() {
 
 inputText.addEventListener('keyup', printTextMeme);
 
+/* Método para exibir as imagens selecionadas no container */
 const output = document.getElementById('meme-insert');
 const memeImg = document.getElementById('meme-image');
 function showImage(e) {
@@ -16,6 +18,7 @@ function showImage(e) {
 }
 output.addEventListener('change', showImage);
 
+/* Métodos para adicionar as bordas pré definidas ao iniciar o programa */
 const btnRed = document.getElementById('fire');
 function changeRed() {
   document.getElementById('meme-image-container').style.border = '3px dashed red';
@@ -39,3 +42,28 @@ function resetBorder() {
   document.getElementById('meme-image-container').style.border = '1px solid black';
 }
 btnReset.addEventListener('click', resetBorder);
+
+/* Método para adicionar os memes pré prontos no container */
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
+const memeChange = document.getElementById('meme-insert');
+
+function setMeme(event) {
+  const meme = event.target;
+  if (meme === meme1) {
+    memeImg.src = '/img/meme1.png';
+  } else if (meme === meme2) {
+    memeImg.src = '/img/meme2.png';
+  } else if (meme === meme3) {
+    memeImg.src = '/img/meme3.png';
+  } else if (meme === meme4) {
+    memeImg.src = '/img/meme4.png';
+  }
+}
+memeChange.addEventListener('change', setMeme);
+meme1.addEventListener('click', setMeme);
+meme2.addEventListener('click', setMeme);
+meme3.addEventListener('click', setMeme);
+meme4.addEventListener('click', setMeme);
