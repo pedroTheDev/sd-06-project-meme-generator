b5162ae5cbe618fb66accabdc13ab9b17877ef84
lucasvr39 <lucasveras1994@gmail.com>
2020-07-31 16:24:54 -0300

    try to fix text and img issue to evaluator again

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const userTextInput = document.querySelector('#text-input');
-const memeTextInput = document.querySelector('#meme-text');
-const imgContainer = document.querySelector('#meme-image-container');
+const userTextInput = document.getElementById('text-input');
+const memeTextInput = document.getElementById('meme-text');
+const imgContainer = document.getElementById('meme-image-container');
 const btnFire = document.getElementById('fire');
 const btnWater = document.getElementById('water');
 const btnEarth = document.getElementById('earth');
@@ -9,12 +9,9 @@ userTextInput.addEventListener('input', function () {
   memeTextInput.innerText = userTextInput.value;
 });
 
-document
-  .getElementById('meme-insert')
-  .addEventListener('change', function (event) {
-    const uploadImg = document.getElementById('meme-image');
-    uploadImg.src = window.URL.createObjectURL(event.target.files[0]);
-  });
+document.getElementById('meme-insert').addEventListener('change', function () {
+  document.getElementById('meme-image').src = window.URL.createObjectURL(this.files[0]);
+});
 
 btnFire.addEventListener('click', function () {
   imgContainer.style.border = '3px dashed red';
