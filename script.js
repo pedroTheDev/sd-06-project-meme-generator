272eb4288ef6e3ef2261a4864112a91b21769af2
LeandroFeitozaGnu <leandro.feitoza8018@gmail.com>
2020-07-31 11:05:12 -0300

    Codeclimate apontando 78 issues

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
-    const loadFile = function(event) {
-    const output = document.getElementById ('meme-image');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
+    const loadFile = function (event) {
+      const output = document.getElementById('meme-image');
+      output.src = URL.createObjectURL(event.target.files[0]);
+      output.onload = function () {
       URL.revokeObjectURL(output.src);
     };
   };
-  const inputTextBox = document.getElementById('text-input');
-  const paragraph = document.getElementById("output-text");
-  function insertText(event) {
+    const inputTextBox = document.getElementById('text-input');
+    const paragraph = document.getElementById("output-text");
+    function insertText(event) {
     document.querySelector('text-input');
     paragraph.innerHTML = inputTextBox.value;
   }
-  inputTextBox.addEventListener('keyup', insertText);
+    inputTextBox.addEventListener('keyup', insertText);
 
-  function styleFire() {
-    document.querySelector('#meme-image-container').style.border = '3px dashed red'
+    function styleFire() {
+    document.querySelector('#meme-image-container').style.border = '3px dashed red';
   }
   
-  function styleWater() {
-    document.querySelector('#meme-image-container').style.border = '5px double blue'
+    function styleWater() {
+    document.querySelector('#meme-image-container').style.border = '5px double blue';
   }
   
-  function styleEarth() {
-    document.querySelector('#meme-image-container').style.border = '6px groove green'
+    function styleEarth() {
+    document.querySelector('#meme-image-container').style.border = '6px groove green';
   }
   // Botões adicionam borda
   // Botão Fire
-  const buttonFire = document.createElement('button');
-  buttonFire.id = 'fire';
-  buttonFire.className = 'custom-button';
-  const textBtnFire = document.createTextNode('Fire');
-  buttonFire.appendChild(textBtnFire);
-  buttonFire.style.backgroundColor = 'red';
-  buttonFire.addEventListener('click', styleFire);
+    const buttonFire = document.createElement('button');
+    buttonFire.id = 'fire';
+    buttonFire.className = 'custom-button';
+    const textBtnFire = document.createTextNode('Fire');
+    buttonFire.appendChild(textBtnFire);
+    buttonFire.style.backgroundColor = 'red';
+    buttonFire.addEventListener('click', styleFire);
   // Botão Water
-  const buttonWater = document.createElement('button');
-  buttonWater.id = 'water';
-  buttonWater.className = 'custom-button';
-  const textBtnWater = document.createTextNode('Water');
-  buttonWater.appendChild(textBtnWater);
-  buttonWater.style.backgroundColor = 'blue';
-  buttonWater.addEventListener('click', styleWater);
+    const buttonWater = document.createElement('button');
+    buttonWater.id = 'water';
+    buttonWater.className = 'custom-button';
+    const textBtnWater = document.createTextNode('Water');
+    buttonWater.appendChild(textBtnWater);
+    buttonWater.style.backgroundColor = 'blue';
+    buttonWater.addEventListener('click', styleWater);
   // botão Earth
-  const buttonEarth = document.createElement('button');
-  buttonEarth.id = 'earth';
-  buttonEarth.className = 'custom-button';
-  const textBtnEarth = document.createTextNode('Earth');
-  buttonEarth.appendChild(textBtnEarth);
-  buttonEarth.style.backgroundColor = 'green';
-  buttonEarth.addEventListener('click', styleEarth);
+    const buttonEarth = document.createElement('button');
+    buttonEarth.id = 'earth';
+    buttonEarth.className = 'custom-button';
+    const textBtnEarth = document.createTextNode('Earth');
+    buttonEarth.appendChild(textBtnEarth);
+    buttonEarth.style.backgroundColor = 'green';
+    buttonEarth.addEventListener('click', styleEarth);
 
-  document.querySelector('.customization').appendChild(buttonFire);
-  document.querySelector('.customization').appendChild(buttonWater);
-  document.querySelector('.customization').appendChild(buttonEarth);
+    document.querySelector('.customization').appendChild(buttonFire);
+    document.querySelector('.customization').appendChild(buttonWater);
+    document.querySelector('.customization').appendChild(buttonEarth);
 
   // Event Bubbling em ação
-  const minis = document.querySelectorAll('.miniatura')
-  for (var i = 0; i < minis.length; i ++ ) {
-    minis[i].onclick = function(event) {
-      document.querySelector('#meme-image').src = event.target.src
-    }
+    const minis = document.querySelectorAll('.miniatura');
+    for (let i = 0; i < minis.length; i += 1) {
+      minis[i].onclick = function(event) {
+      document.querySelector('#meme-image').src = event.target.src;
+    };
   }
