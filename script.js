ece4e19d79e034475e7bf938376cf1b7ed8575c9
Paulo <paulocheloni@gmail.com>
2020-07-31 08:54:08 -0300

    alterando propriedade memeContainer com ComputedStyle

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const insertMeme = document.querySelector('#meme-insert');
-const insertText = document.querySelector('#text-input');
+window.onload = () => {
 
-insertMeme.addEventListener('change', function (e) {
-  addImg(e,insertMeme);
-});
-
-insertText.addEventListener('keyup', function (e) {
-  addTxt(e);
-});
-
-function addTxt (e) {
+  function addTxt (e) {
   textInput = document.querySelector('#text-input').value;
   textMeme = document.querySelector('#meme-text');
-  console.log(textInput);
   textMeme.innerText = textInput;
+  }
 
+  function addImg (e) {
+  document.querySelector('#meme-image').src = URL.createObjectURL(e.target.files[0]);
+  }
 
-}
+  function changeBorderColor (e) {
+    button = e.currentTarget;
+    memeContainer = document.getElementById('meme-image-container');
+    memeContainer.style.borderColor = getComputedStyle(button).backgroundColor;
+  }
 
-function addImg (e) {
-   document.querySelector('#meme-image').src = URL.createObjectURL(e.target.files[0]);
+  const insertMeme = document.querySelector('#meme-insert');
+  const insertText = document.querySelector('#text-input');
+  const changeBorder = document.querySelector('.border').children;
+
+  insertMeme.addEventListener('change', function (e) {
+    addImg(e,insertMeme);
+  });
+
+  insertText.addEventListener('keyup', function (e) {
+    addTxt(e);
+  });
+
+  for (let i = 0; i < changeBorder.length; i += 1)
+  changeBorder[i].addEventListener('click', function (e) {
+    changeBorderColor(e);
+  });
 }
