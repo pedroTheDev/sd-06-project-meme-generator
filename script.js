66b956cec9437c344fd3d2dbe2ef6bb963dd6585
Flavio Oliveira <flavsoliver@gmail.com>
2020-07-31 01:57:28 -0300

    alteração de extensão arquivos de imagem: Evaluator

--- script.js
+++ script.js
@@ -16,35 +16,35 @@ inputImage.addEventListener('change', insertImage);
 const meme1 = document.querySelector('#meme-1');
 const replaceImage01 = document.querySelector('#meme-image');
 function insertImage01() {
-  replaceImage01.src = 'imgs/meme1.jpg';
+  replaceImage01.src = 'imgs/meme1.png';
 }
 meme1.addEventListener('click', insertImage01);
 
 const meme2 = document.querySelector('#meme-2');
 const replaceImage02 = document.querySelector('#meme-image');
 function insertImage02() {
-  replaceImage02.src = 'imgs/meme2.jpg';
+  replaceImage02.src = 'imgs/meme2.png';
 }
 meme2.addEventListener('click', insertImage02);
 
 const meme3 = document.querySelector('#meme-3');
 const replaceImage03 = document.querySelector('#meme-image');
 function insertImage03() {
-  replaceImage03.src = 'imgs/meme3.jpg';
+  replaceImage03.src = 'imgs/meme3.png';
 }
 meme3.addEventListener('click', insertImage03);
 
 // const meme4 = document.querySelector('#meme-4');
 // const replaceImage04 = document.querySelector('#meme-image');
 // function insertImage04() {
-//   replaceImage04.src = 'imgs/meme4.jpg';
+//   replaceImage04.src = 'imgs/meme4.png';
 // }
 // meme4.addEventListener('click', insertImage04);
 
 const meme5 = document.querySelector('#meme-5');
 const replaceImage05 = document.querySelector('#meme-image');
 function insertImage05() {
-  replaceImage05.src = 'imgs/meme5.jpg';
+  replaceImage05.src = 'imgs/meme5.png';
 }
 meme5.addEventListener('click', insertImage05);
 
