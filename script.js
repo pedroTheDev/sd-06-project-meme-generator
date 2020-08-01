8430f6bc301a0bab3e68ebc07ebd9f49047d9f48
vania patricia <vapatty@gmail.com>
2020-08-01 14:22:33 -0300

    modificando javascript, css e html

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
-var textInput = document.getElementById('text-input');
-var memeText = document.getElementById('meme-text');
+let loadImage = function (event) {
+  let output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src);
+  };
+};
 
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
 textInput.addEventListener('input', memeTextInsert);
+
 function memeTextInsert() {
   memeText.innerHTML = textInput.value;
 }
+
+
+
+
 
