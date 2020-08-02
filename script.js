2aa3dff0a18454173797b4f9ecabab423585ff57
theCoyote <flaviolunaferreira@gmail.com>
2020-08-02 19:37:31 -0300

    colocando butoes para bordas

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const userTextInput = document.querySelector('#text-input');
 const memeTextInput = document.querySelector('#meme-text');
-const fileChooser = document.getElementById('meme-insert');
-const imagePreview = document.getElementById('meme-image');
+const abrirImagem = document.getElementById('meme-insert');
+const imagemAberta = document.getElementById('meme-image');
+const bordaStyle = document.getElementById('meme-image-container');
+const fireBtn = document.getElementById('fire');
+const waterBtn = document.getElementById('water');
+const earthBtn = document.getElementById('earth');
 
+/* ********************************************************* */
 userTextInput.addEventListener('input', function () {
   memeTextInput.innerText = userTextInput.value;
 });
 
-function abrirImage() {
-  imagePreview.src = window.URL.createObjectURL(fileChooser.files[0]);
+/* ********************************************************* */
+function abrirImage () {
+  imagemAberta.src = window.URL.createObjectURL(abrirImagem.files[0]);
 }
-fileChooser.addEventListener('change', abrirImage);
+abrirImagem.addEventListener('change', abrirImage);
+
+/* ********************************************************* */
+fireBtn.addEventListener('click', function () {
+  bordaStyle.style.border = '3px dashed red';
+});
+
+/* ********************************************************* */
+waterBtn.addEventListener('click', function () {
+  bordaStyle.style.border = '5px double blue';
+});
+
+earthBtn.addEventListener('click', function () {
+  bordaStyle.style.border = '6px groove green';
+});
+
