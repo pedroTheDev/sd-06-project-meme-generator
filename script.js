1ff0f29ca0c7ee84c553788ab0c07c1f30815c6d
theCoyote <flaviolunaferreira@gmail.com>
2020-08-02 13:51:43 -0300

    brigando com minha imagem

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const userTextInput = document.querySelector('#text-input');
 const memeTextInput = document.querySelector('#meme-text');
+const fileChooser = document.getElementById('meme-insert');
+const imagePreview = document.getElementById('meme-image');
 
 userTextInput.addEventListener('input', function () {
   memeTextInput.innerText = userTextInput.value;
 });
+
+function abrirImage() {
+  imagePreview.src = window.URL.createObjectURL(fileChooser.files[0]);
+}
+fileChooser.addEventListener('change', abrirImage);
