c6c500a9f7472993308b753fccda9a5a05dd5c4e
lucasvr39 <lucasveras1994@gmail.com>
2020-08-01 23:07:33 -0300

    add pre made memes req

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ const imgContainer = document.getElementById('meme-image-container');
 const btnFire = document.getElementById('fire');
 const btnWater = document.getElementById('water');
 const btnEarth = document.getElementById('earth');
+const preMadeOne = document.getElementById('meme-1');
+const preMadeTwo = document.getElementById('meme-2');
+const preMadeThree = document.getElementById('meme-3');
+const preMadeFour = document.getElementById('meme-4');
 
 function uploadFile() {
+  // eslint-disable-next-line node/no-unsupported-features/node-builtins
   memeImg.src = window.URL.createObjectURL(this.files[0]);
 }
 
@@ -27,4 +32,20 @@ btnWater.addEventListener('click', function () {
 
 btnEarth.addEventListener('click', function () {
   imgContainer.style.border = '6px groove green';
+});
+
+preMadeOne.addEventListener('click', function () {
+  memeImg.src = '/sd-06-project-meme-generator/src/meme-1.jpg';
+});
+
+preMadeTwo.addEventListener('click', function () {
+  memeImg.src = '/sd-06-project-meme-generator/src/meme-2.jpg';
+});
+
+preMadeThree.addEventListener('click', function () {
+  memeImg.src = '/sd-06-project-meme-generator/src/meme-3.jpg';
+});
+
+preMadeFour.addEventListener('click', function () {
+  memeImg.src = '/sd-06-project-meme-generator/src/meme-4.jpg';
 });
