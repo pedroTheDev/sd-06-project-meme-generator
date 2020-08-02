4fffbdb7c23834ae475d77741ff5fd98ba04aa12
Bruna Franciele <brunafranciele1902@gmail.com>
2020-08-02 19:52:56 -0300

    envio do requisito 7

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ caixaImagem.addEventListener("change", function(event) {
 document.getElementById("meme-image").src = 
 URL.createObjectURL(event.target.files[0])
 })
-
+// Funções para criar o botões que mudam as bordas
 let botaoFire = document.querySelector("#fire")
 let botaoWater = document.querySelector("#water")
 let botaoEarth = document.querySelector("#earth")
@@ -25,3 +25,21 @@ botaoWater.addEventListener("click", function() {
 botaoEarth.addEventListener("click", function() {
     container.style.border = "6px rgb(0, 128, 0) groove"
 })
+
+// funções para inserir memes pré prontos
+let meme1 = document.querySelector("#meme-1")
+meme1.addEventListener("click", function() {
+    document.getElementById("meme-image").src = "imgs/meme1.png"
+    })
+let meme2 = document.querySelector("#meme-2")
+meme2.addEventListener("click", function() {
+    document.getElementById("meme-image").src = "imgs/meme2.png"
+    })
+let meme3 = document.querySelector("#meme-3")
+meme3.addEventListener("click", function() {
+    document.getElementById("meme-image").src = "imgs/meme3.png"
+     })
+let meme4 = document.querySelector("#meme-4")
+meme4.addEventListener("click", function() {
+    document.getElementById("meme-image").src = "imgs/meme4.png"
+    }) 
