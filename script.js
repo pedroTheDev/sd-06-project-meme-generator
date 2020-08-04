8e1c5557dbd2611ef062baaf815629f7ef47cb16
Pedro Duarte <ikbenpedro@gmail.com>
2020-08-04 14:43:11 -0300

    add fire, water earth button

--- script.js
+++ script.js
@@ -12,4 +12,29 @@ window.onload = function() {
         document.getElementById("meme-text").innerHTML = x.value;
     }
     x.addEventListener("input", myFourthFunction);
+
+
+    let redBtn = document.getElementById("fire")
+
+    function changeBorderRed() {
+        document.getElementById("meme-image-container").style.border = "3px dashed red";
+    }
+    redBtn.addEventListener("click", changeBorderRed)
+
+    let blueBtn = document.getElementById("water")
+
+    function changeBorderBlue() {
+        document.getElementById("meme-image-container").style.border = "5px double blue";
+    }
+    blueBtn.addEventListener("click", changeBorderBlue)
+
+    let greenBtn = document.getElementById("earth")
+
+    function changeBorderGreen() {
+        document.getElementById("meme-image-container").style.border = "6px groove green";
+    }
+    greenBtn.addEventListener("click", changeBorderGreen)
+
+
+
 }
