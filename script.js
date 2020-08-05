334c29eddca41809b5bb2047fb7560dc5a99b0cf
gussta <saideles.silveira@gmail.com>
2020-08-05 10:58:59 -0300

    script e html talvez prontos

---
+++ script.js
@@ -0,0 +1,15 @@
+  const inputImage = document.querySelector("#meme-insert");
+  const inputText = document.querySelector("#text-input");
+  const memeImage = document.querySelector("#meme-image");
+  const memeText = document.querySelector("#meme-text");
+
+  inputImage.addEventListener("change", function(event){
+    memeImage.src = URL.createObjectURL(event.target.files[0])
+  })
+  
+  inputText.addEventListener("keyup", function(){
+    memeText.innerText = inputText.value;
+    memeText.className = "text-input";
+  })
+
+
