895f8eaf80a61b27057073172139eff897fb674b
gussta <saideles.silveira@gmail.com>
2020-08-05 13:17:25 -0300

    correcoes e CSS add.. sera?

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
   const memeImage = document.querySelector('#meme-image');
   const memeText = document.querySelector('#meme-text');
 
-  inputImage.addEventListener('change', function(event) {
+  inputImage.addEventListener('change', function (event) {
     memeImage.src = URL.createObjectURL(event.target.files[0]);
   });
 
-  inputText.addEventListener('keyup', function() {
+  inputText.addEventListener('keyup', function () {
     memeText.innerText = inputText.value;
     memeText.className = 'text-input';
   });
