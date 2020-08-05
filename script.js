3bf3646b09dda671a4667c7486057d22ceb94b26
Anderson R <misterregisslv@gmail.com>
2020-08-05 01:19:44 -0300

    lição bônus 1 adicionada

--- script.js
+++ script.js
@@ -15,4 +15,24 @@ window.onload = function () {
       URL.revokeObjectURL(imagem.src);
     };
   };
+  
+  document.querySelector('#fire').addEventListener('click', fire);
+  document.querySelector('#water').addEventListener('click', water);
+  document.querySelector('#earth').addEventListener('click', earth);
+
 };
+
+function fire () {
+  const moldura = document.querySelector('#meme-image-container');
+  moldura.style.border = '1px dashed red';
+}
+
+function water () {
+  const moldura = document.querySelector('#meme-image-container');
+  moldura.style.border = '5px double blue';
+}
+
+function earth () {
+  const moldura = document.querySelector('#meme-image-container');
+  moldura.style.border = '6px groove green';
+}
