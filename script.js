366aed5f7e1909439581c74eed936e4ec5587ac6
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-08-05 18:47:27 -0300

    Trocando as imagens, e removendo o onload

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-window.onload = function () {
-  const text = document.querySelector('#text-input');
-  const memeText = document.querySelector('#meme-text');
-  const memeCont = document.querySelector('#meme-image-container');
-  const fire = document.querySelector('#fire');
-  const water = document.querySelector('#water');
-  const earth = document.querySelector('#earth');
-  const memeImage = document.getElementById('meme-image');
-  const imgP = document.querySelector('#imgP');
-  text.addEventListener('keyup', function () {
-    memeText.innerText = text.value;
-  });
-  fire.addEventListener('click', function () {
-    memeCont.style.border = '3px dashed red';
-  });
-  water.addEventListener('click', function () {
-    memeCont.style.border = '5px double blue';
-  });
-  earth.addEventListener('click', function () {
-    memeCont.style.border = '6px groove green';
-  });
-  imgP.addEventListener('click', function (event) {
-    memeImage.src = event.target.src;
-  });
-};
+const text = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+const memeCont = document.querySelector('#meme-image-container');
+const fire = document.querySelector('#fire');
+const water = document.querySelector('#water');
+const earth = document.querySelector('#earth');
+const memeImage = document.getElementById('meme-image');
+const imgP = document.querySelector('#imgP');
+
+text.addEventListener('keyup', function () {
+  memeText.innerText = text.value;
+});
+
+fire.addEventListener('click', function () {
+  memeCont.style.border = '3px dashed red';
+});
+
+water.addEventListener('click', function () {
+  memeCont.style.border = '5px double blue';
+});
+
+earth.addEventListener('click', function () {
+  memeCont.style.border = '6px groove green';
+});
+
+imgP.addEventListener('click', function (event) {
+  memeImage.src = event.target.src;
+});
 
 const loadFile = function (event) {
   const memeImage = document.getElementById('meme-image');
