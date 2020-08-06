67a824519d0ac9c3849b306e446faeb58864d23d
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-08-06 02:23:00 -0300

    Terminando projeto

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function onLoadImg() {
     URL.revokeObjectURL(img.src);
   };
 }
+
+function changeBorderFire() {
+  document.getElementById('meme-image-container').style.border = '3px dashed red';
+}
+
+function changeBorderWater() {
+  document.getElementById('meme-image-container').style.border = '5px double blue';
+}
+
+function changeBorderEarth() {
+  document.getElementById('meme-image-container').style.border = '6px groove green';
+}
